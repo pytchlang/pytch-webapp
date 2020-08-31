21b4fa9d626a7ac640d7fd07c1c3349b1953e2f3
Ben North <ben@redfrontdoor.org>
2020-08-31 09:16:48 +0100

    Put NavBanner at top of Welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router"
+import NavBanner from "./NavBanner";
 
 interface WelcomeProps extends RouteComponentProps {}
 
 const Welcome = (props: WelcomeProps) => {
     return (
         <>
+        <NavBanner/>
         <p>Welcome to Pytch!</p>
         </>
     );
