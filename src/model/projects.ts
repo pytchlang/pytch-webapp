2a6e2c4e089597fdc597c1113176593bea5f37a2
Ben North <ben@redfrontdoor.org>
2020-08-31 08:47:26 +0100

    IProjectSummary: Add

---
+++ src/model/projects.ts
@@ -0,0 +1,9 @@
+export interface IProjectSummary {
+  // TODO: Is this the right place to note whether a project
+  // is tracking a tutorial?  Or a separate 'ProjectTutorialBookmark'
+  // 'table', thinking in the relational way?
+
+  id: string;
+  name: string;
+  summary?: string;
+}
