b09a157c10f67171be8d4a79e06d369f7d59ec50
Ben North <ben@redfrontdoor.org>
2020-08-31 09:32:21 +0100

    loadSummaries(): Add database function
    Currently with logging and delay to see what's going on.

--- src/database/projects.ts
+++ src/database/projects.ts
@@ -4,3 +4,16 @@ const dummyProjects: Array<IProjectSummary> = [
   {id: "p1", name: "Bash the zombies", summary: "Run round splatting zombies."},
   {id: "p2", name: "Bike game"},
 ];
+
+const delay = (ms: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  })
+}
+
+export const loadAllSummaries = async (): Promise<Array<IProjectSummary>> => {
+  console.log("db.loadAllSummaries(): entering");
+  await delay(500);
+  console.log("db.loadAllSummaries(): about to return");
+  return dummyProjects;
+}
