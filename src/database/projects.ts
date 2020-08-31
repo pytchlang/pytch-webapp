f82827f7f287abfe8251fa302d39a16bdf38dd7f
Ben North <ben@redfrontdoor.org>
2020-08-31 09:19:59 +0100

    Add database of (dummy) project summaries

---
+++ src/database/projects.ts
@@ -0,0 +1,6 @@
+import { IProjectSummary } from "../model/projects";
+
+const dummyProjects: Array<IProjectSummary> = [
+  {id: "p1", name: "Bash the zombies", summary: "Run round splatting zombies."},
+  {id: "p2", name: "Bike game"},
+];
