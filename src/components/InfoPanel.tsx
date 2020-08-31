11efa74a0d8c7ca9b30d9f6685fc85fb7ce7a98f
Ben North <ben@redfrontdoor.org>
2020-08-31 13:17:18 +0100

    Add placeholder Assets/StdOut/Error panes

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 
+const Assets = () => {
+    return (
+        <div>
+            Your project's images and sounds.
+        </div>
+    );
+};
+
+const StandardOutput = () => {
+    return (
+        <div>
+            Anything your project prints will appear here.
+        </div>
+    )
+}
+
+const Errors = () => {
+    return (
+        <div>
+            Any errors your project encounters will appear here.
+        </div>
+    );
+}
+
 const InfoPanel = () => {
     return (
         <div className="InfoPanel">
