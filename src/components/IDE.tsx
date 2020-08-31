e267022a648d6fa61d51b1226401c548598a96ba
Ben North <ben@redfrontdoor.org>
2020-08-31 13:03:55 +0100

    Load project content into IDE

--- src/components/IDE.tsx
+++ src/components/IDE.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouteComponentProps } from "@reach/router";
+import { useStoreState, useStoreActions } from "../store"
 
 import AceEditor from "react-ace";
 import Stage from "./Stage";
 import StageControls from "./StageControls";
 import InfoPanel from "./InfoPanel";
+import { LoadingState } from "../model/projects";
 
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-github";
@@ -14,13 +16,37 @@ interface IDEProps extends RouteComponentProps {
 }
 
 const IDE: React.FC<IDEProps> = ({projectId}) => {
+    const activeProject = useStoreState(state => state.activeProject);
+    const { activate, deactivate } = useStoreActions(actions => ({
+        activate: actions.activeProject.activate,
+        deactivate: actions.activeProject.deactivate,
+    }));
+
+    if (typeof projectId === "undefined")
+        throw Error("missing projectId for IDE");
+
+    useEffect(() => {
+        document.title = projectId;
+        if (activeProject.loadingState === LoadingState.Succeeded
+                && activeProject.project?.id !== projectId) {
+            deactivate();
+        }
+        if (activeProject.loadingState === LoadingState.Idle) {
+            activate(projectId);
+        }
+    });
+
+    const text = (activeProject.loadingState === LoadingState.Succeeded
+        ? activeProject.project!.codeText
+        : "# Loading...\n");
+
     return (
         <div className="ProjectIDE">
             <div className="CodeAndStage">
                 <AceEditor
                     mode="python"
                     theme="github"
-                    value="# will come from saved project in due course"
+                    value={text}
                     name="editor"
                     fontSize={16}
                     width="auto"
