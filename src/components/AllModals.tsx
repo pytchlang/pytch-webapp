c4d76acb8e7b0df00e5a893afa2ef63ab269ecb6
Ben North <ben@redfrontdoor.org>
2020-08-31 11:37:06 +0100

    AllModals: Add component

---
+++ src/components/AllModals.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CreateProjectModal } from "./CreateProjectModal";
+
+export const AllModals = () => {
+    return (
+        <>
+        <CreateProjectModal/>
+        </>
+    );
+}
