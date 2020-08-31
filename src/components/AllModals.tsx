4acbb536db3b6a0756d1d35c590e8d48d4e2205c
Ben North <ben@redfrontdoor.org>
2020-08-31 15:12:18 +0100

    AddAssetModal: Add to all-modals component

--- src/components/AllModals.tsx
+++ src/components/AllModals.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { CreateProjectModal } from "./CreateProjectModal";
+import AddAssetModal from "./AddAssetModal";
 
 export const AllModals = () => {
     return (
         <>
         <CreateProjectModal/>
+        <AddAssetModal/>
         </>
     );
 }
