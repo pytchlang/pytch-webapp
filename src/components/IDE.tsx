97260db7e0f81bb3446b09fc5aadc484c205d0c9
Ben North <ben@redfrontdoor.org>
2020-08-31 12:02:43 +0100

    Assemble components into IDE

--- src/components/IDE.tsx
+++ src/components/IDE.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
 
+import AceEditor from "react-ace";
+import Stage from "./Stage";
+import StageControls from "./StageControls";
+import InfoPanel from "./InfoPanel";
+
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-github";
+
 interface IDEProps extends RouteComponentProps {
     projectId?: string;
 }
 
 const IDE: React.FC<IDEProps> = ({projectId}) => {
     return (
-        <div>
-        Hello {projectId}.
+        <div className="ProjectIDE">
+            <div className="CodeAndStage">
+                <AceEditor
+                    mode="python"
+                    theme="github"
+                    value="# will come from saved project in due course"
+                    name="editor"
+                    fontSize={16}
+                    width="auto"
+                    height="auto"
+                    // TODO: onChange={setCodeText}
+                />
+                <div className="StageWithControls">
+                    <StageControls/>
+                    <Stage/>
+                </div>
+            </div>
+            <InfoPanel/>
         </div>
     )
 }
