cf3a7c323e0f92cab3e404f1a8f1f563a7a272d6
Ben North <ben@redfrontdoor.org>
2020-08-31 09:58:14 +0100

    Add a few more dummy projects

--- src/database/projects.ts
+++ src/database/projects.ts
@@ -3,6 +3,12 @@ import { IProjectSummary } from "../model/projects";
 const dummyProjects: Array<IProjectSummary> = [
   {id: "p1", name: "Bash the zombies", summary: "Run round splatting zombies."},
   {id: "p2", name: "Bike game"},
+  {id: 'p3', name: "Brag" },
+  {id: 'p4', name: "Pick your nose", summary: "Are you hungry?" },
+  {id: 'p5', name: "Round the world", summary: "Can you beat everyone?" },
+  {id: 'p6', name: "Bop-it", summary: "Bop it!  Twist it!  Pull it!" },
+  {id: 'p7', name: "Space invaders" },
+  {id: 'p8', name: "Pacperson" },
 ];
 
 const delay = (ms: number) => {
