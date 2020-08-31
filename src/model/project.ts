ddba8f60e3c3d4b2be08073a900cb8e1031dda59
Ben North <ben@redfrontdoor.org>
2020-08-31 12:41:11 +0100

    Handle loading progress for active project

--- src/model/project.ts
+++ src/model/project.ts
@@ -2,6 +2,7 @@ import { IAssetInProject } from "./asset";
 
 // TODO: Move LoadingState somewhere central?
 import { LoadingState } from "./projects";
+import { Action, action, Thunk, thunk } from "easy-peasy";
 
 export interface IProjectContent {
     id: string;
@@ -11,12 +12,23 @@ export interface IProjectContent {
 
 export type IMaybeProject = IProjectContent | null;
 
+// TODO: Eliminate dup'd code for loading-state?
 export interface IActiveProject {
     loadingState: LoadingState;
     project: IMaybeProject;
+    loadingPending: Action<IActiveProject>,
+    loadingSucceeded: Action<IActiveProject>,
 }
 
 export const activeProject: IActiveProject = {
     loadingState: LoadingState.Idle,
     project: null,
+
+    loadingPending: action((state) => {
+        state.loadingState = LoadingState.Pending;
+    }),
+
+    loadingSucceeded: action((state) => {
+        state.loadingState = LoadingState.Succeeded;
+    }),
 };
