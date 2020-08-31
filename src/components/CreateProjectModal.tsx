a62e30ea99f7efe9e5a09cb5187cf5827464c026
Ben North <ben@redfrontdoor.org>
2020-08-31 10:56:00 +0100

    CreateProjectModal: Add
    Does not yet actually add project-summary.

---
+++ src/components/CreateProjectModal.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+
+import { useStoreActions, useStoreState } from "../store";
+
+export const CreateProjectModal = () => {
+    const modalName = "create-project";
+
+    const [name, setName] = useState("");
+
+    const isShowing = useStoreState(state => state.modals.isShowing.get(modalName));
+    const hide = useStoreActions(actions => actions.modals.hide);
+
+    console.log("show?", isShowing);
+    const handleCreate = async () => {
+        console.log("creating project", name);
+        hide(modalName);
+    }
+
+    const handleChange = (evt: any) => { setName(evt.target.value); }
+    const handleClose = () => { hide(modalName); }
+
+    return (
+        <Modal
+            show={isShowing}
+            onHide={handleClose}
+            animation={false}
+        >
+        <Modal.Header closeButton>
+            <Modal.Title>Create a new project</Modal.Title>
+        </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group>
+                        <Form.Control
+                            type="text"
+                            value={name}
+                            onChange={handleChange}
+                            placeholder="Name for your new project"
+                        />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Cancel
+                </Button>
+                <Button
+                    disabled={name === ""}
+                    variant="primary"
+                    onClick={handleCreate}
+                >
+                    Create project
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default CreateProjectModal;
