7fd7abb13ffdf974d50f1fe68fbedc5ac36b9a8f
Ben North <ben@redfrontdoor.org>
2020-08-31 12:18:31 +0100

    Include active-project state in top-level model

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,12 +1,15 @@
 import { projectCollection, IProjectCollection } from "./projects";
 import { modals, IModals } from "./ui";
+import { activeProject, IActiveProject } from "./project";
 
 export interface IPytchAppModel {
     projectCollection: IProjectCollection;
+    activeProject: IActiveProject;
     modals: IModals;
 }
 
 export const pytchAppModel: IPytchAppModel = {
   projectCollection,
+  activeProject,
   modals,
 };
