246e189e937abd92ae551e26488f3da4e4bb1f09
Ben North <ben@redfrontdoor.org>
2020-08-31 11:12:32 +0100

    App: Show modals at top level

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import Welcome from "./components/Welcome";
 import ProjectList from "./components/ProjectList";
 
 import "./pytch.scss";
+import { AllModals } from './components/AllModals';
 
 function App() {
   return (
+    <>
     <Router className="App">
       <Welcome path="/"/>
       <ProjectList path="/my-projects/"/>
     </Router>
+    <AllModals/>
+    </>
   );
 }
 
