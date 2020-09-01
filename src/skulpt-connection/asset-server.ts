1ad9513710e969a71fcbe2cacae85aa4d88ea310
Ben North <ben@redfrontdoor.org>
2020-09-01 22:07:24 +0100

    Add initial attempt at asset-server

--- src/skulpt-connection/asset-server.ts
+++ src/skulpt-connection/asset-server.ts
@@ -1,5 +1,108 @@
-class AssetServer {
+import { assetDataServer } from "../database/projects";
+import { IAssetInProject } from "../model/asset";
 
+enum AssetKind {
+    Image,
+    Sound,
+}
+
+type ImageAsset = {
+    kind: AssetKind.Image;
+    image: HTMLImageElement;
+}
+
+type SoundAsset = {
+    kind: AssetKind.Sound;
+    audioData: ArrayBuffer;
+}
+
+type Asset = ImageAsset | SoundAsset;
+
+// Initial implementation re-fetches all assets every time.
+
+class AssetServer {
+    assetByName: Map<string, Promise<Asset>>;
+
+    constructor() {
+        this.assetByName = new Map<string, Promise<Asset>>();
+    }
+
+    rawLoadImage(url: string): Promise<HTMLImageElement> {
+        return new Promise((resolve, reject) => {
+            let img = new Image();
+            img.onload = (() => resolve(img));
+            // TODO: Anything useful for img.onerror?
+            img.src = url;
+        });
+    }
+
+    async fetchAsset(asset: IAssetInProject): Promise<Asset> {
+        const data = await assetDataServer.fetch(asset.id);
+        const mimeTopLevelType = asset.mimeType.split("/")[0];
+        switch (mimeTopLevelType) {
+            // TODO: Should check that we only ever create assets
+            // with these mime top-level types:
+            case "image": {
+                const blob = new Blob(
+                    [data],
+                    {type: asset.mimeType},
+                );
+
+                // TODO: Is the following right?  Is the URL finished with
+                // as soon as we've created the image from it?
+                const dataUrl = URL.createObjectURL(blob);
+                const image = await this.rawLoadImage(dataUrl);
+                URL.revokeObjectURL(dataUrl);
+                return {
+                    kind: AssetKind.Image,
+                    image: image,
+                };
+            }
+            case "audio": {
+                return {
+                    kind: AssetKind.Sound,
+                    audioData: data,
+                };
+            }
+            default:
+                throw Error(
+                    `unknown top-level mime type ${mimeTopLevelType}` +
+                      ` for asset "${asset.name}" with id ${asset.id}`
+                );
+        }
+    }
+
+    // TODO: Better name?
+    prefetch(assets: Array<IAssetInProject>) {
+        this.assetByName.clear();
+        assets.forEach((asset) => {
+            this.assetByName.set(asset.name, this.fetchAsset(asset));
+        });
+    }
+
+    async assetOfKind(name: string, kind: AssetKind, kindTag: string) {
+        const asset = await this.assetByName.get(name);
+        if (asset == null) {
+            throw Error(`no asset-promise for ${name}`);
+        }
+        if (asset.kind !== kind) {
+            throw Error(
+                `asset for "${name}" was ${asset.kind}` +
+                  ` but expecting ${kind}("${kindTag}")`
+            );
+        }
+        return asset;
+    }
+
+    async loadImage(name: string): Promise<HTMLImageElement> {
+        const asset = await this.assetOfKind(name, AssetKind.Image, "Image");
+        return (asset as ImageAsset).image;
+    }
+
+    async loadSoundData(name: string): Promise<ArrayBuffer> {
+        const asset = await this.assetOfKind(name, AssetKind.Sound, "Sound");
+        return (asset as SoundAsset).audioData;
+    }
 }
 
 export const assetServer = new AssetServer();
