13e285a931fbb67d293add8213f17bf57932c8b5
Ben North <ben@redfrontdoor.org>
2020-09-01 12:47:51 +0100

    BuildButton: Add skeleton component

---
+++ src/components/BuildButton.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const BuildButton = () => {
+    return (
+        <Button>BUILD</Button>
+    );
+}
+
+export default BuildButton;
