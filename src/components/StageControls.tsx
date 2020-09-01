6ab75f6b74be0c60f5ff051c7b3e157e360db956
Ben North <ben@redfrontdoor.org>
2020-09-01 12:48:05 +0100

    Use (pseudo-)buttons for stage controls

--- src/components/StageControls.tsx
+++ src/components/StageControls.tsx
@@ -2,15 +2,27 @@ import React from "react";
 import { Link } from "@reach/router";
 import Button from "react-bootstrap/Button";
 import { useStoreActions } from "../store";
+import BuildButton from "./BuildButton";
+
+const GreenFlag = () => {
+    return <div className="StageControlPseudoButton GreenFlag"/>;
+}
+
+const RedStop = () => {
+    return <div className="StageControlPseudoButton RedStop"/>;
+}
 
 const StageControls = () => {
     const save = useStoreActions(actions => actions.activeProject.requestCodeSyncToStorage);
     const handleSave = () => { save(); };
 
     return (
-        <div className="Stage">
-            Build / GREEN / RED /
-            <Button onClick={handleSave}>Save</Button> / <Link to="/my-projects/"><Button>MENU</Button></Link>
+        <div className="StageControls">
+            <BuildButton/>
+            <GreenFlag/>
+            <RedStop/>
+            <Button onClick={handleSave}>Save</Button>
+            <Link to="/my-projects/"><Button>MyStuff</Button></Link>
         </div>
     )
 };
