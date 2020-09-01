e077985dfd16301c91e30334df81b969aa57b5b4
Ben North <ben@redfrontdoor.org>
2020-09-01 16:26:19 +0100

    buildinRead: Add function

--- src/skulpt-connection/build.ts
+++ src/skulpt-connection/build.ts
@@ -1,1 +1,12 @@
 declare var Sk: any;
+
+const builtinRead = (fileName: string) => {
+    if (
+        Sk.builtinFiles === undefined ||
+        Sk.builtinFiles["files"][fileName] === undefined
+    ) {
+        throw Error(`File not found: '${fileName}'`);
+    }
+
+    return Sk.builtinFiles["files"][fileName];
+};
