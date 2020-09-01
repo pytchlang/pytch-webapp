3d81c15553f28d8c730a04477cae6129d46243d3
Ben North <ben@redfrontdoor.org>
2020-09-01 12:23:34 +0100

    Add (temporary) SAVE button to stage controls

--- src/components/StageControls.tsx
+++ src/components/StageControls.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "@reach/router";
+import Button from "react-bootstrap/Button";
+import { useStoreActions } from "../store";
 
 const StageControls = () => {
+    const save = useStoreActions(actions => actions.activeProject.requestCodeSyncToStorage);
+    const handleSave = () => { save(); };
+
     return (
         <div className="Stage">
-            Build / GREEN / RED / <Link to="/my-projects/">MENU</Link>
+            Build / GREEN / RED /
+            <Button onClick={handleSave}>Save</Button> / <Link to="/my-projects/"><Button>MENU</Button></Link>
         </div>
     )
 };
