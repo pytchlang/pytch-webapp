769917f18e8607ee8cabd6ce6c06d31495571ba8
Ben North <ben@redfrontdoor.org>
2020-09-04 13:58:49 +0100

    TutorialList: Load tutorials if not done yet

--- src/components/TutorialList.tsx
+++ src/components/TutorialList.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, createRef } from "react";
 import { RouteComponentProps } from "@reach/router"
 import NavBanner from "./NavBanner";
+import { useStoreActions, useStoreState } from "../store";
+import { SyncState } from "../model/project";
 import { ITutorialSummary } from "../model/tutorials";
 import Alert from "react-bootstrap/Alert";
 
@@ -34,6 +36,16 @@ const Tutorial: React.FC<TutorialProps> = ({ tutorial }) => {
 }
 
 const TutorialList: React.FC<RouteComponentProps> = (props) => {
+    const loadSummaries = useStoreActions(actions => actions.tutorialCollection.loadSummaries);
+    const syncState = useStoreState(state => state.tutorialCollection.syncState);
+    const available = useStoreState(state => state.tutorialCollection.available);
+
+    useEffect(() => {
+        if (syncState === SyncState.NoProject) {
+            loadSummaries();
+        }
+    });
+
     const paneRef: React.RefObject<HTMLDivElement> = React.createRef();
     useEffect(() => { paneRef.current!.focus(); })
     return (
@@ -41,6 +53,9 @@ const TutorialList: React.FC<RouteComponentProps> = (props) => {
         <NavBanner/>
         <div className="TutorialList" tabIndex={-1} ref={paneRef}>
         <h1>Tutorials</h1>
+        <ul>
+        {available.map((t) => <Tutorial key={t.slug} tutorial={t}/>)}
+        </ul>
         </div>
         </>
     );
