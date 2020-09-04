85cee1dc851aed42de8e444fc0212d2beed3c80a
Ben North <ben@redfrontdoor.org>
2020-09-04 13:55:20 +0100

    ITutorialCollection: Define interface

--- src/model/tutorials.ts
+++ src/model/tutorials.ts
@@ -1,4 +1,16 @@
+import { Action, action, Thunk, thunk } from "easy-peasy";
+import { SyncState } from "./project";
+
 export interface ITutorialSummary {
     slug: string;
     contentNodes: Array<Node>;
+}
+
+export interface ITutorialCollection {
+    syncState: SyncState;
+    available: Array<ITutorialSummary>;
+
+    setSyncState: Action<ITutorialCollection, SyncState>;
+    setAvailable: Action<ITutorialCollection, Array<ITutorialSummary>>;
+    loadSummaries: Thunk<ITutorialCollection>;
 }
