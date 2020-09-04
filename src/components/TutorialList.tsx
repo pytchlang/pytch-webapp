42eea46f346545f9b117330d7d0a3b5c57c6ae70
Ben North <ben@redfrontdoor.org>
2020-09-04 13:58:18 +0100

    Tutorial: Add initial version of component
    Does not yet do anything on click.

--- src/components/TutorialList.tsx
+++ src/components/TutorialList.tsx
@@ -1,6 +1,37 @@
 import React, { useEffect, createRef } from "react";
 import { RouteComponentProps } from "@reach/router"
 import NavBanner from "./NavBanner";
+import { ITutorialSummary } from "../model/tutorials";
+import Alert from "react-bootstrap/Alert";
+
+interface TutorialProps {
+    tutorial: ITutorialSummary;
+}
+
+const Tutorial: React.FC<TutorialProps> = ({ tutorial }) => {
+    const alertRef: React.RefObject<HTMLDivElement> = createRef();
+
+    useEffect(() => {
+        tutorial.contentNodes.forEach((ch) => {
+        alertRef.current!.appendChild(ch);
+        });
+    });
+
+    const launchTutorial = () => {
+        console.log("launch:", tutorial.slug);
+    }
+
+    return (
+        <li>
+        <Alert
+            onClick={launchTutorial}
+            className="TutorialCard"
+            variant="success"
+            ref={alertRef}
+        />
+        </li>
+    );
+}
 
 const TutorialList: React.FC<RouteComponentProps> = (props) => {
     const paneRef: React.RefObject<HTMLDivElement> = React.createRef();
