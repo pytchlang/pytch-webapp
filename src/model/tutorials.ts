ac6ee306028bfcd1a02d393f505f48b739336306
Ben North <ben@redfrontdoor.org>
2020-09-04 17:10:35 +0100

    createProjectFromTutorial(): Add thunk
    Creates project and activates it in IDE, but does not yet launch
    actual tutorial.

--- src/model/tutorials.ts
+++ src/model/tutorials.ts
@@ -1,6 +1,9 @@
 import { Action, action, Thunk, thunk } from "easy-peasy";
 import { SyncState } from "./project";
 import { allTutorialSummaries } from "../database/tutorials";
+import { createNewProject } from "../database/indexed-db";
+import { IPytchAppModel } from ".";
+import { navigate } from "@reach/router";
 
 export interface ITutorialSummary {
     slug: string;
@@ -14,6 +17,8 @@ export interface ITutorialCollection {
     setSyncState: Action<ITutorialCollection, SyncState>;
     setAvailable: Action<ITutorialCollection, Array<ITutorialSummary>>;
     loadSummaries: Thunk<ITutorialCollection>;
+
+    createProjectFromTutorial: Thunk<ITutorialCollection, string, {}, IPytchAppModel>;
 }
 
 export const tutorialCollection: ITutorialCollection = {
@@ -34,4 +39,16 @@ export const tutorialCollection: ITutorialCollection = {
         actions.setAvailable(summaries);
         actions.setSyncState(SyncState.Syncd);
     }),
+
+    createProjectFromTutorial: thunk(async (actions, tutorialSlug, helpers) => {
+        const addProject = helpers.getStoreActions().projectCollection.addProject;
+        const name = `My "${tutorialSlug}"`;
+        const summary = `This project is following the tutorial "${tutorialSlug}"`;
+        const project = await createNewProject(name, summary);
+
+        console.log("ITutorialCollection.createProjectFromTutorial(): about to add", project);
+        addProject(project);
+        console.log("ITutorialCollection.createProjectFromTutorial(): added", project);
+        await navigate(`/ide/${project.id}`);
+    })
 };
