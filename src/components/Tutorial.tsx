1441cf13d4950e463ace53266d7206a51c2b44bc
Ben North <ben@redfrontdoor.org>
2020-09-05 08:44:54 +0100

    TutorialNavigation: Add component

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -2,3 +2,36 @@ import React from "react";
 import { useStoreState, useStoreActions } from "../store";
 import { SyncState } from "../model/project";
 import RawElement from "./RawElement";
+
+interface TutorialNavigationProps {
+  kind: "prev" | "next";
+  toChapterIndex: number;
+}
+
+const TutorialNavigation = ({
+  kind,
+  toChapterIndex,
+}: TutorialNavigationProps) => {
+  const chapters = useStoreState(
+    (state) => state.activeTutorial.tutorial?.chapters
+  );
+
+  if (chapters == null) {
+    throw Error("no chapters to create navigation element");
+  }
+
+  const navigateToChapter = useStoreActions(
+    (actions) => actions.activeTutorial.navigateToChapter
+  );
+
+  const navigateToTargetChapter = () => navigateToChapter(toChapterIndex);
+
+  const toChapterTitle = chapters[toChapterIndex].title;
+
+  // TODO: Turn 'kind' into something more human-friendly.
+  return (
+    <span onClick={navigateToTargetChapter}>
+      {kind}: {toChapterTitle}
+    </span>
+  );
+};
