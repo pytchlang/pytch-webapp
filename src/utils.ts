7a4044f3fee2d5721573d35f73b27e34b08fa153
Ben North <ben@redfrontdoor.org>
2020-09-14 11:31:04 +0100

    delaySeconds(): Add ad-hoc testing helper

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export const withinApp = (url: string) => {
   return url[0] === "/" ? process.env.PUBLIC_URL + url : url;
 };
+
+// For ad-hoc UI testing:
+export const delaySeconds = (seconds: number) =>
+  new Promise((r) => setTimeout(r, 1000.0 * seconds));
