1f40dc3784865b1f482169108a257a4be002ad23
Ben North <ben@redfrontdoor.org>
2020-09-14 09:56:53 +0100

    Simplify "management of project list" tests
    Use pytchResetDatabase().

--- cypress/integration/project-list.spec.ts
+++ cypress/integration/project-list.spec.ts
@@ -2,9 +2,7 @@
 
 context("Management of project list", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/").then((window) => {
-      window.indexedDB.deleteDatabase("pytch");
-    });
+    cy.pytchResetDatabase();
     cy.contains("My projects").click();
     cy.location("pathname").should("include", "projects");
   });
