2e1d981bb0be4cbb6b3c07c1b9a1146388c3b14b
Ben North <ben@redfrontdoor.org>
2020-09-14 10:24:43 +0100

    Add "gives build error if typo" test
    Currently fails because we're not switching to error-pane on build
    error.  Will fix shortly.

--- cypress/integration/build-errors.spec.ts
+++ cypress/integration/build-errors.spec.ts
@@ -4,4 +4,13 @@ context("Build errors", () => {
   before(() => {
     cy.pytchExactlyOneProject("Project with build errors");
   });
+
+  it("gives build error if typo", () => {
+    cy.pytchBuildCode(`
+      import pytch
+      pront("oh no!")
+    `);
+
+    cy.pytchShouldShowErrorCard("NameError");
+  });
 });
