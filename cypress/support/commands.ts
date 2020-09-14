528fa1e1f03bc305da8cfdd97bd4ee3dd6a2bb48
Ben North <ben@redfrontdoor.org>
2020-09-14 09:57:27 +0100

    pytchExactlyOneProject(): Add command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -5,3 +5,13 @@ Cypress.Commands.add("pytchResetDatabase", () => {
     window.indexedDB.deleteDatabase("pytch");
   });
 });
+
+Cypress.Commands.add("pytchExactlyOneProject", (projectName: string) => {
+  cy.pytchResetDatabase();
+  cy.contains("My projects").click();
+  cy.contains("Create a new project").click();
+  cy.get("input[type=text]").type(projectName);
+  cy.get("button").contains("Create project").click();
+  cy.contains(projectName).click();
+  cy.contains("Images and sounds");
+});
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,5 +2,9 @@ declare namespace Cypress {
   interface Chainable {
     /** Reset the "pytch" storage database to be empty. */
     pytchResetDatabase(): Chainable<Element>;
+
+    /** Reset the "pytch" storage database, then navigate to a
+     * newly-created project with the given name. */
+    pytchExactlyOneProject(projectName: string): Chainable<Element>;
   }
 }
