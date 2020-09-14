6721c522be0f68189eea63b46587b8de263f4fe4
Ben North <ben@redfrontdoor.org>
2020-09-14 09:26:03 +0100

    Create project to run tests with

--- cypress/integration/run-project.spec.ts
+++ cypress/integration/run-project.spec.ts
@@ -1,4 +1,15 @@
 /// <reference types="cypress" />
 
 context("Build and run projects", () => {
+  before(() => {
+    cy.visit("http://localhost:3000/").then((window) => {
+      window.indexedDB.deleteDatabase("pytch");
+    });
+    cy.contains("My projects").click();
+    cy.contains("Create a new project").click();
+    cy.get("input[type=text]").type("Test project");
+    cy.get("button").contains("Create project").click();
+    cy.contains("Test project").click();
+    cy.contains("Images and sounds");
+  });
 });
