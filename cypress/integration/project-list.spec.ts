d2aaef4cbf65004d626bae4d2d073c9b848b4f0a
Ben North <ben@redfrontdoor.org>
2020-09-14 09:15:56 +0100

    Start project-list tests with empty DB

--- cypress/integration/project-list.spec.ts
+++ cypress/integration/project-list.spec.ts
@@ -1,4 +1,11 @@
 /// <reference types="cypress" />
 
 context("Management of project list", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/").then((window) => {
+      window.indexedDB.deleteDatabase("pytch");
+    });
+    cy.contains("My projects").click();
+    cy.location("pathname").should("include", "projects");
+  });
 });
