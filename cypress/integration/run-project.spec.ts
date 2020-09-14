4ca31876a82cdb36c51bf2ed5f15ff658230755b
Ben North <ben@redfrontdoor.org>
2020-09-14 09:57:47 +0100

    Simplify "build and run projects" tests
    Use pytchExactlyOneProject().

--- cypress/integration/run-project.spec.ts
+++ cypress/integration/run-project.spec.ts
@@ -4,15 +4,7 @@ import { IAceEditor } from "react-ace/lib/types";
 
 context("Build and run projects", () => {
   before(() => {
-    cy.visit("http://localhost:3000/").then((window) => {
-      window.indexedDB.deleteDatabase("pytch");
-    });
-    cy.contains("My projects").click();
-    cy.contains("Create a new project").click();
-    cy.get("input[type=text]").type("Test project");
-    cy.get("button").contains("Create project").click();
-    cy.contains("Test project").click();
-    cy.contains("Images and sounds");
+    cy.pytchExactlyOneProject("Test project");
   });
 
   const allSpaces = new RegExp("^ *$");
