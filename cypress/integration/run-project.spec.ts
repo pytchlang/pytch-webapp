914b90c8f84bff7d342a0e932ed70fa50d0ae55e
Ben North <ben@redfrontdoor.org>
2020-09-15 22:04:36 +0100

    Add "can re-build a project with sound" test
    Currently fails; fix to follow.

--- cypress/integration/run-project.spec.ts
+++ cypress/integration/run-project.spec.ts
@@ -13,4 +13,24 @@ context("Build and run projects", () => {
     cy.pytchShouldHaveBuiltWithoutErrors();
     cy.pytchStdoutShouldContain("Hello world\n");
   });
+
+  it("can re-build a project with sound", () => {
+    cy.pytchBuildCode(`
+      import pytch
+
+      class Siren(pytch.Sprite):
+        Costumes = []
+        Sounds = [("noise", "sine-1kHz-2s.mp3")]
+
+        def __init__(self):
+          pytch.Sprite.__init__(self)
+          print("making a Siren")
+    `);
+    cy.pytchShouldHaveBuiltWithoutErrors();
+    cy.pytchStdoutShouldContain("Siren");
+
+    cy.pytchBuild();
+    cy.pytchShouldHaveBuiltWithoutErrors();
+    cy.pytchStdoutShouldContain("Siren");
+  });
 });
