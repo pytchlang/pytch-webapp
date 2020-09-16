d07922d020cacf0d51e0d1c8bfa9a1b7f7606e72
Ben North <ben@redfrontdoor.org>
2020-09-16 09:43:04 +0100

    (Whitespace)

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -27,7 +27,7 @@ declare namespace Cypress {
     pytchShouldHaveBuiltWithoutErrors(): Chainable<Element>;
 
     /** Assert that the "Errors" pane is active and contains an error
-     * card containing the given fragment.  */
+     * card containing the given fragment. */
     pytchShouldShowErrorCard(match: ContentMatch): Chainable<Element>;
   }
 }
