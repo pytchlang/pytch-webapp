2940033af989216e0209d0500171725b2deb88aa
Ben North <ben@redfrontdoor.org>
2020-09-16 15:26:29 +0100

    Add "reports multiple simultaneous errors" test

--- cypress/integration/runtime-errors.ts
+++ cypress/integration/runtime-errors.ts
@@ -76,4 +76,32 @@ context("Runtime errors", () => {
     cy.pytchShouldShowErrorCard("division or modulo by zero");
     cy.pytchShouldHaveErrorStackTraceOfLength(4);
   });
+
+  it("reports multiple simultaneous errors", () => {
+    cy.pytchBuildCode(`
+      import pytch
+
+      class Banana(pytch.Sprite):
+        Costumes = []
+        @pytch.when_green_flag_clicked
+        def banana_trouble(self):
+          self.no_such_method()
+
+      class Cherry(pytch.Sprite):
+        Costumes = []
+        @pytch.when_green_flag_clicked
+        def cherry_trouble(self):
+          print(1 / 0)
+    `);
+    cy.pytchShouldHaveBuiltWithoutErrors();
+    cy.pytchGreenFlag();
+    cy.pytchShouldShowErrorContext("has stopped");
+    cy.get(".ErrorReportAlert").should("have.length", 2);
+    cy.pytchShouldShowErrorCard(
+      /Banana.*banana_trouble.*green-flag.*AttributeError.*no_such_method/
+    );
+    cy.pytchShouldShowErrorCard(
+      /Cherry.*cherry_trouble.*green-flag.*ZeroDivisionError/
+    );
+  });
 });
