cd622a89346199c07043ef5429fcd0ec5f91e351
Ben North <ben@redfrontdoor.org>
2020-09-20 10:04:32 +0100

    ConfirmProjectDeleteModal: Just return content
    This component is no longer responsible for the interaction with the
    user.  Remove the buttons.  Return just the content of the
    modal ("content" is header plus body, i.e., not the footer).  Accept a
    descriptor rather than the project name.

--- src/components/ConfirmProjectDeleteModal.tsx
+++ src/components/ConfirmProjectDeleteModal.tsx
@@ -1,56 +1,23 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { IDeleteProjectDescriptor } from "../model/ui";
 
-import { useStoreActions, useStoreState } from "../store";
+interface Props {
+  descriptor: IDeleteProjectDescriptor;
+}
 
-export const ConfirmProjectDeleteModal = () => {
-  const modalName = "confirm-project-delete";
-
-  const isShowing = useStoreState((state) =>
-    state.modals.isShowing.get(modalName)
-  );
-  const projectId = useStoreState(
-    (state) => state.userConfirmations.projectToDelete?.id ?? -1
-  );
-  const projectName = useStoreState(
-    (state) =>
-      state.userConfirmations.projectToDelete?.name ?? "SHOULD NEVER SEE THIS"
-  );
-
-  const { hide, requestDelete } = useStoreActions((actions) => ({
-    hide: actions.modals.hide,
-    requestDelete: actions.projectCollection.requestDeleteProject,
-  }));
-
-  const handleClose = () => {
-    hide(modalName);
-  };
-
-  const handleConfirm = () => {
-    requestDelete(projectId);
-    hide(modalName);
-  };
-
+export const ConfirmProjectDeleteModal: React.FC<Props> = ({ descriptor }) => {
   return (
-    <Modal show={isShowing} onHide={handleClose} animation={false}>
+    <>
       <Modal.Header closeButton>
         <Modal.Title>Delete project?</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <p>
-          Are you sure you want to delete project <strong>{projectName}</strong>
-          ?
+          Are you sure you want to delete project{" "}
+          <strong>{descriptor.projectName}</strong>?
         </p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button variant="danger" onClick={handleConfirm}>
-          DELETE
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    </>
   );
 };
