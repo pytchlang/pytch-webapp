414d0980de660e75eeb2f11567f14a1c31c63a0b
Ben North <ben@redfrontdoor.org>
2020-09-21 09:49:16 +0100

    ConfirmProjectDeleteModal: Split header/body

--- src/components/ConfirmProjectDeleteModal.tsx
+++ src/components/ConfirmProjectDeleteModal.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import { IDeleteProjectDescriptor } from "../model/ui";
 
-interface Props {
-  descriptor: IDeleteProjectDescriptor;
-}
-
-export const ConfirmProjectDeleteModal: React.FC<Props> = ({ descriptor }) => {
-  return (
-    <>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete project?</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
+export const ConfirmProjectDeleteModal = (
+  descriptor: IDeleteProjectDescriptor
+) => {
+  return {
+    header: <Modal.Title>Delete project?</Modal.Title>,
+    body: (
+      <>
         <p>
           Are you sure you want to delete project{" "}
           <strong>{descriptor.projectName}</strong>?
         </p>
-      </Modal.Body>
-    </>
-  );
+      </>
+    ),
+  };
 };
