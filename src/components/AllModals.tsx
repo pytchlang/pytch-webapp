03d74b17e05857d88b312b46ba2936db8f208661
Ben North <ben@redfrontdoor.org>
2020-09-21 12:32:50 +0100

    AllModals: Include RenameAssetModal

--- src/components/AllModals.tsx
+++ src/components/AllModals.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { CreateProjectModal } from "./CreateProjectModal";
 import AddAssetModal from "./AddAssetModal";
 import { ConfirmDangerousActionModal } from "./ConfirmDangerousActionModal";
+import { RenameAssetModal } from "./RenameAssetModal";
 
 export const AllModals = () => {
   return (
     <>
       <CreateProjectModal />
       <AddAssetModal />
+      <RenameAssetModal />
       <ConfirmDangerousActionModal />
     </>
   );
