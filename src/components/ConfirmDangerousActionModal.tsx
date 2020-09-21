3d9c419bff3e091bc1783cf8ce34d2208f46a0a1
Ben North <ben@redfrontdoor.org>
2020-09-21 09:38:45 +0100

    Show spinner when awaiting project delete
    Not quite done yet: Need to remove close button from header, but that
    currently comes as a lump as the 'main content'; will fix shortly.

--- src/components/ConfirmDangerousActionModal.tsx
+++ src/components/ConfirmDangerousActionModal.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
 import { useStoreActions, useStoreState } from "../store";
 import {
+  DangerousActionProgress,
   IDangerousActionDescriptor,
   IDeleteProjectDescriptor,
 } from "../model/ui";
@@ -29,7 +31,11 @@ export const ConfirmDangerousActionModal = () => {
   const invoke = useStoreActions(
     (actions) => actions.userConfirmations.invokeDangerousAction
   );
+
   const isShowing = actionToConfirm != null;
+  const awaitingAction =
+    actionToConfirm?.progress ===
+    DangerousActionProgress.AwaitingActionCompletion;
 
   const handleClose = () => dismiss();
   const handleConfirm = () => invoke();
@@ -44,9 +50,24 @@ export const ConfirmDangerousActionModal = () => {
         <Button variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
-        <Button variant="danger" onClick={handleConfirm}>
-          {actionText}
-        </Button>
+        {awaitingAction ? (
+          <Button disabled variant="danger" className="awaiting-action">
+            <span className="spacing-text">{actionText}</span>
+            <span className="spinner-container">
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+            </span>
+          </Button>
+        ) : (
+          <Button variant="danger" onClick={handleConfirm}>
+            {actionText}
+          </Button>
+        )}
       </Modal.Footer>
     </Modal>
   );
