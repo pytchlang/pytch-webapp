39f05895e1522d07d83939c893db7ca342ca0a01
Ben North <ben@redfrontdoor.org>
2020-09-22 14:28:40 +0100

    modalUserInteraction(): Add function

--- src/model/user-interactions/index.ts
+++ src/model/user-interactions/index.ts
@@ -8,6 +8,7 @@ import {
   thunk,
 } from "easy-peasy";
 import { IPytchAppModel } from "..";
+import { delaySeconds } from "../../utils";
 
 export type InteractionProgress =
   | { status: "not-happening" }
@@ -48,3 +49,53 @@ type AttemptActionFunction<Descriptor> = (
   actions: Actions<IPytchAppModel>,
   descriptor: Descriptor
 ) => Promise<void>;
+
+export function modalUserInteraction<TaskDescriptor, SpecificModel>(
+  attemptAction: AttemptActionFunction<TaskDescriptor>,
+  specificModel: SpecificModel
+): IModalUserInteraction<TaskDescriptor> & SpecificModel {
+  const baseModel: IModalUserInteraction<TaskDescriptor> = {
+    progress: { status: "not-happening" },
+    inputsReady: false,
+
+    isActive: computed((state) => state.progress.status !== "not-happening"),
+    isInteractable: computed((state) => {
+      const status = state.progress.status;
+      return status === "not-tried-yet" || status === "failed";
+    }),
+    attemptSucceeded: computed(
+      (state) => state.progress.status === "succeeded"
+    ),
+    maybeLastFailureMessage: computed((state) =>
+      state.progress.status === "failed" ? state.progress.message : null
+    ),
+
+    superLaunch: thunk((actions) => {
+      actions.setProgress({ status: "not-tried-yet" });
+      actions.setInputsReady(false);
+    }),
+    dismiss: thunk((actions) =>
+      actions.setProgress({ status: "not-happening" })
+    ),
+    attempt: thunk(async (actions, actionDescriptor, helpers) => {
+      try {
+        actions.setProgress({ status: "trying" });
+        await attemptAction(helpers.getStoreActions(), actionDescriptor);
+        actions.setProgress({ status: "succeeded" });
+        await delaySeconds(0.8);
+        actions.setProgress({ status: "not-happening" });
+      } catch (err) {
+        actions.setProgress({ status: "failed", message: err.message });
+      }
+    }),
+
+    setProgress: action((state, newProgress) => {
+      state.progress = newProgress;
+    }),
+    setInputsReady: action((state, inputsReady) => {
+      state.inputsReady = inputsReady;
+    }),
+  };
+
+  return Object.assign({}, baseModel, specificModel);
+}
