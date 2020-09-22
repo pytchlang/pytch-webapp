94f01997ddd40ccd6612b4b06b96c6a2437f0789
Ben North <ben@redfrontdoor.org>
2020-09-22 10:06:33 +0100

    AddAssetModal: Update to new mechanism

--- src/components/AddAssetModal.tsx
+++ src/components/AddAssetModal.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useStoreActions, useStoreState } from "../store";
+import { IRequestAddAssetPayload } from "../model/project";
 
 // Have to have this logic in the component to keep thunk payload as simple data?
 const readArraybuffer = (file: File): Promise<ArrayBuffer> => {
@@ -16,17 +17,19 @@ const readArraybuffer = (file: File): Promise<ArrayBuffer> => {
 
 const AddAssetModal = () => {
   const fileInputRef: React.RefObject<HTMLInputElement> = React.createRef();
-  const modalName = "add-asset";
 
-  const { isShowing } = useStoreState((state) => ({
-    isShowing: state.modals.isShowing.get("add-asset"),
-  }));
-  const { requestAddAsset, hide } = useStoreActions((actions) => ({
-    requestAddAsset: actions.activeProject.requestAddAssetAndSync,
-    hide: actions.modals.hide,
-  }));
+  const {
+    isActive,
+    isInteractable,
+    inputsReady,
+  } = useStoreState((state) => state.userConfirmations.addAssetInteraction);
 
-  const [haveFile, setHaveFile] = useState(false);
+  const { attempt, dismiss, setInputsReady } = useStoreActions((actions) => ({
+    attempt: actions.userConfirmations.addAssetInteraction.attempt,
+    dismiss: actions.userConfirmations.addAssetInteraction.dismiss,
+    setInputsReady:
+      actions.userConfirmations.addAssetInteraction.setInputsReady,
+  }));
 
   const handleAdd = async () => {
     // TODO: Should I check for non-null on these rather than "!"?
@@ -36,27 +39,21 @@ const AddAssetModal = () => {
     const fileBuffer = await readArraybuffer(file);
     console.log("got contents:", fileBuffer.byteLength);
 
-    await requestAddAsset({
+    const addDescriptor: IRequestAddAssetPayload = {
       name: file.name,
       mimeType: file.type,
       data: fileBuffer,
-    });
+    };
 
-    handleClose();
+    attempt(addDescriptor);
   };
 
-  const handleClose = () => {
-    setHaveFile(false); // Clear for next time.
-    hide(modalName);
-  };
-
-  const handleFileSelection = () => {
-    setHaveFile(true);
-  };
+  const handleClose = () => dismiss();
+  const handleFileSelection = () => setInputsReady(true);
 
   return (
-    <Modal show={isShowing} onHide={handleClose} animation={false}>
-      <Modal.Header closeButton>
+    <Modal show={isActive} onHide={handleClose} animation={false}>
+      <Modal.Header closeButton={isInteractable}>
         <Modal.Title>Add an image or sound</Modal.Title>
       </Modal.Header>
       <Modal.Body>
@@ -68,10 +65,18 @@ const AddAssetModal = () => {
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
+        <Button
+          disabled={!isInteractable}
+          variant="secondary"
+          onClick={handleClose}
+        >
           Cancel
         </Button>
-        <Button disabled={!haveFile} variant="primary" onClick={handleAdd}>
+        <Button
+          disabled={!(isInteractable && inputsReady)}
+          variant="primary"
+          onClick={handleAdd}
+        >
           Add to project
         </Button>
       </Modal.Footer>
