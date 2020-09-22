a78c5c9fa4c8e04f930dae4e5bb45f569679db81
Ben North <ben@redfrontdoor.org>
2020-09-22 11:01:19 +0100

    Update/remove stale comments

--- src/utils.ts
+++ src/utils.ts
@@ -2,13 +2,12 @@ export const withinApp = (url: string) => {
   return url[0] === "/" ? process.env.PUBLIC_URL + url : url;
 };
 
-// For ad-hoc UI testing:
 export const delaySeconds = (seconds: number) => {
   const timeoutMs = PYTCH_CYPRESS()["instantDelays"] ? 0 : 1000.0 * seconds;
   return new Promise((r) => setTimeout(r, timeoutMs));
 };
 
-// For testing hooks:
+// To allow testing to hook into various aspects of behaviour:
 const PYTCH_CYPRESS_default = {
   instantDelays: false,
 };
