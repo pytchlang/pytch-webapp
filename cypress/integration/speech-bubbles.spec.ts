35bfa32847e8a926ee8850b46fc00bdbd62f66cf
Ben North <ben@redfrontdoor.org>
2020-09-26 20:46:53 +0100

    Test that a speech bubble appears

--- cypress/integration/speech-bubbles.spec.ts
+++ cypress/integration/speech-bubbles.spec.ts
@@ -4,4 +4,21 @@ context("Speech bubbles", () => {
   before(() => {
     cy.pytchExactlyOneProject();
   });
+
+  it("shows a speech bubble", () => {
+    cy.pytchBuildCode(`
+      import pytch
+
+      class Rectangle(pytch.Sprite):
+        Costumes = ["red-rectangle-80-60.png"]
+
+        @pytch.when_green_flag_clicked
+        def go(self):
+          self.go_to_xy(0, 24)
+          self.say("Hello world!")
+    `);
+    cy.pytchShouldHaveBuiltWithoutErrors();
+    cy.pytchGreenFlag();
+    cy.get("div.speech-bubble").contains("Hello world!");
+  });
 });
