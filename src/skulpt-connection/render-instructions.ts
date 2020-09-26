9b119365fa487768843b9e7a640e3ac744902cbd
Ben North <ben@redfrontdoor.org>
2020-09-26 19:50:36 +0100

    RenderInstruction: Include speech-bubble in union

--- src/skulpt-connection/render-instructions.ts
+++ src/skulpt-connection/render-instructions.ts
@@ -14,5 +14,6 @@ export interface SpeechBubbleRenderInstruction {
   tip_y: number;
 }
 
-// In due course there will be other kinds.
-export type RenderInstruction = ImageRenderInstruction;
+export type RenderInstruction =
+  | ImageRenderInstruction
+  | SpeechBubbleRenderInstruction;
