98396657dd34dd2435b52b029ae207ea6c805fe7
Ben North <ben@redfrontdoor.org>
2020-09-29 09:10:12 +0100

    Introduce constants for stage half-width/-height

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 export const stageWidth: number = 480;
 export const stageHeight: number = 360;
+
+export const stageHalfWidth: number = 0.5 * stageWidth;
+export const stageHalfHeight: number = 0.5 * stageHeight;
