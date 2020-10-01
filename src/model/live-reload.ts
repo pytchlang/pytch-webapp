1da2f4656861f3801048e42322a2d2654cb4c17b
Ben North <ben@redfrontdoor.org>
2020-10-01 13:52:07 +0100

    Add comment re (lack of) websocket retry logic

--- src/model/live-reload.ts
+++ src/model/live-reload.ts
@@ -2,6 +2,11 @@ import { thunk, Thunk } from "easy-peasy";
 import { IPytchAppModel } from ".";
 import { liveReloadEnabled, liveReloadURL } from "../constants";
 
+// This is functionality for developers, so don't bother with retrying
+// the connection.  We will log a message if we hit an error, though, so
+// they can at least be prompted to make sure they're running the local
+// watch server.
+
 export interface IReloadServer {
   webSocket: WebSocket | null;
   maybeConnect: Thunk<IReloadServer, void, any, IPytchAppModel>;
