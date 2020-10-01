66f1b138f15873fc958c7fa366bb874072ab4a84
Ben North <ben@redfrontdoor.org>
2020-10-01 13:34:18 +0100

    liveReloadURL: Define constant

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const stageHeight: number = 360;
 
 export const stageHalfWidth: number = 0.5 * stageWidth;
 export const stageHalfHeight: number = 0.5 * stageHeight;
+
+export const liveReloadURL = "ws://127.0.0.1:4111/";
