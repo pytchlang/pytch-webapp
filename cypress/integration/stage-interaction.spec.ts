c45d6def99cba5b8bd33597654316ef137d6a836
Ben North <ben@redfrontdoor.org>
2020-10-02 19:51:58 +0100

    Test mouse clicks reach sprite after BUILD
    Currently fails; will fix shortly.

--- cypress/integration/stage-interaction.spec.ts
+++ cypress/integration/stage-interaction.spec.ts
@@ -4,4 +4,28 @@ context("Interaction with the stage", () => {
   beforeEach(() => {
     cy.pytchExactlyOneProject();
   });
+
+  it("directs mouse clicks to stage after build", () => {
+    cy.pytchBuildCode(`
+      import pytch
+
+      class Monitor(pytch.Sprite):
+        Costumes = ["red-rectangle-80-60.png"]
+        @pytch.when_this_sprite_clicked
+        def say_hello(self):
+          print("hello")
+    `);
+
+    // Just build, to emulate user interaction just with "BUILD" button,
+    // rather than the user then clicking on the Errors tab to check for
+    // errors, which is what pytchShouldHaveBuiltWithoutErrors() would
+    // do.
+    cy.pytchBuild();
+
+    // The sprite is in the centre of the stage, so should receive this
+    // click:
+    cy.focused().click("center");
+
+    cy.pytchStdoutShouldContain("hello\n");
+  });
 });
