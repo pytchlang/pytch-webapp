5eb143cd6f69b78d4ddf6b423ba8301f00144999
Ben North <ben@redfrontdoor.org>
2020-10-12 14:45:27 +0100

    Enable Ace auto-completion in general

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
 import { useStoreState, useStoreActions } from "../store";
 import { SyncState } from "../model/project";
 import { setAceController } from "../skulpt-connection/code-editor";
