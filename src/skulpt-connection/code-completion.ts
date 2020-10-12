e3ffbb70478cecf51dc1225c0e0e92b1b50a03bf
Ben North <ben@redfrontdoor.org>
2020-10-12 14:42:47 +0100

    candidateFromSymbol: Add helper function

--- src/skulpt-connection/code-completion.ts
+++ src/skulpt-connection/code-completion.ts
@@ -2,3 +2,11 @@
 //
 // Only complete "pytch." and "self.", with hard-coded list of options
 // based on the public module functions and base-class methods.
+
+const candidateFromSymbol = (meta: string) => (symbol: string) => {
+  return {
+    name: symbol,
+    value: symbol,
+    meta: meta,
+  };
+};
