e1e446c48d229d173798d037c7796bc31b5b410e
Ben North <ben@redfrontdoor.org>
2020-10-12 11:16:50 +0100

    Add "can navigate through tutorial" test

--- cypress/integration/tutorials.spec.ts
+++ cypress/integration/tutorials.spec.ts
@@ -4,4 +4,21 @@ context("Interact with a tutorial", () => {
   beforeEach(() => {
     cy.pytchProjectFollowingTutorial();
   });
+
+  it("can navigate through tutorial", () => {
+    cy.contains("Next:").click();
+    cy.get(".ToC > li.active")
+      .should("have.length", 1)
+      .contains("Make the playing area");
+    cy.contains("Back:").click();
+    cy.get(".ToC > li.active")
+      .should("have.length", 1)
+      .contains("Make a Pong-like game");
+    cy.contains("Next:").click();
+    cy.contains("Next:").click();
+    cy.contains("Next:").click();
+    cy.get(".ToC > li.active")
+      .should("have.length", 1)
+      .contains("Add the ball");
+  });
 });
