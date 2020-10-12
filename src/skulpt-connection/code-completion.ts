a15a85305ef9c0950048d42c6746891523d7f8b3
Ben North <ben@redfrontdoor.org>
2020-10-12 14:45:16 +0100

    PytchAceAutoCompleter: Add class

--- src/skulpt-connection/code-completion.ts
+++ src/skulpt-connection/code-completion.ts
@@ -3,6 +3,8 @@
 // Only complete "pytch." and "self.", with hard-coded list of options
 // based on the public module functions and base-class methods.
 
+import { IAceEditor } from "react-ace/lib/types";
+
 const candidateFromSymbol = (meta: string) => (symbol: string) => {
   return {
     name: symbol,
@@ -50,3 +52,33 @@ const completionsActorMethod = [
   "move_backward_layers",
   "switch_backdrop",
 ].map(candidateFromSymbol("Sprite/Stage method"));
+
+export class PytchAceAutoCompleter {
+  // TODO: Proper types for the remaining arguments.
+  getCompletions(
+    _editor: IAceEditor,
+    session: any,
+    pos: any,
+    prefix: string,
+    callback: any
+  ) {
+    const cursorLine = session.getLine(pos.row);
+    const lineHead = cursorLine.substring(0, pos.column);
+
+    if (!lineHead.endsWith(prefix)) {
+      // TODO: What's the right way to report this error to Ace?
+      callback(null, []);
+    }
+
+    const prePrefixLength = lineHead.length - prefix.length;
+    const prePrefix = lineHead.substring(0, prePrefixLength);
+
+    const candidates = prePrefix.endsWith("pytch.")
+      ? completionsPytchBuiltin
+      : prePrefix.endsWith("self.")
+      ? completionsActorMethod
+      : [];
+
+    callback(null, candidates);
+  }
+}
