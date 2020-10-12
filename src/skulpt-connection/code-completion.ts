454fad42c24bc638cca1c10e11f56710c0377024
Ben North <ben@redfrontdoor.org>
2020-10-12 14:42:53 +0100

    Add hard-coded lists of completions
    For "pytch.*" and "self.*" (assuming on Sprite or Stage).

--- src/skulpt-connection/code-completion.ts
+++ src/skulpt-connection/code-completion.ts
@@ -10,3 +10,43 @@ const candidateFromSymbol = (meta: string) => (symbol: string) => {
     meta: meta,
   };
 };
+
+const completionsPytchBuiltin = [
+  "Sprite",
+  "Stage",
+  "when_green_flag_clicked",
+  "when_I_receive",
+  "when_key_pressed",
+  "when_I_start_as_a_clone",
+  "when_this_sprite_clicked",
+  "when_stage_clicked",
+  "create_clone_of",
+  "broadcast",
+  "broadcast_and_wait",
+  "stop_all_sounds",
+  "wait_seconds",
+  "key_is_pressed",
+].map(candidateFromSymbol("pytch built-in"));
+
+const completionsActorMethod = [
+  "start_sound",
+  "play_sound_until_done",
+  "go_to_xy",
+  "get_x",
+  "set_x",
+  "change_x",
+  "get_y",
+  "set_y",
+  "change_y",
+  "set_size",
+  "show",
+  "hide",
+  "switch_costume",
+  "touching",
+  "delete_this_clone",
+  "move_to_front_layer",
+  "move_to_back_layer",
+  "move_forward_layers",
+  "move_backward_layers",
+  "switch_backdrop",
+].map(candidateFromSymbol("Sprite/Stage method"));
