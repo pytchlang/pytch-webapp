32d6d09ee307b5390413f7ca609d2e08ac57cb52
Ben North <ben@redfrontdoor.org>
2020-10-17 08:03:14 +0100

    DownloadZipfileModal: Add draft component

---
+++ src/components/DownloadZipfileModal.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useStoreActions, useStoreState } from "../store";
+import { failIfNull } from "../utils";
+import { MaybeErrorOrSuccessReport } from "./MaybeErrorOrSuccessReport";
+
+export const DownloadZipfileModal = () => {
+  const {
+    isActive,
+    inputsReady,
+    isInteractable,
+    attemptSucceeded,
+    maybeLastFailureMessage,
+    fileContents,
+  } = useStoreState(
+    (state) => state.userConfirmations.downloadZipfileInteraction
+  );
+
+  const { dismiss, attempt } = useStoreActions(
+    (actions) => actions.userConfirmations.downloadZipfileInteraction
+  );
+
+  const handleClose = () => dismiss();
+  const handleDownload = () =>
+    attempt({
+      data: failIfNull(
+        fileContents,
+        "cannot do download if file contents null"
+      ),
+    });
+
+  return (
+    <Modal
+      className="DownloadZipfile"
+      show={isActive}
+      onHide={handleClose}
+      animation={false}
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title>
+          {inputsReady ? "Download zipfile" : "Preparing zipfile..."}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>TODO: Spinner while preparing.</p>
+        <MaybeErrorOrSuccessReport
+          messageWhenSuccess="Downloading!"
+          attemptSucceeded={attemptSucceeded}
+          maybeLastFailureMessage={maybeLastFailureMessage}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          disabled={!isInteractable}
+          variant="secondary"
+          onClick={handleClose}
+        >
+          Cancel
+        </Button>
+        <Button
+          disabled={!(isInteractable && inputsReady)}
+          variant="primary"
+          onClick={handleDownload}
+        >
+          Download
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
