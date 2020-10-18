62244cf97895393481db036db8f308bcf3ad559d
Ben North <ben@redfrontdoor.org>
2020-10-18 07:53:23 +0100

    Add text input to modal for filename

--- src/components/DownloadZipfileModal.tsx
+++ src/components/DownloadZipfileModal.tsx
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import Button from "react-bootstrap/Button";
 import Spinner from "react-bootstrap/Spinner";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 import { useStoreActions, useStoreState } from "../store";
 import { failIfNull } from "../utils";
 import { MaybeErrorOrSuccessReport } from "./MaybeErrorOrSuccessReport";
@@ -20,9 +21,17 @@ export const DownloadZipfileModal = () => {
     (state) => state.userConfirmations.downloadZipfileInteraction
   );
 
-  const { dismiss, attempt } = useStoreActions(
+  const { dismiss, attempt, setFilename, setInputsReady } = useStoreActions(
     (actions) => actions.userConfirmations.downloadZipfileInteraction
   );
+
+  const inputRef: React.RefObject<HTMLInputElement> = React.createRef();
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const value = evt.target.value;
+    setInputsReady(value !== "" && fileContents != null);
+    setFilename(value);
+  };
 
   const handleClose = () => dismiss();
   const handleDownload = () =>
@@ -54,6 +63,16 @@ export const DownloadZipfileModal = () => {
           ) : null}
           {!inputsReady ? <Spinner animation="border" /> : null}
         </div>
+
+        <Form>
+          <Form.Control
+            type="text"
+            value={filename}
+            onChange={handleChange}
+            tabIndex={-1}
+            ref={inputRef}
+          />
+        </Form>
 
         <MaybeErrorOrSuccessReport
           messageWhenSuccess="Downloading!"
