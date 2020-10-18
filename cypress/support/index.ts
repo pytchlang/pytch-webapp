a0fe4713647e441036a7c68db8c180143e7f5301
Ben North <ben@redfrontdoor.org>
2020-10-18 08:29:51 +0100

    cartesianProduct(): Add test helper function

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,1 +1,14 @@
 require("./commands");
+
+export function cartesianProduct<T1, T2>(
+  xs1: Array<T1>,
+  xs2: Array<T2>
+): Array<T1 & T2> {
+  let xs12: Array<T1 & T2> = [];
+  xs1.forEach((x1) => {
+    xs2.forEach((x2) => {
+      xs12.push(Object.assign({}, x1, x2));
+    });
+  });
+  return xs12;
+}
