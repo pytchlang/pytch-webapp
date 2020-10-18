81a131593d009259e12965a32aa55efb32724740
Ben North <ben@redfrontdoor.org>
2020-10-18 15:28:02 +0100

    Remove unused imports, variable

--- src/components/EditorWebSocketInfo.tsx
+++ src/components/EditorWebSocketInfo.tsx
@@ -1,18 +1,16 @@
-import React, { createRef, RefObject, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useStoreActions, useStoreState } from "../store";
-import { ancestorHavingClass } from "../utils";
 
 const EditorWebSocketInfo = () => {
   const text = useStoreState((state) => state.editorWebSocketLog.text);
   const { maybeConnect } = useStoreActions((actions) => actions.reloadServer);
-  const contentDivRef: RefObject<HTMLDivElement> = createRef();
 
   useEffect(() => {
     maybeConnect();
   });
 
   return (
-    <div ref={contentDivRef} className="EditorWebSocketInfo">
+    <div className="EditorWebSocketInfo">
       <pre>{text}</pre>
     </div>
   );
