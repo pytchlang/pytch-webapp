1ff356a8461052aa852a3cb9ddad9e27768c71cc
Ben North <ben@redfrontdoor.org>
2020-10-19 10:32:43 +0100

    IIDELayout, ideLayout: Define model slice
    Default to the current layout.

--- src/model/ui.tsx
+++ src/model/ui.tsx
@@ -22,6 +22,18 @@ import {
 
 export type IDELayoutKind = "wide-info-pane" | "tall-code-editor";
 
+export interface IIDELayout {
+  kind: IDELayoutKind;
+  setKind: Action<IIDELayout, IDELayoutKind>;
+}
+
+export const ideLayout: IIDELayout = {
+  kind: "wide-info-pane",
+  setKind: action((state, kind) => {
+    state.kind = kind;
+  }),
+};
+
 /** General modal dialog support. */
 
 type IsShowingByName = Map<string, boolean>;
