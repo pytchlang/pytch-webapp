aebd0bdd94c7ad5dba2bf34c99fadc32bf5061e1
Ben North <ben@redfrontdoor.org>
2020-10-26 07:50:38 +0000

    Add "can use print as a function" test

--- cypress/integration/python3.spec.ts
+++ cypress/integration/python3.spec.ts
@@ -4,4 +4,13 @@ context("Python 3 features", () => {
   before(() => {
     cy.pytchExactlyOneProject();
   });
+
+  it("can use print as a function", () => {
+    cy.pytchBuildCode(`
+      import pytch
+      print("Hello", "world")
+    `);
+    cy.pytchShouldHaveBuiltWithoutErrors();
+    cy.pytchStdoutShouldContain("Hello world\n");
+  });
 });
