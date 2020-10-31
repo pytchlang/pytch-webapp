ee96bb8e8b688a64d2b73ea150a35f8baeb37d7d
Ben North <ben@redfrontdoor.org>
2020-10-31 10:09:20 +0000

    ICreateProjectDescriptor: Add interface
    And ICreateProjectBase type.

--- src/model/user-interactions/create-project.ts
+++ src/model/user-interactions/create-project.ts
@@ -1,3 +1,9 @@
 import { Action, action, Actions, Thunk, thunk } from "easy-peasy";
 import { IPytchAppModel } from "..";
 import { IModalUserInteraction, modalUserInteraction } from ".";
+
+interface ICreateProjectDescriptor {
+  name: string;
+}
+
+type ICreateProjectBase = IModalUserInteraction<ICreateProjectDescriptor>;
