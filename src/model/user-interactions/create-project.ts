4b8b56e8e6fc5c57fdf66dcaad74793441ab0f60
Ben North <ben@redfrontdoor.org>
2020-10-31 10:10:36 +0000

    createProjectSpecific: Define model slice

--- src/model/user-interactions/create-project.ts
+++ src/model/user-interactions/create-project.ts
@@ -26,3 +26,20 @@ const attemptCreate = async (
   const newProject = await createNewProject(descriptor.name);
   await navigate(withinApp(`/ide/${newProject.id}`));
 };
+
+const createProjectSpecific: ICreateProjectSpecific = {
+  name: "",
+  setName: action((state, name) => {
+    state.name = name;
+  }),
+
+  refreshInputsReady: thunk((actions, _payload, helpers) => {
+    const state = helpers.getState();
+    actions.setInputsReady(state.name !== "");
+  }),
+
+  launch: thunk((actions, _payload, helpers) => {
+    actions.setName("");
+    actions.superLaunch();
+  }),
+};
