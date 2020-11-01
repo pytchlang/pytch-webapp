5c6d18a613d0bce7e40298766e12f2227ac6cbe3
Ben North <ben@redfrontdoor.org>
2020-11-01 08:57:41 +0000

    ProjectAssetList: Use new syncState

--- src/components/ProjectAssetList.tsx
+++ src/components/ProjectAssetList.tsx
@@ -103,16 +103,16 @@ const ProjectAssetList = () => {
 
   const showAddModal = () => showModal();
 
-  switch (syncState) {
-    case SyncState.SyncNotStarted:
-      return <div>Assets will load shortly....</div>;
-    case SyncState.SyncingFromBackEnd:
+  switch (syncState.loadState) {
+    case "pending":
       return <div>Assets loading....</div>;
-    case SyncState.Error:
+    case "failed":
+      // TODO: Handle more usefully
       return <div>Assets failed to load, oh no</div>;
-    case SyncState.SyncingToBackEnd:
-    case SyncState.Syncd:
-      break; // Handle normal cases below.
+    case "succeeded":
+      break; // Handle normal case below.
+    default:
+      throw new Error(`unknown loadState "${syncState.loadState}"`);
   }
 
   const assets = failIfNull(
