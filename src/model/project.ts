24c950bc7be76cd33525495ff8247a4429bb17d8
Ben North <ben@redfrontdoor.org>
2020-11-01 08:51:47 +0000

    (Note query re SyncState type)

--- src/model/project.ts
+++ src/model/project.ts
@@ -58,6 +58,7 @@ export interface ILoadSaveStatus {
   saveState: SyncRequestState;
 }
 
+// Used elsewhere but maybe those places needed review too?
 export enum SyncState {
   SyncNotStarted,
   SyncingFromBackEnd,
