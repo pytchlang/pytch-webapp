f8bc3f87454b61382bffdbf4755aa248b0fbe2f9
Ben North <ben@redfrontdoor.org>
2020-11-02 09:29:23 +0000

    Remove stale TODO
    I think it's correct that the component re-renders when its
    state (which includes the code content) changes.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -29,13 +29,6 @@ const ReadOnlyOverlay = () => {
   return null;
 };
 
-// TODO: This keeps re-rendering completely when the code text changes.
-// Is there a way to be able to force content into the editor, e.g., for
-// "loading..." message and initial content when loaded?  Depend on a
-// piece of state like "exogenousText", perhaps with also "exogenousSeqNum"
-// to force re-display of the same content?  Could keep having onChange
-// update the state so other components can get at it.
-//
 const CodeEditor = () => {
   const { codeTextOrPlaceholder, syncState } = useStoreState((state) => ({
     codeTextOrPlaceholder: state.activeProject.codeTextOrPlaceholder,
