cb121552d38f76c4219e5242a36105ef6a371a55
Ben North <ben@redfrontdoor.org>
2020-11-02 10:19:53 +0000

    Mark code as "saved" when loading new project

--- src/model/project.ts
+++ src/model/project.ts
@@ -322,6 +322,7 @@ export const activeProject: IActiveProject = {
     batch(() => {
       storeActions.standardOutputPane.clear();
       storeActions.errorReportList.clear();
+      actions.noteCodeSaved();
     });
 
     try {
