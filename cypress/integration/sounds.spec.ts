386e5e187f89c687ca2ca4a6b6afefe2f9ab6aa4
Ben North <ben@redfrontdoor.org>
2020-11-14 07:58:51 +0000

    Configure test project with silence asset

--- cypress/integration/sounds.spec.ts
+++ cypress/integration/sounds.spec.ts
@@ -1,4 +1,9 @@
 /// <reference types="cypress" />
 
 context("Playing sounds", () => {
+  const silenceAsset = [{ name: "silence-500ms.mp3", mimeType: "audio/mpeg" }];
+
+  before(() => {
+    cy.pytchExactlyOneProject(silenceAsset);
+  });
 });
