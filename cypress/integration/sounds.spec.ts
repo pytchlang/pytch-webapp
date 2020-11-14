62791fe852672ad45be182fac403c01ab4a1623f
Ben North <ben@redfrontdoor.org>
2020-11-14 07:59:14 +0000

    Test that execution continues after playing sound
    Currently fails; will fix shortly.

--- cypress/integration/sounds.spec.ts
+++ cypress/integration/sounds.spec.ts
@@ -6,4 +6,22 @@ context("Playing sounds", () => {
   before(() => {
     cy.pytchExactlyOneProject(silenceAsset);
   });
+
+  it("resumes after play-sound-until-done", () => {
+    cy.pytchBuildCode(`
+      import pytch
+
+      class Quiet(pytch.Sprite):
+        Costumes = []
+        Sounds = ["silence-500ms.mp3"]
+        @pytch.when_key_pressed("a")
+        def emit_silence(self):
+          self.play_sound_until_done("silence-500ms")
+          print("finished being silent")
+    `);
+
+    cy.pytchShouldHaveBuiltWithoutErrors();
+    cy.pytchSendKeysToProject("a");
+    cy.pytchStdoutShouldContain("finished");
+  });
 });
