832333f66f49708632bb95be76cb8ab26ca30b38
Ben North <ben@redfrontdoor.org>
2021-01-09 11:15:43 +0000

    VerticalResizer: Use relevant state and actions

--- src/components/VerticalResizer.tsx
+++ src/components/VerticalResizer.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useStoreState, useStoreActions } from "../store";
 
 const VerticalResizer = () => {
+  const resizeState = useStoreState(
+    (state) => state.ideLayout.stageVerticalResizeState
+  );
+  const {
+    initiateVerticalResize,
+    completeVerticalResize,
+    setStageDisplayHeight,
+  } = useStoreActions((actions) => actions.ideLayout);
+
 };
 
 export default VerticalResizer;
