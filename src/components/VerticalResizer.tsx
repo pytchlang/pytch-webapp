f7f1577025e19ee0800609a04e67424b6a2ed7ab
Ben North <ben@redfrontdoor.org>
2021-01-09 11:23:26 +0000

    Vert.Resizer: onTouchMove(), onMouseMove(): Add

--- src/components/VerticalResizer.tsx
+++ src/components/VerticalResizer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { stageHeight } from "../constants";
 import { useStoreState, useStoreActions } from "../store";
 
 const VerticalResizer = () => {
@@ -19,6 +20,29 @@ const VerticalResizer = () => {
       touches: [{ clientX: event.clientX, clientY: event.clientY }],
     });
     onTouchStart(eventWithTouches);
+  };
+
+  const onTouchMove = (event: any) => {
+    if (resizeState == null) return;
+
+    const eventY = event.touches[0].clientY;
+    const rawNewHeight =
+      resizeState.dragStartHeight + eventY - resizeState.dragStartY;
+
+    // Clamp to reasonable range.
+    const newHeight = Math.min(
+      Math.round(1.25 * stageHeight),
+      Math.max(Math.round(0.5 * stageHeight), rawNewHeight)
+    );
+
+    setStageDisplayHeight(newHeight);
+  };
+
+  const onMouseMove = (event: any) => {
+    const eventWithTouches = Object.assign({}, event, {
+      touches: [{ clientX: event.clientX, clientY: event.clientY }],
+    });
+    onTouchMove(eventWithTouches);
   };
 };
 
