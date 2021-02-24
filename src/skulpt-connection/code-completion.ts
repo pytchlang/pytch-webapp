b1cf2423328d57295860d3d2b10a6e77adc465f6
Ben North <ben@redfrontdoor.org>
2021-02-24 07:47:04 +0000

    Gather completion info via Python function

--- src/skulpt-connection/code-completion.ts
+++ src/skulpt-connection/code-completion.ts
@@ -5,6 +5,8 @@
 
 import { IAceEditor } from "react-ace/lib/types";
 
+declare var Sk: any;
+
 interface IAceCompletion {
   caption: string;
   value: string;
@@ -24,6 +26,57 @@ const completionsFromPyList = (meta: string | null, lst: any) =>
 
 let pytchCompletions: Array<IAceCompletion>;
 let actorCompletions: Array<IAceCompletion>;
+
+// Invoke the Python function _user_facing_completions() and
+// use the provided info on the user-facing attributes of pytch,
+// Actor, Sprite, and Stage.
+//
+(() => {
+  Sk.configure({});
+  const pyStr = (s: string) => new Sk.builtin.str(s);
+  const sUserFacingCompletions = pyStr("_user_facing_completions");
+  const sPytch = pyStr("pytch");
+  const sActor = pyStr("Actor");
+  const sSprite = pyStr("Sprite");
+  const sStage = pyStr("Stage");
+
+  const pyMod = Sk.builtin.__import__("pytch", {}, {}, [], -1);
+  const pyCompletionsInfoFun = Sk.builtin.getattr(
+    pyMod,
+    sUserFacingCompletions
+  );
+  const pyCompletionsInfo = pyCompletionsInfoFun.tp$call([], {});
+
+  // Return value is (completions, attrsWithoutDocs); ignore the latter.
+  const pyCompletionsByKind = pyCompletionsInfo.v[0];
+
+  // Set top-level var holding completions for "pytch.":
+  //
+  pytchCompletions = completionsFromPyList(
+    null,
+    pyCompletionsByKind.mp$subscript(sPytch)
+  );
+
+  // Set top-level var holding completions for "self.":
+  //
+  const baseActorCompletions = completionsFromPyList(
+    "[Spr/Stg]",
+    pyCompletionsByKind.mp$subscript(sActor)
+  );
+  const spriteCompletions = completionsFromPyList(
+    "[Spr]",
+    pyCompletionsByKind.mp$subscript(sSprite)
+  );
+  const stageCompletions = completionsFromPyList(
+    "[Stg]",
+    pyCompletionsByKind.mp$subscript(sStage)
+  );
+  actorCompletions = [
+    ...baseActorCompletions,
+    ...spriteCompletions,
+    ...stageCompletions,
+  ];
+})();
 
 export class PytchAceAutoCompleter {
   // TODO: Proper types for the remaining arguments.
