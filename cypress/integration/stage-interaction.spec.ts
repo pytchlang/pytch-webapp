842ef9c86d5d1e4cd26ef7a326c8f5bea6ed9950
Ben North <ben@redfrontdoor.org>
2021-03-03 08:27:07 +0000

    Add tests for click coord computation
    Currently fails when stage is anything other than default size.  Will
    fix shortly.

--- cypress/integration/stage-interaction.spec.ts
+++ cypress/integration/stage-interaction.spec.ts
@@ -62,4 +62,63 @@ context("Interaction with the stage", () => {
       cy.pytchStdoutShouldEqual(`${spec.extraOutput}hello\nhello\n`);
     })
   );
+
+  [
+    { label: "default-size", setupFun: () => {} },
+    { label: "max-size", setupFun: () => cy.pytchDragStageDivider(200) },
+    { label: "min-size", setupFun: () => cy.pytchDragStageDivider(-200) },
+  ].forEach((spec) =>
+    it(`computes click coords (${spec.label} stage)`, () => {
+      spec.setupFun();
+
+      cy.pytchBuildCode(`
+        import pytch
+        locations = [(x, y) for x in [-240, 240] for y in [-180, 180]]
+        class Monitor(pytch.Sprite):
+          Costumes = ["red-rectangle-80-60.png"]
+          @pytch.when_green_flag_clicked
+          def shrink(self):
+            self.set_size(0.25)
+            self.n_clicks = 0
+            self.next_location_idx = 0
+          @pytch.when_this_sprite_clicked
+          def say_hello(self):
+            self.n_clicks += 1
+            print("hello", self.n_clicks, self.get_x(), self.get_y())
+          @pytch.when_key_pressed("m")
+          def next_location(self):
+            self.go_to_xy(*locations[self.next_location_idx])
+            self.next_location_idx += 1
+            print("moved", self.n_clicks)
+    `);
+
+      cy.pytchGreenFlag();
+      cy.pytchClickStage(0, 0);
+      cy.pytchStdoutShouldContain("hello 1 0 0\n");
+
+      cy.pytchSendKeysToApp("m");
+      cy.pytchStdoutShouldContain("moved 1\n");
+      cy.pytchClickStage(-230, -170);
+      cy.pytchClickStage(-239, -179);
+      cy.pytchStdoutShouldContain("hello 2 -240 -180\n");
+
+      cy.pytchSendKeysToApp("m");
+      cy.pytchStdoutShouldContain("moved 2\n");
+      cy.pytchClickStage(-230, 170);
+      cy.pytchClickStage(-239, 179);
+      cy.pytchStdoutShouldContain("hello 3 -240 180\n");
+
+      cy.pytchSendKeysToApp("m");
+      cy.pytchStdoutShouldContain("moved 3\n");
+      cy.pytchClickStage(230, -170);
+      cy.pytchClickStage(239, -179);
+      cy.pytchStdoutShouldContain("hello 4 240 -180\n");
+
+      cy.pytchSendKeysToApp("m");
+      cy.pytchStdoutShouldContain("moved 4\n");
+      cy.pytchClickStage(230, 170);
+      cy.pytchClickStage(239, 179);
+      cy.pytchStdoutShouldContain("hello 5 240 180\n");
+    })
+  );
 });
