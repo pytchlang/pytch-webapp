45e15c2b74ffbee760203aa59f166039ae4cbbc3
Ben North <ben@redfrontdoor.org>
2021-03-06 17:46:57 +0000

    PopperIDETooltip: Define, export

--- src/components/PopperIDETooltip.tsx
+++ src/components/PopperIDETooltip.tsx
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useStoreState } from "../store";
+import { usePopper } from "react-popper";
 
 type PopperIDETooltipProps = {
   referenceElement: HTMLElement | null;
   targetTourStage: string;
 };
+
+const PopperIDETooltip: React.FC<PopperIDETooltipProps> = ({
+  referenceElement,
+  targetTourStage,
+  children,
+}) => {
+  const buttonTourProgressStage = useStoreState(
+    (state) => state.ideLayout.buttonTourProgressStage
+  );
+
+  const [popperElt, setPopperElt] = useState<HTMLDivElement | null>(null);
+  const [arrowElt, setArrowElt] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(
+    referenceElement,
+    popperElt,
+    {
+      modifiers: [
+        { name: "arrow", options: { element: arrowElt } },
+        { name: "offset", options: { offset: [0, 15] } },
+      ],
+    }
+  );
+
+  if (buttonTourProgressStage !== targetTourStage) {
+    return null;
+  }
+
+  return (
+    <div
+      className="pytch-tooltip"
+      ref={setPopperElt}
+      style={styles.popper}
+      {...attributes.popper}
+    >
+      <div className="pytch-tooltip-content">
+        {children}
+      </div>
+      <div
+        className="pytch-tooltip-arrow"
+        ref={setArrowElt}
+        style={styles.arrow}
+      />
+    </div>
+  );
+};
+
+export default PopperIDETooltip;
