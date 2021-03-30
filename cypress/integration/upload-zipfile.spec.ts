f34261179c5750b03858cfd9482f0e596fd64e1c
Ben North <ben@redfrontdoor.org>
2021-03-30 16:22:49 +0100

    tryUploadZipfile(): Add test helper

--- cypress/integration/upload-zipfile.spec.ts
+++ cypress/integration/upload-zipfile.spec.ts
@@ -3,4 +3,10 @@ context("Upload project from zipfile", () => {
     cy.pytchResetDatabase();
     cy.contains("My projects").click();
   });
+
+  const tryUploadZipfile = (zipBasename: string) => {
+    cy.contains("Upload project").click();
+    cy.get(".form-control-file").attachFile(`project-zipfiles/${zipBasename}`);
+    cy.get(".modal-footer").contains("Upload project").click();
+  };
 });
