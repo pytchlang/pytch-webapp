f65261319ed8248d6dfc260d7db307608ca7ac03
Ben North <ben@redfrontdoor.org>
2021-03-30 15:18:11 +0100

    _zipObjOrFail(): Add helper

--- src/model/user-interactions/upload-zipfile.ts
+++ src/model/user-interactions/upload-zipfile.ts
@@ -1,5 +1,6 @@
 import { Thunk } from "easy-peasy";
 import { IModalUserInteraction } from ".";
+import JSZip from "jszip";
 
 export interface IUploadZipfileDescriptor {
   zipName: string;
@@ -11,3 +12,10 @@ type IUploadZipfileBase = IModalUserInteraction<IUploadZipfileDescriptor>;
 interface IUploadZipfileSpecific {
   launch: Thunk<IUploadZipfileBase & IUploadZipfileSpecific>;
 }
+
+const _zipObjOrFail = (zip: JSZip, path: string) => {
+  const maybeZipObj = zip.file(path);
+  if (maybeZipObj == null)
+    throw new Error(`could not find "${path}" within zipfile`);
+  return maybeZipObj;
+};
