bc275c3719be76b790bf61e33cfd978321a04d8f
Ben North <ben@redfrontdoor.org>
2021-05-10 15:51:56 +0100

    QuestionInputPanel: Define component

--- src/components/QuestionInputPanel.tsx
+++ src/components/QuestionInputPanel.tsx
@@ -1,4 +1,59 @@
+import React, { ChangeEvent, useEffect } from "react";
+import { useStoreState, useStoreActions } from "../store";
+import { focusOrBlurFun } from "../utils";
+
+import Form from "react-bootstrap/Form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const QuestionInputPanel = () => {
+  const state = useStoreState((state) => state.userTextInput.state);
+  const maybePrompt = useStoreState((state) => state.userTextInput.prompt);
+  const answer = useStoreState((state) => state.userTextInput.answer);
+  const { setAnswer, submit } = useStoreActions(
+    (actions) => actions.userTextInput
+  );
+
+  const isInteractable = state === "interactable";
+
+  const inputRef: React.RefObject<HTMLInputElement> = React.createRef();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(focusOrBlurFun(inputRef, isInteractable, isInteractable));
+
+  if (!isInteractable) {
+    return null;
+  }
+
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) =>
+    setAnswer(evt.target.value);
+
+  const handleKeyPress: React.KeyboardEventHandler = (evt) => {
+    if (evt.key === "Enter") {
+      submit();
+    }
+  };
+
+  return (
+    <div className="question-and-answer">
+      {maybePrompt == null ? null : <div className="prompt">{maybePrompt}</div>}
+      <div className="input">
+        <Form.Control
+          ref={inputRef}
+          type="text"
+          value={answer}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+          tabIndex={-1}
+        ></Form.Control>
+        <div className="font-container">
+          <FontAwesomeIcon
+            className="check-icon fa-lg"
+            icon="check-square"
+            onClick={() => submit()}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionInputPanel;
