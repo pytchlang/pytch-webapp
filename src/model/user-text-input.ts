5944e53b288dcdf7558975a09002621a15e79b51
Ben North <ben@redfrontdoor.org>
2021-05-10 15:47:38 +0100

    Add types for dataflow in/out of user-text-input

--- src/model/user-text-input.ts
+++ src/model/user-text-input.ts
@@ -2,3 +2,19 @@ import { Action, action, Thunk, thunk } from "easy-peasy";
 import { IPytchAppModel } from ".";
 
 ////////////////////////////////////////////////////////////////////////////////
+
+// Description, coming from the VM, of a question which should be live.
+export interface IQuestionFromVM {
+  id: number;
+  prompt: string | null;
+}
+
+// Description, ready to be given to the VM, of the answer submitted by
+// the user.
+export interface IUserAnswerSubmissionToVM {
+  questionId: number;
+  answer: string;
+}
+
+// There isn't always a submitted answer.
+export type MaybeUserAnswerSubmissionToVM = IUserAnswerSubmissionToVM | null;
