5121817bcff5397249fda5b7411a35d6eabaae81
Ben North <ben@redfrontdoor.org>
2021-05-10 15:48:51 +0100

    Add documentation for user-text-input

--- src/model/user-text-input.ts
+++ src/model/user-text-input.ts
@@ -2,6 +2,53 @@ import { Action, action, Thunk, thunk } from "easy-peasy";
 import { IPytchAppModel } from ".";
 
 ////////////////////////////////////////////////////////////////////////////////
+//
+// After each project frame, there might be a question which the user's
+// code has posed, via ask_and_wait_for_answer().  This user-text-input
+// model slice represents the the webapp's view of the state of this
+// interaction.  From the webapp's point of view, there can sometimes be
+// an active "user text input" element.  This goes through the following
+// states:
+//
+// "idle" --- no question is being asked by the VM
+//
+//     After a call to setQuestion(), move to:
+//
+// "interactable" --- there is a live question being asked by the VM,
+// and the user is being given a chance to answer it; the user can
+// interact with the text input element of the UI; in this state,
+// repeated calls to setAnswer() are permissible
+//
+//     After a call to submit(), move to:
+//
+// "submitted" --- the user has pressed the Enter key or clicked on the
+// check-box icon, to submit their response *to the webapp*; there is a
+// further step required before that answer gets *to the VM*; see below
+//
+//     After a call to maybeAcquireSubmission(), move back to "idle".
+//
+// It is also permissible to call reset() while in "interactable", to
+// move back to "idle".
+//
+// The main ProjectEngine.oneFrame() method takes care of moving data in
+// both directions: passing a question from the VM to the webapp, and
+// passing an answer from the webapp to the VM.
+//
+// The project within the VM reports whether a question should be live
+// in the return value of its one_frame() method.  If there is a live
+// question, the ProjectEngine passes it to the user-text-input model
+// slice via setQuestion().
+//
+// In the other direction, the ProjectEngine takes care of 'acquiring'
+// any submitted-to-webapp answer and passing it on to the VM.  The act
+// of acquiring the answer from the user-text-input puts its state back
+// to "idle".
+//
+// The user's answer is get/set by the QuestionInputPanel component,
+// which renders the text input element, along with the live question's
+// prompt, if any.
+
+////////////////////////////////////////////////////////////////////////////////
 
 // Description, coming from the VM, of a question which should be live.
 export interface IQuestionFromVM {
