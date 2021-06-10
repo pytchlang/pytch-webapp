114fbea200bab0cba1724d2620efd5fb20d20b88
Ben North <ben@redfrontdoor.org>
2021-06-10 10:26:38 +0100

    Add types for help-content elements

--- src/model/help-sidebar.ts
+++ src/model/help-sidebar.ts
@@ -1,5 +1,24 @@
 import { Action, action } from "easy-peasy";
 
+export type HeadingElementDescriptor = {
+  kind: "heading";
+  heading: string;
+};
+
+export type BlockElementDescriptor = {
+  kind: "block";
+  python: string;
+  scratch: string; // TODO: Convert to Scratchblocks SVG
+  help: string; // TODO: Convert to DOM element
+  helpIsVisible: boolean;
+};
+
+export type HelpElementDescriptor =
+  | HeadingElementDescriptor
+  | BlockElementDescriptor;
+
+export type HelpContentDescriptor = Array<HelpElementDescriptor>;
+
 export interface IHelpSidebar {
   isVisible: boolean;
   toggleVisibility: Action<IHelpSidebar>;
