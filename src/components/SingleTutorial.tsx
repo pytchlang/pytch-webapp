418d396245ac4e5ce4e74d23253cd06499988f1c
Ben North <ben@redfrontdoor.org>
2021-07-07 20:50:43 +0100

    SingleTutorialContent: Add local component

--- src/components/SingleTutorial.tsx
+++ src/components/SingleTutorial.tsx
@@ -1,7 +1,51 @@
 import React, { useEffect } from "react";
+import { SyncState } from "../model/project";
+import { ITutorialSummary } from "../model/tutorials";
+import { TutorialSummaryDisplay } from "./TutorialSummaryDisplay";
 
 const SingleTutorialError = () => (
   <div className="loading-error">
     <p>Sorry, there was a problem finding the suggested tutorial.</p>
   </div>
 );
+
+interface SingleTutorialContentProps {
+  targetSlug: string;
+  syncState: SyncState;
+  availableSummaries: Array<ITutorialSummary>;
+}
+
+const SingleTutorialContent: React.FC<SingleTutorialContentProps> = (props) => {
+  switch (props.syncState) {
+    case SyncState.Syncd:
+      // Fall through into rest of function for usual case.
+      break;
+
+    case SyncState.SyncNotStarted:
+    case SyncState.SyncingFromBackEnd:
+      return (
+        <div className="loading-placeholder">
+          <p>Loading...</p>
+        </div>
+      );
+
+    case SyncState.Error:
+    case SyncState.SyncingToBackEnd:
+    default:
+      return <SingleTutorialError />;
+  }
+
+  const requestedTutorial = props.availableSummaries.find(
+    (t) => t.slug === props.targetSlug
+  );
+
+  if (requestedTutorial == null) {
+    return <SingleTutorialError />;
+  }
+
+  return (
+    <ul className="tutorial-list">
+      <TutorialSummaryDisplay tutorial={requestedTutorial} />
+    </ul>
+  );
+};
