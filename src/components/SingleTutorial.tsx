af38d1c2c15da61bbfe59bfd26a44cb48577176c
Ben North <ben@redfrontdoor.org>
2021-07-07 20:49:48 +0100

    SingleTutorialError: Add local component

--- src/components/SingleTutorial.tsx
+++ src/components/SingleTutorial.tsx
@@ -1,1 +1,7 @@
 import React, { useEffect } from "react";
+
+const SingleTutorialError = () => (
+  <div className="loading-error">
+    <p>Sorry, there was a problem finding the suggested tutorial.</p>
+  </div>
+);
