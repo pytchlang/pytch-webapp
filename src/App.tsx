22bc8d0e5c5a2a0221bf06b37bf5f9cce9b907ef
Ben North <ben@redfrontdoor.org>
2021-07-12 11:49:53 +0100

    UnknownRoute: Add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router } from "@reach/router";
+import { RouteComponentProps, Router } from "@reach/router";
 import Welcome from "./components/Welcome";
 import ProjectList from "./components/ProjectList";
 import TutorialList from "./components/TutorialList";
@@ -12,6 +12,22 @@ import "./font-awesome-lib";
 
 import { AllModals } from "./components/AllModals";
 import { SingleTutorial } from "./components/SingleTutorial";
+import Link from "./components/LinkWithinApp";
+import NavBanner from "./components/NavBanner";
+
+const UnknownRoute = (props: RouteComponentProps) => {
+  return (
+    <>
+      <NavBanner />
+      <div className="unknown-route">
+        <p>Sorry, we could not find that page.</p>
+        <p>
+          Try returning to the <Link to="/">Pytch homepage</Link>.
+        </p>
+      </div>
+    </>
+  );
+};
 
 function App() {
   const basepath = process.env.PUBLIC_URL || "/";
