7bb96817d4e9b712e1ea51d26ac40107ac321cd5
Ben North <ben@redfrontdoor.org>
2021-08-10 15:52:39 +0100

    maybeJoinStudyCode(): Add helper

--- src/components/StudySessionManager.tsx
+++ src/components/StudySessionManager.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+const maybeJoinStudyCode = () => {
+  const joinMatcher = new RegExp("/join/([0-9a-f-]*)$");
+  const joinMatch = joinMatcher.exec(window.location.href);
+  return joinMatch == null ? null : joinMatch[1];
+};
+
 export const StudySessionManager = () => {
   return (
     <div className="App">
