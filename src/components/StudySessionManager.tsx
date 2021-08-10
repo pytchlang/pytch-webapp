4c75d353a7d37fbdfd016a27308a19b7c51b3141
Ben North <ben@redfrontdoor.org>
2021-08-10 15:52:59 +0100

    ActionPendingSpinner: Add component

--- src/components/StudySessionManager.tsx
+++ src/components/StudySessionManager.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Spinner from "react-bootstrap/Spinner";
 
 const maybeJoinStudyCode = () => {
   const joinMatcher = new RegExp("/join/([0-9a-f-]*)$");
   const joinMatch = joinMatcher.exec(window.location.href);
   return joinMatch == null ? null : joinMatch[1];
+};
+
+const ActionPendingSpinner = () => {
+  return (
+    <div className="modal-waiting-spinner">
+      <Spinner animation="border" variant="primary" />
+    </div>
+  );
 };
 
 export const StudySessionManager = () => {
