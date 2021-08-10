24a0e8761f3f14aec03dbb31e0c82ee318089215
Ben North <ben@redfrontdoor.org>
2021-08-10 16:59:38 +0100

    Add machinery for testing stubbed / localhost

--- cypress/integration/study-data.spec.ts
+++ cypress/integration/study-data.spec.ts
@@ -7,4 +7,20 @@ context("Joining and signing out of a study", () => {
     it("skipped — Cypress env.var STUDY_API_BASE not set", () => {});
     return;
   }
+
+  let backendSpecs = [{ key: "stubbed", intercept: cy.intercept }];
+
+  let validStudyCode = "11111111-2222-3333-4444-555555555555";
+  let validParticipantCode = "abcd-efgh";
+
+  const rawLocalhostCodes = Cypress.env("STUDY_API_TEST_VALID_CODES");
+  if (rawLocalhostCodes != null) {
+    [validStudyCode, validParticipantCode] = rawLocalhostCodes.split(":");
+    backendSpecs.push({ key: "localhost", intercept: () => null });
+  }
+
+  backendSpecs.forEach((backendSpec) => {
+    context(backendSpec.key, () => {
+    });
+  });
 });
