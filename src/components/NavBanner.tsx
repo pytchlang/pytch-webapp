cd1df1d088f0c7bdb5706c4196d660a13339c91f
Ben North <ben@redfrontdoor.org>
2021-08-10 16:48:08 +0100

    NavBanner: Maybe include sign-out-session link

--- src/components/NavBanner.tsx
+++ src/components/NavBanner.tsx
@@ -1,10 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { studyEnabled } from "../database/study-server";
+import { useStoreActions } from "../store";
 import { withinSite } from "../utils";
 import { Link } from "./LinkWithinApp";
 
 const versionTag = process.env.REACT_APP_VERSION_TAG;
 
 const NavBanner = () => {
+  const signOut = useStoreActions(
+    (actions) => actions.sessionState.signOutSession
+  );
+
+  const maybeSignOut = studyEnabled && (
+    <li className="slightly-prominent" onClick={() => signOut()}>
+      <FontAwesomeIcon icon="sign-out-alt" /> Sign out of study
+    </li>
+  );
+
   return (
     <div className="NavBar">
       <div className="title-and-version">
@@ -25,6 +38,7 @@ const NavBanner = () => {
         <Link to="/my-projects/">
           <li>My projects</li>
         </Link>
+        {maybeSignOut}
       </ul>
     </div>
   );
