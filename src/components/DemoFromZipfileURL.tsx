216f4a576767256a8b04d2b74177475d3c653502
Ben North <ben@redfrontdoor.org>
2021-09-02 12:22:14 +0100

    DemoFromZipfileURL: Add component skeleton

---
+++ src/components/DemoFromZipfileURL.tsx
@@ -0,0 +1,12 @@
+import { RouteComponentProps } from "@reach/router";
+import React, { useEffect } from "react";
+
+interface DemoFromZipfileURLProps extends RouteComponentProps {
+  buildId?: string;
+  demoId?: string;
+}
+
+export const DemoFromZipfileURL: React.FC<DemoFromZipfileURLProps> = (
+  props
+) => {
+};
