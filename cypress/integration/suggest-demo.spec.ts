4ef1f911539c8607192ef52562eb59216c057147
Ben North <ben@redfrontdoor.org>
2021-09-06 12:00:40 +0100

    Add tests

---
+++ cypress/integration/suggest-demo.spec.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+context("Work with suggested demos", () => {
+  beforeEach(() => {
+    cy.pytchResetDatabase();
+  });
+
+  // TODO: Set PYTCH_CYPRESS.instantDelays for these tests:
+
+  it("Launch a suggested demo", () => {
+    cy.visit("/suggested-demo/fake-build-id-for-tests/bubbles");
+    cy.contains("Bubbles");
+    cy.contains("Try this project").click();
+    cy.contains("images and sounds");
+    cy.get(".ReadOnlyOverlay").should("not.exist");
+    cy.contains("Click the green flag");
+  });
+
+  it("Give error if no such demo", () => {
+    cy.visit("/suggested-demo/fake-build-id-for-tests/does-not-exist");
+    cy.contains("Problem");
+    cy.contains("Sorry");
+  });
+});
