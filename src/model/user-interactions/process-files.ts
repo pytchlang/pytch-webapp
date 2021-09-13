0c32d85bf63b7c89e3c3ac832fe13d528c970599
Ben North <ben@redfrontdoor.org>
2021-09-13 21:31:18 +0100

    Add comment as to why using function not constant

--- src/model/user-interactions/process-files.ts
+++ src/model/user-interactions/process-files.ts
@@ -28,6 +28,14 @@ export type IProcessFilesInteraction = IProcessFilesBase & {
   tryProcess: Thunk<IProcessFilesBase, FileList, any, IPytchAppModel>;
 };
 
+// This is a function returning a new object from a literal, rather than
+// a simple constant.  Turned out that using a constant here (and spread
+// into addAssetsInteraction and uploadZipfilesInteraction) resulted in
+// "addAssetsInteraction.launch()" launching the upload-zipfiles modal.
+// I didn't get to the bottom of this, but suspect it's to do with
+// Easy-Peasy needing some way to map from the thunk to its place in the
+// store, and this mechanism being confused if the self-same thunk
+// appears in two different places in the store.
 export const processFilesBase: () => IProcessFilesBase = () => ({
   status: "idle",
 
