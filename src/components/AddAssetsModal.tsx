3c6eb393d7c5ff40915ca544394403bd2b577e9f
Ben North <ben@redfrontdoor.org>
2021-09-13 12:14:33 +0100

    FileProcessingFailures: Move to own file
    Move AdditionFailures component to new file under new name.  Just move
    content.  Does not yet build.

--- src/components/AddAssetsModal.tsx
+++ src/components/AddAssetsModal.tsx
@@ -1,34 +1,8 @@
 import React from "react";
-import Modal from "react-bootstrap/Modal";
 import { useStoreActions, useStoreState } from "../store";
-import { Failure } from "../model/user-interactions/add-assets";
 import { assertNever } from "../utils";
 import { ChooseFiles } from "./ChooseFiles";
-
-const AdditionFailures: React.FC<{ failures: Array<Failure> }> = (props) => {
-  const dismiss = useStoreActions(
-    (actions) => actions.userConfirmations.addAssetsInteraction.dismiss
-  );
-
-  const failureEntries = props.failures.map((failure) => (
-    <li key={failure.fileName}>
-      <code>{failure.fileName}</code> — {failure.reason}
-    </li>
-  ));
-
-  return (
-    <Modal show={true} animation={false} className="add-asset-failures">
-      <Modal.Header closeButton={true} onHide={() => dismiss()}>
-        <Modal.Title>Problem adding images or sounds</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Sorry, there was a problem adding files to your project:</p>
-        <ul>{failureEntries}</ul>
-        <p>Please check the files and try again.</p>
-      </Modal.Body>
-    </Modal>
-  );
-};
+import { FileProcessingFailures } from "./FileProcessingFailures";
 
 export const AddAssetsModal = () => {
   const state = useStoreState(
