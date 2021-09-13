a511011476db545872e64e5ab593121dfe01a57b
Ben North <ben@redfrontdoor.org>
2021-09-13 10:51:17 +0100

    renameProjectInteraction: Add model slice
    Very similar to renameAssetInteraction.  Would be good to review and
    see whether worth factoring out "rename" behaviour.

---
+++ src/model/user-interactions/rename-project.ts
@@ -0,0 +1,57 @@
+import { Action, action, Actions, Thunk, thunk } from "easy-peasy";
+import { IPytchAppModel } from "..";
+import { IRenameProjectDescriptor } from "../project";
+import { IModalUserInteraction, modalUserInteraction } from ".";
+import { batch } from "react-redux";
+import { IProjectSummary, ProjectId } from "../projects";
+
+type IRenameProjectBase = IModalUserInteraction<IRenameProjectDescriptor>;
+
+interface IRenameProjectSpecific {
+  projectId: ProjectId;
+  oldName: string;
+  newName: string;
+  setProjectId: Action<IRenameProjectSpecific, ProjectId>;
+  setOldName: Action<IRenameProjectSpecific, string>;
+  setNewName: Action<IRenameProjectSpecific, string>;
+  launch: Thunk<IRenameProjectBase & IRenameProjectSpecific, IProjectSummary>;
+}
+
+const attemptRename = (
+  actions: Actions<IPytchAppModel>,
+  renameDescriptor: IRenameProjectDescriptor
+) =>
+  actions.projectCollection.requestRenameProjectThenResync({
+    id: renameDescriptor.projectId,
+    name: renameDescriptor.newName,
+  });
+
+const renameProjectSpecific: IRenameProjectSpecific = {
+  projectId: -1,
+  oldName: "",
+  newName: "",
+  setProjectId: action((state, projectId) => {
+    state.projectId = projectId;
+  }),
+  setOldName: action((state, oldName) => {
+    state.oldName = oldName;
+  }),
+  setNewName: action((state, newName) => {
+    state.newName = newName;
+  }),
+  launch: thunk((actions, projectSummary) => {
+    batch(() => {
+      actions.setProjectId(projectSummary.id);
+      actions.setOldName(projectSummary.name);
+      actions.setNewName(projectSummary.name);
+      actions.superLaunch();
+    });
+  }),
+};
+
+export type IRenameProjectInteraction = IRenameProjectBase &
+  IRenameProjectSpecific;
+export const renameProjectInteraction = modalUserInteraction(
+  attemptRename,
+  renameProjectSpecific
+);
