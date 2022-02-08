d2b2e08c947a8de85ea6d696232e8e31f26b7476
Ben North <ben@redfrontdoor.org>
2022-02-08 13:02:21 +0000

    stageFullScreenBorderPx: Add constant

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,8 @@ export const stageHeight: number = 360;
 export const stageHalfWidth: number = 0.5 * stageWidth;
 export const stageHalfHeight: number = 0.5 * stageHeight;
 
+export const stageFullScreenBorderPx: number = 16;
+
 export const liveReloadEnabled =
   process.env.REACT_APP_ENABLE_LIVE_RELOAD_WEBSOCKET === "yes";
 
