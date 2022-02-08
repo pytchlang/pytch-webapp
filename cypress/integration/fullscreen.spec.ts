a6446672d6d5647396d081f3fcf6e64abed77e7c
Ben North <ben@redfrontdoor.org>
2022-02-08 14:14:51 +0000

    Add "enters and leaves full-screen layout" test

--- cypress/integration/fullscreen.spec.ts
+++ cypress/integration/fullscreen.spec.ts
@@ -16,4 +16,16 @@ context("Full-screen layout", () => {
     cy.get("#pytch-canvas").its("0.width").should("eq", stageWidth);
     cy.get("#pytch-canvas").its("0.height").should("eq", stageHeight);
   });
+
+  it("enters and leaves full-screen layout", () => {
+    cy.get(".LayoutChooser .full-screen").click();
+    cy.get(".CodeEditor").should("not.exist");
+    cy.get(".InfoPanel").should("not.exist");
+    cy.get(".LayoutChooser").should("not.exist");
+
+    cy.get(".leave-full-screen").click();
+    cy.get(".CodeEditor");
+    cy.get(".InfoPanel");
+    cy.get(".LayoutChooser");
+  });
 });
