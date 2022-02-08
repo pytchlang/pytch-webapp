3d2274b8ca2fac80f19b1508b917874dc21151f9
Ben North <ben@redfrontdoor.org>
2022-02-08 14:14:32 +0000

    Add "does not resize stage in IDE layout" test

--- cypress/integration/fullscreen.spec.ts
+++ cypress/integration/fullscreen.spec.ts
@@ -1,7 +1,19 @@
 /// <reference types="cypress" />
 
+import {
+  stageWidth,
+  stageHeight,
+} from "../../src/constants";
+
 context("Full-screen layout", () => {
   before(() => {
     cy.pytchExactlyOneProject();
+  });
+
+  it("does not resize stage in IDE layout", () => {
+    cy.get(".LayoutChooser button.wide-info").click().click();
+    cy.viewport(800, 600);
+    cy.get("#pytch-canvas").its("0.width").should("eq", stageWidth);
+    cy.get("#pytch-canvas").its("0.height").should("eq", stageHeight);
   });
 });
