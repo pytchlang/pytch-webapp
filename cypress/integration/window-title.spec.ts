817eb3dd8b61a350b4d28ed24907e63eb7cc207f
Ben North <ben@redfrontdoor.org>
2022-02-17 20:09:54 +0000

    Add "home page after project" test
    Currently fails.

--- cypress/integration/window-title.spec.ts
+++ cypress/integration/window-title.spec.ts
@@ -4,4 +4,11 @@ context("Browser window title", () => {
   before(() => {
     cy.pytchExactlyOneProject();
   });
+
+  it("home page after project", () => {
+    cy.visit("/my-projects/");
+    cy.pytchOpenProject("Test seed");
+    cy.pytchHomeFromIDE();
+    cy.title().should("eq", "Pytch");
+  });
 });
