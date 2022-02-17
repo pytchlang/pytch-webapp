a483a10a1e06200b17f3fe0dceae7653861b54e0
Ben North <ben@redfrontdoor.org>
2022-02-17 20:24:39 +0000

    Also test for other page titles

--- cypress/integration/window-title.spec.ts
+++ cypress/integration/window-title.spec.ts
@@ -7,7 +7,13 @@ context("Browser window title", () => {
 
   it("home page after project", () => {
     cy.visit("/my-projects/");
+    cy.title().should("eq", "Pytch: My projects");
     cy.pytchOpenProject("Test seed");
+
+    // TODO: This should really be the name the user gave the project,
+    // not an internal ID integer.
+    cy.title().should("match", /Pytch: Project [0-9]+/);
+
     cy.pytchHomeFromIDE();
     cy.title().should("eq", "Pytch");
   });
