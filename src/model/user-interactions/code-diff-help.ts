b69b0b78b7482417bc814e8d582169ffc5407729
Ben North <ben@redfrontdoor.org>
2022-02-17 11:00:25 +0000

    IDiffHelpSamples: Define interface

--- src/model/user-interactions/code-diff-help.ts
+++ src/model/user-interactions/code-diff-help.ts
@@ -4,3 +4,9 @@ import { IPytchAppModel } from "..";
 // It's a bit sledgehammer/nut to use this machinery for the simple
 // "display code-diff help" modal, since there is no action to attempt,
 // but doing so keeps the approach consistent.
+
+export interface IDiffHelpSamples {
+  unchanged: HTMLTableElement | null;
+  deleted: HTMLTableElement | null;
+  added: HTMLTableElement | null;
+}
