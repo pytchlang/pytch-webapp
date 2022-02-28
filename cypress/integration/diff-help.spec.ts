516e395b1cb9303ea7eec784aa2471482a529a5d
Ben North <ben@redfrontdoor.org>
2022-02-28 12:32:51 +0000

    Test can repeatedly summon/dismiss same help

--- cypress/integration/diff-help.spec.ts
+++ cypress/integration/diff-help.spec.ts
@@ -11,12 +11,15 @@ context("Work with pop-up diff help", () => {
   it("can summon and dismiss diff help", () => {
     cy.get("ul.ToC").contains("Add the player's bat").click();
 
-    cy.get(".patch-container .header button").eq(0).click();
-    cy.get(".modal-body .patch-container").should("have.length", 2);
-    cy.get(diffSelector("unch")).contains("BoingBackground");
-    cy.get(diffSelector("add")).contains("PlayerBat");
-    cy.get(".modal-header .close").click();
-    cy.get(".modal").should("not.exist");
+    // Check can repeatedly summon and dismiss:
+    for (let _i = 0; _i !== 3; ++_i) {
+      cy.get(".patch-container .header button").eq(0).click();
+      cy.get(".modal-body .patch-container").should("have.length", 2);
+      cy.get(diffSelector("unch")).contains("BoingBackground");
+      cy.get(diffSelector("add")).contains("PlayerBat");
+      cy.get(".modal-header .close").click();
+      cy.get(".modal").should("not.exist");
+    }
 
     cy.get(".patch-container .header button").eq(3).click();
     cy.get(".modal-body .patch-container").should("have.length", 3);
