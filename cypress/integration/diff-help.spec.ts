60d33b8469c47fb38e19a62530c9c30ce0e6b53b
Ben North <ben@redfrontdoor.org>
2022-02-28 12:25:21 +0000

    diffSelector(): Add test helper

--- cypress/integration/diff-help.spec.ts
+++ cypress/integration/diff-help.spec.ts
@@ -4,4 +4,7 @@ context("Work with pop-up diff help", () => {
   beforeEach(() => {
     cy.pytchProjectFollowingTutorial();
   });
+
+  const diffSelector = (kind: string) =>
+    `.modal-body .patch-container .diff-${kind}`;
 });
