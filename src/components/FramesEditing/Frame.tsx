6747f9627438955b115dc5efdf35cdc8224973d5
Ben North <ben@redfrontdoor.org>
2022-03-04 14:37:56 +0000

    componentFromKind: Add look-up

--- src/components/FramesEditing/Frame.tsx
+++ src/components/FramesEditing/Frame.tsx
@@ -2,3 +2,12 @@ import React from "react";
 
 import { Editable, Frame as FrameT } from "../../model/frames-editing";
 import { CommentFrame } from "./CommentFrame";
+
+const componentFromKind = {
+  comment: CommentFrame,
+  // TODO: Add
+  //
+  // assignment: AssignmentFrame
+  //
+  // etc. when implemented.
+};
