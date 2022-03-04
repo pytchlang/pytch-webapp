e8a2c91e7b0571dce08c01506ad1a26e928c2239
Ben North <ben@redfrontdoor.org>
2022-03-04 14:38:06 +0000

    FrameContent: Add local component

--- src/components/FramesEditing/Frame.tsx
+++ src/components/FramesEditing/Frame.tsx
@@ -11,3 +11,14 @@ const componentFromKind = {
   //
   // etc. when implemented.
 };
+
+const FrameContent: React.FC<Editable<FrameT>> = (props) => {
+  const mComponent = componentFromKind[props.frame.kind];
+  if (mComponent != null) {
+    // Need "as any" because TypeScript can't work out that the runtime
+    // behaviour will be for the types to match.
+    return React.createElement(mComponent as any, props, null);
+  } else {
+    return <div>UNKNOWN FRAME-KIND!?</div>;
+  }
+};
