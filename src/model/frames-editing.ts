4d738bca259a988881e71333454571e5c153131c
Ben North <ben@redfrontdoor.org>
2022-03-04 14:17:46 +0000

    EditState: Add type

--- src/model/frames-editing.ts
+++ src/model/frames-editing.ts
@@ -42,3 +42,28 @@ type AssignmentCore = {
 // TODO: Uncomment AssignmentFrame; add other frame types when done.
 
 export type Frame = /* AssignmentFrame | WhileLoopFrame | ... | */ CommentFrame;
+
+////////////////////////////////////////////////////////////////////////
+// Editing a frame
+
+// This is all a bit cumbersome because I'm trying to bridge the purely
+// functional world of props-based React components with the "one huge
+// global variable" world of Easy-Peasy.
+
+// The type of the EditState.save() function (below) when in state
+// "being-edited" could be tighter, because we only want to update the
+// frame with one of the same kind.  But I spent too long down the
+// rabbit-hole of how to explain this to the TypeScript type system
+// without getting to a good answer!
+export type EditState =
+  | {
+      status: "being-edited";
+      save: (newFrame: Frame) => void;
+      // TODO: cancel: () => void;
+      delete: () => void;
+    }
+  | {
+      status: "saved";
+      edit: () => void;
+      delete: () => void;
+    };
