e22a2846542262c3c774dc54e8db02759b07ec69
justinemoulin <justinemoulin5@icloud.com>
2022-05-25 12:46:04 +0100

    Add scratchblocks-render file

---
+++ src/model/scratchblocks-render.ts
@@ -0,0 +1,24 @@
+import scratchblocks from "scratchblocks";
+
+/**
+ * Convert scratchblocks text `scratchText` into SVG element, scaling
+ * down.  The containing DIV needs to be scaled similarly when the SVG
+ * is inserted into the DOM in a `useEffect()` of the relevant
+ * component.
+ */
+export const makeScratchSVG = (
+  scratchText: string,
+  scale: number
+): SVGElement => {
+  const sbOptions = { style: "scratch3" };
+  const sbDoc = scratchblocks.parse(scratchText, sbOptions);
+
+  let sbSvg: SVGElement = scratchblocks.render(sbDoc, sbOptions);
+  sbSvg.setAttribute("class", "scratchblocks");
+  sbSvg.setAttribute(
+    "style",
+    `transform:scale(${scale});transform-origin:0 0;`
+  );
+
+  return sbSvg;
+};
