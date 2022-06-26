076cb2e9b38aad1887b345d072e979c0dde0bbe6
Ben North <ben@redfrontdoor.org>
2022-06-26 09:23:31 +0100

    CopyProjectModal: Extract state, actions

--- src/components/CopyProjectModal.tsx
+++ src/components/CopyProjectModal.tsx
@@ -1,4 +1,23 @@
 import React from "react";
+import { useStoreActions, useStoreState } from "../store";
 
 export const CopyProjectModal = () => {
+  const {
+    isActive,
+    inputsReady,
+    isInteractable,
+    attemptSucceeded,
+    maybeLastFailureMessage,
+    sourceProjectId,
+    nameOfCopy,
+  } = useStoreState((state) => state.userConfirmations.copyProjectInteraction);
+
+  const {
+    dismiss,
+    attempt,
+    setNameOfCopy,
+    refreshInputsReady,
+  } = useStoreActions(
+    (actions) => actions.userConfirmations.copyProjectInteraction
+  );
 };
