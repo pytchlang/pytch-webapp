6645165077361f26d2224ad7981e69849684de95
Ben North <ben@redfrontdoor.org>
2022-06-26 10:12:05 +0100

    ICopyProjectSpecific: Define interface

--- src/model/user-interactions/save-project-as.ts
+++ src/model/user-interactions/save-project-as.ts
@@ -1,4 +1,23 @@
+import { action, Action, Actions, thunk, Thunk } from "easy-peasy";
 import { IModalUserInteraction, modalUserInteraction } from ".";
-import { ICopyProjectDescriptor } from "../projects";
+import { ICopyProjectDescriptor, ProjectId } from "../projects";
+import { IPytchAppModel } from "..";
 
 type ICopyProjectBase = IModalUserInteraction<ICopyProjectDescriptor>;
+
+interface ICopyProjectSpecific {
+  sourceProjectId: ProjectId;
+  setSourceProjectId: Action<ICopyProjectSpecific, ProjectId>;
+  nameOfCopy: string;
+  setNameOfCopy: Action<ICopyProjectSpecific, string>;
+  refreshInputsReady: Thunk<ICopyProjectBase & ICopyProjectSpecific>;
+
+  // Slight fudge to use ICopyProjectDescriptor as the payload type
+  // for launch(), but its properties are of the correct type.
+  launch: Thunk<
+    ICopyProjectBase & ICopyProjectSpecific,
+    ICopyProjectDescriptor,
+    any,
+    IPytchAppModel
+  >;
+}
