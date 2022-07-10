eab05111651add8de7179da97a54fb2a2c60d350
Ben North <ben@redfrontdoor.org>
2022-07-10 12:56:01 +0100

    AssetServer: Add docstrings

--- src/skulpt-connection/asset-server.ts
+++ src/skulpt-connection/asset-server.ts
@@ -24,6 +24,25 @@ type Asset = ImageAsset | SoundAsset;
 
 // Initial implementation re-fetches all assets every time.
 
+/**
+ * AssetServer: Object which provides images and sounds to a client.
+ *
+ * Used as a singleton, via the global instance `assetServer` defined
+ * below.
+ *
+ * Each time a new project is activated, that project's assets are
+ * loaded into the server via `prepare()`.  This method is also used
+ * when a new asset is added to a project.  The actual asset data is
+ * loaded from the IndexedDB store, keyed by asset-ID.
+ *
+ * To discard all stored assets from the asset-server, the `clear()`
+ * method is used.  This is done as a preparatory step when syncing all
+ * assets of a project into the React state, and also when deactivating
+ * a project.
+ *
+ * The `loadImage()` and `loadSoundData()` methods provide access to
+ * assets which have been loaded into the server via `prepare()`.
+ */
 class AssetServer {
   assetByName: Map<string, Asset>;
 
@@ -73,6 +92,11 @@ class AssetServer {
     }
   }
 
+  /** Ensure all the given assets are available for access via
+   * `loadImage()` or `loadSoundData()` as appropriate.  Actual asset
+   * data is pulled from the IndexedDB storage.  The returned Promise
+   * resolves (with `void`) when all assets are available.
+   */
   async prepare(assets: Array<IAssetInProject>) {
     await Promise.all(
       assets.map(async (asset) => {
@@ -81,6 +105,7 @@ class AssetServer {
     );
   }
 
+  /** Discard all stored assets. */
   clear() {
     const revokeURLIfImage = (asset: Asset) => {
       if (asset.kind === AssetKind.Image) {
@@ -110,11 +135,13 @@ class AssetServer {
     return asset;
   }
 
+  /** Return an image corresponding to the given asset name. */
   loadImage(name: string): HTMLImageElement {
     const asset = this.assetOfKind(name, AssetKind.Image, "Image");
     return (asset as ImageAsset).image;
   }
 
+  /** Return sound data corresponding to the given asset name. */
   loadSoundData(name: string): ArrayBuffer {
     const asset = this.assetOfKind(name, AssetKind.Sound, "Sound");
     return (asset as SoundAsset).audioData;
