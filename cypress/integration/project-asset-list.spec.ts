d05c914f02815f42752653177ad6c0cfa87a3217
justinemoulin <justinemoulin5@icloud.com>
2022-07-20 14:34:31 +0100

    Add "handles one failure and two successes" test

--- cypress/integration/project-asset-list.spec.ts
+++ cypress/integration/project-asset-list.spec.ts
@@ -276,4 +276,21 @@ context("Management of project assets", () => {
       ]);
     });
 
+    it("handles one failure and two successes", () => {
+      cy.contains("Choose from library").click();
+      cy.get(".clipart-card").contains("alien").click();
+      cy.get(".clipart-card").contains("angel").click();
+      cy.get(".clipart-card").contains("basic_guy").click();
+      clickAdd();
+      cy.contains(
+        '2 cliparts successfully added, but 1 problem encontered (alien: Your project already contains an asset called "alien.png".) Please modify your selection.'
+      );
+      cy.contains("Cancel").click();
+      cy.pytchShouldShowAssets([
+        ...initialAssets,
+        "alien.png",
+        "angel.png",
+        "basic_guy.png",
+      ]);
+    });
 });
