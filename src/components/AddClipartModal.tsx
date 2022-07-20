2f140a9c20e96f1d1dd0285860e2b7859a3cef05
justinemoulin <justinemoulin5@icloud.com>
2022-07-20 12:00:35 +0100

    bodyContent(): display of cliparts gallery

--- src/components/AddClipartModal.tsx
+++ src/components/AddClipartModal.tsx
@@ -6,3 +6,55 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ClipArtGalleryState } from "../model/clipart-gallery";
 import { assertNever } from "../utils";
 import { MaybeErrorOrSuccessReport } from "./MaybeErrorOrSuccessReport";
+
+const bodyContent = (
+  gallery: ClipArtGalleryState,
+  selectedIds: Array<number>,
+  selectItemById: (id: number) => void,
+  deselectItemById: (id: number) => void
+) => {
+  switch (gallery.status) {
+    case "fetch-failed":
+      return <p>Oops, sorry</p>;
+    case "fetch-not-started":
+    case "fetch-pending":
+      return <p>loading...</p>;
+    case "ready":
+      return (
+        <div className="clipart-gallery">
+          <ul>
+            {gallery.items.map((item: any) => {
+              const isSelected =
+                selectedIds.findIndex((id) => id === item.id) !== -1;
+              const extraClass = isSelected ? " selected" : "";
+              const clickHandler = isSelected
+                ? deselectItemById
+                : selectItemById;
+              return (
+                <li>
+                  <div
+                    className="clipart-card"
+                    onClick={() => clickHandler(item.id)}
+                  >
+                    <p className="clipart-checkmark">
+                      <span className={`clipart-selection${extraClass}`}>
+                        <FontAwesomeIcon
+                          className="fa-lg"
+                          icon="check-circle"
+                        />
+                      </span>
+                    </p>
+                    <p className="clipart-name">{item.name}</p>
+                    <p className="clipart-thumbnail">
+                      <img style={{ width: 100 }} src={item.url} />
+                    </p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+  }
+};
+
