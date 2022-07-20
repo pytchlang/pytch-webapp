ff7b9cc1b2cff9ad536c1a50399059032684ab37
justinemoulin <justinemoulin5@icloud.com>
2022-07-20 11:34:27 +0100

    Add ClipArtGalleryState type

--- src/model/clipart-gallery.ts
+++ src/model/clipart-gallery.ts
@@ -6,3 +6,9 @@ export type ClipArtGalleryItem = {
   name: string;
   data: any; // TODO: Work out what kind of image data should go here.
 };
+
+export type ClipArtGalleryState =
+  | { status: "fetch-not-started" }
+  | { status: "fetch-pending" }
+  | { status: "fetch-failed"; message: string }
+  | { status: "ready"; items: Array<ClipArtGalleryItem> };
