a8efa79391551d24a06f27ee7f9987154bfe46bc
justinemoulin <justinemoulin5@icloud.com>
2022-07-20 11:37:27 +0100

    Add intance of IClipArtGallery: clipArtGallery

--- src/model/clipart-gallery.ts
+++ src/model/clipart-gallery.ts
@@ -19,3 +19,40 @@ export interface IClipArtGallery {
 
   startFetchIfRequired: Thunk<IClipArtGallery, void, any, IPytchAppModel>;
 }
+
+export const clipArtGallery: IClipArtGallery = {
+  state: { status: "fetch-not-started" },
+  setState: action((state, innerState) => {
+    state.state = innerState;
+  }),
+
+  // Core work is in startFetchIfRequired().
+  startFetchIfRequired: thunk(async (actions, _voidPayload, helpers) => {
+    const state = helpers.getState().state;
+    if (state.status !== "fetch-not-started") return;
+
+    actions.setState({ status: "fetch-pending" });
+
+    // TODO: Implement actual fetching of raw data from server and
+    // conversion into array of gallery-items:
+    //
+    const resp = await fetch("http://localhost:8127/clipart_assets_list.json");
+    const galleryItems = await resp.json();
+
+    galleryItems.forEach((element: any) => {
+      element.url = `http://localhost:8127/${element.data}`;
+    });
+
+    // So we can see what's going on for now:
+    const items: Array<ClipArtGalleryItem> = galleryItems;
+
+    actions.setState({ status: "ready", items });
+
+    // TODO: Wrap the above in try/catch, and in catch do sth like
+    //
+    // actions.setState({
+    //   status: "fetch-failed",
+    //   message: "sorry something went wrong",
+    // });
+  }),
+};
