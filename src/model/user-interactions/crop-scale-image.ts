d8c946c0e9ca83c9b9dbd0451c487f58a9bb8e82
Ben North <ben@redfrontdoor.org>
2022-10-03 19:51:00 +0100

    ICropScaleImageSpecific: Define skeleton

--- src/model/user-interactions/crop-scale-image.ts
+++ src/model/user-interactions/crop-scale-image.ts
@@ -1,6 +1,8 @@
+import { action, Action, computed, Computed, thunk, Thunk } from "easy-peasy";
 import { IModalUserInteraction } from ".";
 import { AssetLocator, UpdateAssetTransformDescriptor } from "../project";
 import { ImageCropDescriptor, ImageDimensions } from "../asset";
+import { ProjectId } from "../projects";
 
 type ICropScaleImageBase = IModalUserInteraction<
   UpdateAssetTransformDescriptor
@@ -11,3 +13,11 @@ type CropScaleImageInitState = AssetLocator & {
   sourceURL: URL;
   originalSize: ImageDimensions;
 };
+
+interface ICropScaleImageSpecific extends CropScaleImageInitState {
+  setProjectId: Action<ICropScaleImageSpecific, ProjectId>;
+  setAssetName: Action<ICropScaleImageSpecific, string>;
+  setExistingCrop: Action<ICropScaleImageSpecific, ImageCropDescriptor>;
+  setSourceURL: Action<ICropScaleImageSpecific, URL>;
+  setOriginalSize: Action<ICropScaleImageSpecific, ImageDimensions>;
+}
