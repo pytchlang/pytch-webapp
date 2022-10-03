4ddf92e8ea46ed84c2c091248aa165da6d7ad2df
Ben North <ben@redfrontdoor.org>
2022-10-03 19:16:41 +0100

    CropScaleImageInitState: Define type

--- src/model/user-interactions/crop-scale-image.ts
+++ src/model/user-interactions/crop-scale-image.ts
@@ -1,6 +1,13 @@
 import { IModalUserInteraction } from ".";
-import { UpdateAssetTransformDescriptor } from "../project";
+import { AssetLocator, UpdateAssetTransformDescriptor } from "../project";
+import { ImageCropDescriptor, ImageDimensions } from "../asset";
 
 type ICropScaleImageBase = IModalUserInteraction<
   UpdateAssetTransformDescriptor
 >;
+
+type CropScaleImageInitState = AssetLocator & {
+  existingCrop: ImageCropDescriptor;
+  sourceURL: URL;
+  originalSize: ImageDimensions;
+};
