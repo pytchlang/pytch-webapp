5e813d8298aed36d52f6de1ee411fdc4759a8dd8
Ben North <ben@redfrontdoor.org>
2022-10-03 19:18:13 +0100

    cropScaleImageSpecific: Define skeleton
    Properties from CropScaleImageInitState and matching mutation actions.

--- src/model/user-interactions/crop-scale-image.ts
+++ src/model/user-interactions/crop-scale-image.ts
@@ -21,3 +21,35 @@ interface ICropScaleImageSpecific extends CropScaleImageInitState {
   setSourceURL: Action<ICropScaleImageSpecific, URL>;
   setOriginalSize: Action<ICropScaleImageSpecific, ImageDimensions>;
 }
+
+const cropScaleImageSpecific: ICropScaleImageSpecific = {
+  // Will be overwritten on launch():
+  projectId: -1,
+  setProjectId: action((state, projectId) => {
+    state.projectId = projectId;
+  }),
+
+  // Will be overwritten on launch():
+  assetName: "",
+  setAssetName: action((state, assetName) => {
+    state.assetName = assetName;
+  }),
+
+  // Will be overwritten on launch():
+  existingCrop: identityCrop,
+  setExistingCrop: action((state, existingCrop) => {
+    state.existingCrop = existingCrop;
+  }),
+
+  // Will be overwritten on launch():
+  sourceURL: new URL("data:,"),
+  setSourceURL: action((state, sourceURL) => {
+    state.sourceURL = sourceURL;
+  }),
+
+  // Will be overwritten on launch():
+  originalSize: { width: 1, height: 1 },
+  setOriginalSize: action((state, originalSize) => {
+    state.originalSize = originalSize;
+  }),
+};
