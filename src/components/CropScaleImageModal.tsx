4179381788b7fc76609bac558777b8ba5d37803a
Ben North <ben@redfrontdoor.org>
2022-10-03 19:57:04 +0100

    Define conversion functions proportion/percent

--- src/components/CropScaleImageModal.tsx
+++ src/components/CropScaleImageModal.tsx
@@ -1,6 +1,39 @@
 import React from "react";
 import { useStoreActions, useStoreState } from "../store";
 
+import { Crop as ReactCropSpec } from "react-image-crop";
+import { ImageCropSourceDescriptor } from "../model/asset";
+
+// The react-image-crop interface works in percentages but the model
+// state and the transformation functions work in proportions.  And the
+// property names are different.  The below two functions convert
+// back/forth between these two representations.
+
+/** Compute a proportion-valued (`cropOriginX`, `cropOriginY`,
+ * `cropWidth`, `cropHeight`) crop specifier given a percentage-valued
+ * (`x`, `y`, `width`, `height`) crop specifier . */
+const proportionCropFromPercentCrop = (
+  pctCrop: ReactCropSpec
+): ImageCropSourceDescriptor => ({
+  originX: 0.01 * pctCrop.x,
+  originY: 0.01 * pctCrop.y,
+  width: 0.01 * pctCrop.width,
+  height: 0.01 * pctCrop.height,
+});
+
+/** Compute a percentage-valued (`x`, `y`, `width`, `height`) crop
+ * specifier given a proportion-valued (`cropOriginX`, `cropOriginY`,
+ * `cropWidth`, `cropHeight`) crop specifier. */
+const percentCropFromProportionCrop = (
+  propCrop: ImageCropSourceDescriptor
+): ReactCropSpec => ({
+  x: 100.0 * propCrop.originX,
+  y: 100.0 * propCrop.originY,
+  width: 100.0 * propCrop.width,
+  height: 100.0 * propCrop.height,
+  unit: "%",
+});
+
 export const CropScaleImageModal = () => {
   return (
   );
