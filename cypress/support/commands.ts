49b7c4731358a662ef88482e7205573564569ff0
Ben North <ben@redfrontdoor.org>
2022-10-04 12:16:18 +0100

    Be stricter with types re optional arrays

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -58,7 +58,7 @@ Cypress.Commands.add("pytchResetDatabase", (options?: ResetDatabaseOptions) => {
 
     const allProjectNames = [
       "Test seed project",
-      ...effectiveOptions.extraProjectNames,
+      ...(effectiveOptions.extraProjectNames || []),
     ];
     const projectSummaries = await Promise.all(
       allProjectNames.map((name) => db.createNewProject(name))
@@ -69,7 +69,7 @@ Cypress.Commands.add("pytchResetDatabase", (options?: ResetDatabaseOptions) => {
     const allFixtureAssets = [
       { name: "red-rectangle-80-60.png", mimeType: "image/png" },
       { name: "sine-1kHz-2s.mp3", mimeType: "audio/mpeg" },
-      ...effectiveOptions.extraAssets,
+      ...(effectiveOptions.extraAssets || []),
     ];
 
     for (const { name, mimeType } of allFixtureAssets) {
