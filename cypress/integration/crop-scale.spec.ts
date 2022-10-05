edbb29d59b0acf2a72c05b2e67ca695dfea01690
Ben North <ben@redfrontdoor.org>
2022-10-05 12:26:30 +0100

    Add description of test image and its colours

--- cypress/integration/crop-scale.spec.ts
+++ cypress/integration/crop-scale.spec.ts
@@ -1,4 +1,20 @@
 /// <reference types="cypress" />
 
+// The bulk of this file is the description of what we expect to see as
+// we work with the test image.
+
+////////////////////////////////////////////////////////////////////////
+//
+// The image used for testing, and the colours we expect when using it.
+
+const blueOrangeImage = {
+  name: "blue-orange-crop-test.png",
+  mimeType: "image/png",
+};
+
+const emptyColour = [0, 0, 0, 0];
+const blueColour = [77, 151, 255, 255];
+const orangeColour = [255, 191, 0, 255];
+
 context("Crop and scale images", () => {
 });
