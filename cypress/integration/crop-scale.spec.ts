f86e596d32c0ec61d50a25ff5d6236542444dd97
Ben North <ben@redfrontdoor.org>
2022-10-05 12:27:02 +0100

    Add types for runs of colour
    Define types
    
        SolidColourRun
        SolidColourRuns
        PixelStripSpec
        PixelStripSpecs

--- cypress/integration/crop-scale.spec.ts
+++ cypress/integration/crop-scale.spec.ts
@@ -16,5 +16,32 @@ const emptyColour = [0, 0, 0, 0];
 const blueColour = [77, 151, 255, 255];
 const orangeColour = [255, 191, 0, 255];
 
+////////////////////////////////////////////////////////////////////////
+//
+// Types for saying what we expect to see.  We will make assertions
+// about a one-pixel-wide slice taken vertically down the rendered
+// stage.  With our image, these strips should consist of a small number
+// of runs of solid colour.  We will allow a small amount of tolerance
+// in the pixel-run specs and in the X values for where we take the
+// slices.
+
+type SolidColourRun = {
+  begin: number;
+  end: number;
+  colour: Array<number>;
+};
+
+type SolidColourRuns = Array<SolidColourRun>;
+
+// A test will describe the solid-colour runs we expect to see in
+// vertical slices taken at various horizontal offsets across the image.
+
+type PixelStripSpec = {
+  sliceOffset: number;
+  runs: SolidColourRuns;
+};
+
+type PixelStripSpecs = Array<PixelStripSpec>;
+
 context("Crop and scale images", () => {
 });
