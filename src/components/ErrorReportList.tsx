45198a819f2a3fbfc94ed09c033a26b77ce640f5
Ben North <ben@redfrontdoor.org>
2022-10-12 10:48:25 +0100

    delayedGpioErrorIntro(): Add

--- src/components/ErrorReportList.tsx
+++ src/components/ErrorReportList.tsx
@@ -261,6 +261,25 @@ const attributeWatchErrorIntro = (errorContext: any) => {
   );
 };
 
+// TODO: Convert to i18n.
+const delayedGpioErrorIntro = (errorContext: any) => {
+  const operationDescription = ((operation: any) => {
+    switch (operation.kind) {
+      case "set-output":
+        return `set GPIO pin ${operation.pin} to output level ${operation.level}`;
+      default:
+        return "perform an unknown operation";
+    }
+  })(errorContext.failedOperation);
+
+  return (
+    <p>
+      Shortly after trying to {operationDescription}, Pytch encountered this
+      error:
+    </p>
+  );
+};
+
 type ErrorIntroProps = {
   errorContext: any;
 };
