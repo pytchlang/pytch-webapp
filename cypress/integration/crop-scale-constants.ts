91bbac3a164c982d1a3e63b0563688238e08cb5e
Ben North <ben@redfrontdoor.org>
2022-12-02 10:42:30 +0000

    Move test-spec constants to own file

---
+++ cypress/integration/crop-scale-constants.ts
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////
+//
+// The image used for testing, and the colours we expect when using it.
+//
+// In separate file to allow use by specs besides "crop-scale.spec".
+
+export const blueOrangeImage = {
+  name: "blue-orange-crop-test.png",
+  mimeType: "image/png",
+};
+
+export const emptyColour = [0, 0, 0, 0];
+export const blueColour = [77, 151, 255, 255];
+export const orangeColour = [255, 191, 0, 255];
--- cypress/integration/crop-scale.spec.ts
+++ cypress/integration/crop-scale.spec.ts
@@ -1,22 +1,15 @@
 /// <reference types="cypress" />
 
 import { stageHeight } from "../../src/constants";
+import {
+  blueOrangeImage,
+  emptyColour,
+  blueColour,
+  orangeColour,
+} from "./crop-scale-constants";
 
 // The bulk of this file is the description of what we expect to see as
 // we work with the test image.
-
-////////////////////////////////////////////////////////////////////////
-//
-// The image used for testing, and the colours we expect when using it.
-
-const blueOrangeImage = {
-  name: "blue-orange-crop-test.png",
-  mimeType: "image/png",
-};
-
-const emptyColour = [0, 0, 0, 0];
-export const blueColour = [77, 151, 255, 255];
-const orangeColour = [255, 191, 0, 255];
 
 ////////////////////////////////////////////////////////////////////////
 //
