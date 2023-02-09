5554a47eb4673cfa7e0898aeb1292b96b084e45d
Ben North <ben@redfrontdoor.org>
2023-02-09 16:30:38 +0000

    Add types
    Types
    
        ClipArtGalleryItem
        ClipArtGalleryEntry
        ClipArtGalleryData

--- src/model/clipart-gallery-core.ts
+++ src/model/clipart-gallery-core.ts
@@ -1,1 +1,20 @@
 export type ClipArtGalleryEntryId = number;
+
+export type ClipArtGalleryItem = {
+  name: string;
+  relativeUrl: string;
+  size: [number, number];
+  url: string; // Populated when loaded
+};
+
+export type ClipArtGalleryEntry = {
+  id: ClipArtGalleryEntryId;
+  name: string;
+  items: Array<ClipArtGalleryItem>;
+  tags: Array<string>;
+};
+
+export type ClipArtGalleryData = {
+  entries: Array<ClipArtGalleryEntry>;
+  tags: Array<string>;
+};
