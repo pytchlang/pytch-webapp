3091653109f0c9a3f45fb323f26a750fb59c4f5b
Ben North <ben@redfrontdoor.org>
2023-03-05 17:34:47 +0000

    Replace copyright link with referral text

--- src/components/AddClipartModal.tsx
+++ src/components/AddClipartModal.tsx
@@ -262,11 +262,7 @@ export const AddClipartModal = () => {
       </Modal.Body>
       <Modal.Footer className="clipart-footer">
         <div className="licence-info">
-          <p>
-            <a href="#TODO" target="_blank">
-              Copyright and licensing information
-            </a>
-          </p>
+          <p>For copyright and licensing information, see help pages.</p>
         </div>
         <div className="buttons">
           <Button variant="secondary" onClick={() => dismiss()}>
