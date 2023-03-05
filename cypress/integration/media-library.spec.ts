e46dcb58677071c07eafc54eea95a2d89c44ece8
Ben North <ben@redfrontdoor.org>
2023-03-05 10:45:00 +0000

    Move media-library tests to own spec file

---
+++ cypress/integration/media-library.spec.ts
@@ -0,0 +1,103 @@
+/// <reference types="cypress" />
+
+context("Add clipart from library, handling errors", () => {
+  const clickAddN = (expAddN: number) => {
+    const expLabel = `Add ${expAddN} to project`;
+    cy.get("button").contains(expLabel).click();
+  };
+
+  const attemptChooseClipArt = (
+    clipArtNames: Array<string>,
+    expAddN: number
+  ) => {
+    cy.contains("Choose from library").click();
+    cy.contains("Add to project").should("be.disabled");
+    clipArtNames.forEach((clipArtName) =>
+      cy.get(".clipart-card").contains(clipArtName).click()
+    );
+    clickAddN(expAddN);
+  };
+
+  const chooseClipArt = (clipArtNames: Array<string>, expAddN: number) => {
+    attemptChooseClipArt(clipArtNames, expAddN);
+    cy.get(".modal-content").should("not.exist");
+  };
+
+  const startTestAssets = [
+    "red-rectangle-80-60.png",
+    "sine-1kHz-2s.mp3",
+    "apple.png",
+  ];
+
+  beforeEach(() => {
+    cy.pytchExactlyOneProject();
+    cy.contains("Images and sounds").click();
+    chooseClipArt(["apple.png"], 1);
+    cy.pytchShouldShowAssets(startTestAssets);
+  });
+
+  it("can add a single-item entry", () => {
+    chooseClipArt(["bird.png"], 1);
+    cy.pytchShouldShowAssets([...startTestAssets, "bird.png"]);
+  });
+
+  it("can add a multi-item entry", () => {
+    chooseClipArt(["blocks"], 2);
+    cy.pytchShouldShowAssets([
+      ...startTestAssets,
+      "block-lit.png",
+      "block-unlit.png",
+    ]);
+  });
+
+  it("can scroll through the gallery to find clipart", () => {
+    chooseClipArt(["world.png"], 1);
+    cy.pytchShouldShowAssets([...startTestAssets, "world.png"]);
+  });
+
+  it("rejects adding same clipart twice", () => {
+    attemptChooseClipArt(["apple.png"], 1);
+    cy.contains(
+      "oh, no! The selected clipart can not be added" +
+        ' (apple.png: Your project already contains an asset called "apple.png".)' +
+        " Please modify your selection."
+    );
+    cy.contains("Cancel").click();
+    cy.pytchShouldShowAssets(startTestAssets);
+  });
+
+  it("handles one failure and one success", () => {
+    attemptChooseClipArt(["apple.png", "orange.png"], 2);
+    cy.contains(
+      "1 clipart successfully added, but not the other" +
+        ' (apple.png: Your project already contains an asset called "apple.png".)' +
+        " Please modify your selection."
+    );
+    cy.contains("Cancel").click();
+    cy.pytchShouldShowAssets([...startTestAssets, "orange.png"]);
+  });
+
+  it("handles two failures and one success", () => {
+    chooseClipArt(["orange.png"], 1);
+    attemptChooseClipArt(["orange.png", "apple.png", "bird.png"], 3);
+    cy.contains(
+      "1 clipart successfully added, but not the 2 others" +
+        ' (apple.png: Your project already contains an asset called "apple.png".' +
+        ' orange.png: Your project already contains an asset called "orange.png". )' +
+        " Please modify your selection."
+    );
+    cy.contains("Cancel").click();
+    cy.pytchShouldShowAssets([...startTestAssets, "orange.png", "bird.png"]);
+  });
+
+  it("handles one failure and two successes", () => {
+    attemptChooseClipArt(["apple.png", "orange.png", "bird.png"], 3);
+    cy.contains(
+      "2 cliparts successfully added, but 1 problem encontered" +
+        ' (apple.png: Your project already contains an asset called "apple.png".)' +
+        " Please modify your selection."
+    );
+    cy.contains("Cancel").click();
+    cy.pytchShouldShowAssets([...startTestAssets, "orange.png", "bird.png"]);
+  });
+});
