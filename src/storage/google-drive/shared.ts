4c36536cfe57a27e4fa0cd6b2aafd69eb6372b32
Ben North <ben@redfrontdoor.org>
2023-05-13 16:14:18 +0100

    GoogleDriveApi: Define type

--- src/storage/google-drive/shared.ts
+++ src/storage/google-drive/shared.ts
@@ -12,3 +12,22 @@ export type AsyncFile = {
 export type AcquireTokenOptions = {
   signal: AbortSignal;
 };
+
+export interface GoogleDriveApi {
+  /** Acquire an access token for use with future API calls.  To allow
+   * external cancellation, the acquisition process listens for the
+   * `"abort"` event on the `signal` property of the given `options`. If
+   * `"abort"` is raised, the Promise returned by `acquireToken()`
+   * rejects. */
+  acquireToken(options: AcquireTokenOptions): Promise<TokenInfo>;
+
+  /** Allow user to choose a file or some files to import from, using
+   * the given `tokenInfo` to authorise.  The returned Promise can
+   * reject, for example if the user cancels the operation or if an
+   * error occurs. */
+  importFiles(tokenInfo: TokenInfo): Promise<Array<AsyncFile>>;
+
+  /** Export the data in the given `file`, using the given `tokenInfo`
+   * to authorise. */
+  exportFile(tokenInfo: TokenInfo, file: AsyncFile): Promise<void>;
+}
