064c1d3aba077be6cd0eae4f08d59c4ede6812e3
Ben North <ben@redfrontdoor.org>
2023-05-13 16:36:48 +0100

    maybeBoot(): Add thunk

--- src/model/google-drive-import-export.ts
+++ src/model/google-drive-import-export.ts
@@ -1,5 +1,6 @@
-import { Action } from "easy-peasy";
+import { Action, Thunk, thunk } from "easy-peasy";
 import { propSetterAction } from "../utils";
+import { bootApi } from "../storage/google-drive";
 
 type ApiBootStatus =
   | { kind: "not-yet-started" }
@@ -10,9 +11,28 @@ type ApiBootStatus =
 export type GoogleDriveIntegration = {
   apiBootStatus: ApiBootStatus;
   setApiBootStatus: Action<GoogleDriveIntegration, ApiBootStatus>;
+
+  maybeBoot: Thunk<GoogleDriveIntegration>;
 };
 
 export let googleDriveIntegration: GoogleDriveIntegration = {
   apiBootStatus: { kind: "not-yet-started" },
   setApiBootStatus: propSetterAction("apiBootStatus"),
+
+  maybeBoot: thunk(async (actions, _voidPayload, helpers) => {
+    const state = helpers.getState();
+    if (state.apiBootStatus.kind !== "not-yet-started") {
+      return;
+    }
+
+    try {
+      actions.setApiBootStatus({ kind: "pending" });
+      const api = await bootApi().boot();
+      actions.setApiBootStatus({ kind: "succeeded", api });
+    } catch (err) {
+      // TODO: Any useful way to report this to user?
+      console.error("GoogleDriveIntegration.maybeBoot(): boot failed", err);
+      actions.setApiBootStatus({ kind: "failed" });
+    }
+  }),
 };
