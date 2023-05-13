f45ef88caf41fa3c05d73768d92b60c505177926
Ben North <ben@redfrontdoor.org>
2023-05-13 16:14:00 +0100

    AsyncFile: Define type

--- src/storage/google-drive/shared.ts
+++ src/storage/google-drive/shared.ts
@@ -2,3 +2,9 @@ export type TokenInfo = {
   token: string;
   expiration: Date;
 };
+
+export type AsyncFile = {
+  name(): Promise<string>;
+  mimeType(): Promise<string>;
+  data(): Promise<ArrayBuffer>;
+};
