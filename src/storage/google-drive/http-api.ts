698d1b00f52be5653126447281a6ec62838b44d4
Ben North <ben@redfrontdoor.org>
2023-05-15 10:47:53 +0100

    getFileMetadata(): Define

--- src/storage/google-drive/http-api.ts
+++ src/storage/google-drive/http-api.ts
@@ -1,2 +1,16 @@
 import { kGetFileUrlBase, kPostResumableUploadUrl } from "./constants";
 import { throwIfResponseNotOk } from "./error-messages";
+
+export const getFileMetadata = async (token: string, fileId: string) => {
+  const authHeader = `Bearer ${token}`;
+  const url = `${kGetFileUrlBase}/${fileId}?fields=name,mimeType`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: { Authorization: authHeader },
+  });
+
+  await throwIfResponseNotOk("Could not get file information", response);
+
+  const responseObj = await response.json();
+  return { name: responseObj.name, mimeType: responseObj.mimeType };
+};
