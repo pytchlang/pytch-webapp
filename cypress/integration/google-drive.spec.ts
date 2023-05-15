c40d763ecb81d66125154d7285b5cf9566dcc874
Ben North <ben@redfrontdoor.org>
2023-05-15 10:39:43 +0100

    Define test helper options constant

--- cypress/integration/google-drive.spec.ts
+++ cypress/integration/google-drive.spec.ts
@@ -4,4 +4,11 @@ import { AsyncFile } from "../../src/storage/google-drive";
 import { MockApiBehaviour } from "../../src/storage/google-drive/mock";
 
 context("Google Drive import and export", () => {
+  const setApiBehaviourOpts = (behaviour: MockApiBehaviour) => ({
+    extraWindowActions: [
+      async (window: Window) => {
+        (window as any).$GoogleDriveApiBehaviour = behaviour;
+      },
+    ],
+  });
 });
