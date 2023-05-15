109b936a9fd50ece36b02ad14c20c42dcd5a901b
Ben North <ben@redfrontdoor.org>
2023-05-15 10:29:07 +0100

    GoogleOperationModals: Add skeleton file

---
+++ src/components/GoogleOperationModals.tsx
@@ -0,0 +1,4 @@
+import { Button, Modal, Spinner } from "react-bootstrap";
+import { useStoreState, useStoreActions } from "../store";
+import { useEffect } from "react";
+import { assertNever } from "../utils";
