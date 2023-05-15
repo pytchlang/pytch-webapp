1da678aaf0840e927e5203e8c0998e742dbc0f3a
Ben North <ben@redfrontdoor.org>
2023-05-15 10:30:29 +0100

    GoogleTaskStatusModal: Add

--- src/components/GoogleOperationModals.tsx
+++ src/components/GoogleOperationModals.tsx
@@ -51,3 +51,112 @@ export const GoogleAuthenticationStatusModal = () => {
       return assertNever(authState);
   }
 };
+
+export const GoogleTaskStatusModal = () => {
+  const taskState = useStoreState(
+    (state) => state.googleDriveImportExport.taskState
+  );
+  const setTaskState = useStoreActions(
+    (actions) => actions.googleDriveImportExport.setTaskState
+  );
+
+  switch (taskState.kind) {
+    case "idle":
+    case "pending-already-modal":
+      return null;
+    case "pending": {
+      return (
+        <Modal
+          className="GoogleTaskStatusModal"
+          show={true}
+          animation={false}
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title>{taskState.summary}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="pending">
+            <Spinner animation="border" />
+          </Modal.Body>
+        </Modal>
+      );
+    }
+    case "done": {
+      const dismiss = () => setTaskState({ kind: "idle" });
+
+      const taskOutcome = taskState.outcome;
+
+      const nSuccesses = taskOutcome.successes.length;
+      const successIntro =
+        nSuccesses === 1 ? (
+          <p>
+            The following operation was <strong>successful</strong>:
+          </p>
+        ) : (
+          <p>
+            The following operations were <strong>successful</strong>:
+          </p>
+        );
+
+      const nFailures = taskOutcome.failures.length;
+      const failureIntro =
+        nFailures === 1 ? (
+          <p>
+            The following <strong>problem</strong> occurred:
+          </p>
+        ) : (
+          <p>
+            The following <strong>problems</strong> occurred:
+          </p>
+        );
+
+      const successDiv =
+        nSuccesses === 0 ? null : (
+          <div className="outcome-summary successes">
+            {successIntro}
+            <ul>
+              {taskState.outcome.successes.map((s, idx) => (
+                <li key={idx}>{s}</li>
+              ))}
+            </ul>
+          </div>
+        );
+
+      const failureDiv =
+        nFailures === 0 ? null : (
+          <div className="outcome-summary failures">
+            {failureIntro}
+            <ul>
+              {taskState.outcome.failures.map((s, idx) => (
+                <li key={idx}>{s}</li>
+              ))}
+            </ul>
+          </div>
+        );
+
+      return (
+        <Modal
+          className="GoogleTaskStatusModal"
+          show={true}
+          animation={false}
+          centered
+        >
+          <Modal.Header>
+            <Modal.Title>{taskState.summary}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {successDiv}
+            {failureDiv}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={dismiss}>
+              OK
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    }
+    default:
+      return assertNever(taskState);
+  }
+};
