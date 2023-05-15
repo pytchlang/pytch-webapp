6f2499aa06ea0285365ecd61aa09f607a5845e22
Ben North <ben@redfrontdoor.org>
2023-05-15 10:39:26 +0100

    Add skeleton test spec file

---
+++ cypress/integration/google-drive.spec.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+import { AsyncFile } from "../../src/storage/google-drive";
+import { MockApiBehaviour } from "../../src/storage/google-drive/mock";
+
+context("Google Drive import and export", () => {
+});
