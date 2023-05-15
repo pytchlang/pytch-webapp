5ce42acd3f674a65333a245c8f01715de7ec93f2
Ben North <ben@redfrontdoor.org>
2023-05-15 10:57:56 +0100

    acquireToken(): Define

--- src/storage/google-drive/real.ts
+++ src/storage/google-drive/real.ts
@@ -7,4 +7,43 @@ const loadGapiClient = (gapi: any, libraries: string): Promise<void> =>
   });
 
 const realApi = (google: any, tokenClient: any): GoogleDriveApi => {
+  function acquireToken(options: AcquireTokenOptions): Promise<TokenInfo> {
+    const { signal } = options;
+
+    return new Promise<TokenInfo>((resolve, reject) => {
+      const doUserCancel = () => reject(new Error(signal.reason));
+      signal.addEventListener("abort", doUserCancel, { once: true });
+
+      tokenClient.callback = (response: any) => {
+        const maybeError = response.error;
+        if (maybeError !== undefined) {
+          console.error("tokenClient.callback(): error:", response);
+          if (signal.aborted) {
+            console.log("already abort()'d with", signal.reason);
+          } else {
+            signal.removeEventListener("abort", doUserCancel);
+            reject(
+              new Error(`Could not log in to Google (code "${maybeError}")`)
+            );
+          }
+        } else {
+          console.log("tokenClient.callback(): non-error:", response);
+          if (signal.aborted) {
+            console.log("already abort()'d with", signal.reason);
+          } else {
+            // TODO: Also get user info?
+            signal.removeEventListener("abort", doUserCancel);
+            resolve({
+              token: response.access_token,
+              expiration: new Date(), // TODO: Get from response.expires_in
+            });
+          }
+        }
+      };
+
+      // Empty prompt means "The user will be prompted only the first
+      // time your app requests access".
+      tokenClient.requestAccessToken({ prompt: "" });
+    });
+  }
 };
