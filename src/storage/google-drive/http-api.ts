4ff43045590ce2731e5756fd9ab894e604c8b406
Ben North <ben@redfrontdoor.org>
2023-05-15 10:48:02 +0100

    getFileContent(): Add

--- src/storage/google-drive/http-api.ts
+++ src/storage/google-drive/http-api.ts
@@ -14,3 +14,18 @@ export const getFileMetadata = async (token: string, fileId: string) => {
   const responseObj = await response.json();
   return { name: responseObj.name, mimeType: responseObj.mimeType };
 };
+
+export const getFileContent = async (token: string, fileId: string) => {
+  // TODO: Stream in the body and thereby provide a progress indicator?
+
+  const authHeader = `Bearer ${token}`;
+  const url = `${kGetFileUrlBase}/${fileId}?alt=media`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: { Authorization: authHeader },
+  });
+
+  await throwIfResponseNotOk("Could not get file contents", response);
+
+  return await response.arrayBuffer();
+};
