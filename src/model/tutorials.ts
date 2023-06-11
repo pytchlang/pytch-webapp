8c3ccfc15779b85b22611417cd929de48380df44
Sara Fiori <fioris@tcd.ie>
2023-06-11 21:02:48 +0100

    SingleTutorialDisplayKind: Add new kind

--- src/model/tutorials.ts
+++ src/model/tutorials.ts
@@ -14,7 +14,7 @@ import { batch } from "react-redux";
 import { ITrackedTutorialRef } from "./projects";
 import { PytchProgram, PytchProgramOps } from "./pytch-program";
 
-export type SingleTutorialDisplayKind = "tutorial-only" | "tutorial-and-demo";
+export type SingleTutorialDisplayKind = "tutorial-only" | "tutorial-and-demo" | "tutorial-demo-and-share";
 
 export interface ITutorialSummary {
   slug: string;
