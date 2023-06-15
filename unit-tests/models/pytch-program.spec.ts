5b776f892581cef39d3f1eb51e40445a1f0dd454
Ben North <ben@redfrontdoor.org>
2023-06-15 16:48:17 +0100

    Add test helper function and constant

--- unit-tests/models/pytch-program.spec.ts
+++ unit-tests/models/pytch-program.spec.ts
@@ -2,4 +2,10 @@ import { assert } from "chai";
 import { PytchProgram, PytchProgramOps } from "../../src/model/pytch-program";
 
 describe("PytchProgram operations", () => {
+  function assertFlatPython(program: PytchProgram, expCodeText: string) {
+    assert.equal(program.kind, "flat");
+    assert.equal(program.text, expCodeText);
+  }
+
+  const codeText = "import pytch\nprint(42)\n";
 });
