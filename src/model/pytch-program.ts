fcf8573ca9945df3f6996cc6fabbf4eff638c96a
Ben North <ben@redfrontdoor.org>
2023-06-15 16:42:52 +0100

    fromPythonCode(): Define

--- src/model/pytch-program.ts
+++ src/model/pytch-program.ts
@@ -1,4 +1,9 @@
 export type PytchProgram = { kind: "flat"; text: string };
 
 export const PytchProgramOps = {
+  /** Return a new `PytchProgram` instance of kind `"flat"` and with the
+   * given `text`. */
+  fromPythonCode(text: string): PytchProgram {
+    return { kind: "flat", text };
+  },
 };
