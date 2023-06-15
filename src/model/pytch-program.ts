fd866341710d94a7f16ae694a8df708dfa80a9b2
Ben North <ben@redfrontdoor.org>
2023-06-15 16:43:28 +0100

    fromJson(): Define

--- src/model/pytch-program.ts
+++ src/model/pytch-program.ts
@@ -1,3 +1,12 @@
+// To regenerate the JavaScript after updating the schema file
+// "pytch-program-schema.json", be in the same directory as
+// this file, then run:
+//
+//   ./refresh-pytch-program-json-validation.sh
+//
+import _validatePytchProgramJson from "./pytch-program-json-validation";
+const validatePytchProgramJson = _validatePytchProgramJson as any;
+
 export type PytchProgram = { kind: "flat"; text: string };
 
 export const PytchProgramOps = {
@@ -5,5 +14,24 @@ export const PytchProgramOps = {
    * given `text`. */
   fromPythonCode(text: string): PytchProgram {
     return { kind: "flat", text };
+  },
+
+  /** Attempt to parse the given `json` string as the JSON
+   * representation of a `PytchProgram`, and return the resulting
+   * `PytchProgram` if successful.  If not successful, throw an error.
+   * */
+  fromJson(json: string): PytchProgram {
+    let obj: any = null;
+    try {
+      obj = JSON.parse(json);
+    } catch {
+      throw new Error("malformed JSON for PytchProgram");
+    }
+
+    if (!validatePytchProgramJson(obj)) {
+      throw new Error("invalid JSON for PytchProgram");
+    }
+
+    return obj;
   },
 };
