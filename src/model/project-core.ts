559e1e7a86a3d6fbbaccf0d3d35743b0d4463163
Ben North <ben@redfrontdoor.org>
2023-06-18 11:06:56 +0100

    StoredProjectData: Define generic type

--- src/model/project-core.ts
+++ src/model/project-core.ts
@@ -6,3 +6,11 @@ export interface ITrackedTutorial {
   content: ITutorialContent;
   activeChapterIndex: number;
 }
+
+export type StoredProjectData<AssetT> = {
+  id: ProjectId;
+  name: string;
+  codeText: string;
+  assets: Array<AssetT>;
+  trackedTutorial?: ITrackedTutorial;
+};
