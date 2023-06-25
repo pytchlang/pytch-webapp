4687a37ac855242d895a15fb36f70ab9767cade8
Ben North <ben@redfrontdoor.org>
2023-06-25 18:05:33 +0100

    CoordsChooser: Define model slice type and value

--- src/model/coordinates-chooser.ts
+++ src/model/coordinates-chooser.ts
@@ -1,6 +1,22 @@
+import { Action, Thunk, action, thunk } from "easy-peasy";
+import { IPytchAppModel } from ".";
+
 type StateKind = "idle" | "active" | "active-with-copied-message";
 
 type State = {
   seqnum: number;
   kind: StateKind;
+};
+
+export type CoordsChooser = State & {
+  _setState: Action<CoordsChooser, State>;
+};
+
+export let coordsChooser: CoordsChooser = {
+  seqnum: 4001,
+  kind: "idle",
+  _setState: action((state, { seqnum, kind }) => {
+    state.seqnum = seqnum;
+    state.kind = kind;
+  }),
 };
