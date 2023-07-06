10c276d8175d26c104a402a82712f90f755d64c4
Ben North <ben@redfrontdoor.org>
2023-07-06 21:29:35 +0100

    NavigateArgs: Define type

--- src/model/navigation-request-queue.ts
+++ src/model/navigation-request-queue.ts
@@ -1,2 +1,8 @@
 import { Action, Thunk, action, thunk } from "easy-peasy";
 import { IPytchAppModel } from ".";
+import { NavigateOptions } from "react-router-dom";
+
+export type NavigateArgs = {
+  path: string;
+  opts?: NavigateOptions;
+};
