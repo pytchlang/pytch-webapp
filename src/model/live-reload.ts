ee9f41f843695cd20517f46a059effcad2e10950
Ben North <ben@redfrontdoor.org>
2023-07-07 20:51:34 +0100

    Handle live-reload state and constants locally
    And update env.var name at the same time: change to VITE_ prefix, and
    shorten.

--- src/model/live-reload.ts
+++ src/model/live-reload.ts
@@ -1,6 +1,11 @@
 import { thunk, Thunk } from "easy-peasy";
 import { IPytchAppModel } from ".";
-import { liveReloadEnabled, liveReloadURL } from "../constants";
+import { envVarOrDefault } from "../env-utils";
+
+export const isEnabled =
+  envVarOrDefault("VITE_LIVE_RELOAD_WEBSOCKET", "no") === "yes";
+
+export const liveReloadURL = "ws://127.0.0.1:4111/";
 
 // This is functionality for developers, so don't bother with retrying
 // the connection.  We will log a message if we hit an error, though, so
@@ -8,15 +13,17 @@ import { liveReloadEnabled, liveReloadURL } from "../constants";
 // watch server.
 
 export interface IReloadServer {
+  isEnabled: boolean;
   webSocket: WebSocket | null;
   maybeConnect: Thunk<IReloadServer, void, void, IPytchAppModel>;
 }
 
 export const reloadServer: IReloadServer = {
+  isEnabled,
   webSocket: null,
 
   maybeConnect: thunk((_actions, _voidPayload, helpers) => {
-    if (!liveReloadEnabled) return;
+    if (!isEnabled) return;
 
     // In general it's a bad idea to mutate state within a thunk, but we
     // want to ensure we assign to the event handlers of the WebSocket
