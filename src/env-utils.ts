716e1cdf77b374a4a746acea62f934feb6f7907e
Ben North <ben.north@tcd.ie>
2023-07-12 21:32:15 +0100

    envVarMap: Populate in main.tsx, incl BASE_URL
    Removing mention of "import.meta" from env-utils.ts.

--- src/env-utils.ts
+++ src/env-utils.ts
@@ -1,19 +1,3 @@
-// This is a bit repetitive, but Vite does not support dynamic expansion
-// of env.vars.  Gather all the ones we use in one place.
-const allViteEnvVars = new Map<string, string | null>([
-  ["VITE_DEMOS_BASE", import.meta.env.VITE_DEMOS_BASE],
-  ["VITE_DEPLOY_BASE_URL", import.meta.env.VITE_DEPLOY_BASE_URL],
-  ["VITE_LIVE_RELOAD_WEBSOCKET", import.meta.env.VITE_LIVE_RELOAD_WEBSOCKET],
-  ["VITE_GOOGLE_API_KEY", import.meta.env.VITE_GOOGLE_API_KEY],
-  ["VITE_GOOGLE_APP_ID", import.meta.env.VITE_GOOGLE_APP_ID],
-  ["VITE_GOOGLE_CLIENT_ID", import.meta.env.VITE_GOOGLE_CLIENT_ID],
-  ["VITE_MEDIALIB_BASE", import.meta.env.VITE_MEDIALIB_BASE],
-  ["VITE_SKULPT_BASE", import.meta.env.VITE_SKULPT_BASE],
-  ["VITE_TUTORIALS_BASE", import.meta.env.VITE_TUTORIALS_BASE],
-  ["VITE_USE_REAL_GOOGLE_DRIVE", import.meta.env.VITE_USE_REAL_GOOGLE_DRIVE],
-  ["VITE_VERSION_TAG", import.meta.env.VITE_VERSION_TAG],
-]);
-
 class EnvVarMap {
   map: Map<string, string | null>;
 
