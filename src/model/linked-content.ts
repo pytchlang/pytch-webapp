027d80882a1e33d42babb8a2c664aa1d1ddbfcfe
Ben North <ben.north@tcd.ie>
2023-07-28 10:06:25 +0100

    LinkedContentRef: Define type
    And constant LinkedContentRefNone.

--- src/model/linked-content.ts
+++ src/model/linked-content.ts
@@ -1,1 +1,7 @@
 export type SpecimenContentHash = string;
+
+export type LinkedContentRef =
+  | { kind: "none" }
+  | { kind: "specimen"; specimenContentHash: SpecimenContentHash };
+
+export const LinkedContentRefNone: LinkedContentRef = { kind: "none" };
