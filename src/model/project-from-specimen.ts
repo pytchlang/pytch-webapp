cfe9cc7d32c80010c14ded068c710e6fe974c7eb
Ben North <ben.north@tcd.ie>
2023-07-28 10:32:27 +0100

    ProjectFromSpecimenState: Define type

--- src/model/project-from-specimen.ts
+++ src/model/project-from-specimen.ts
@@ -1,3 +1,4 @@
+import { IProjectSummary } from "./projects";
 import { ProjectId } from "./project-core";
 import {
   LessonDescriptor,
@@ -6,3 +7,16 @@ import {
 export type StartAfreshOption =
   | { kind: "create"; lesson: LessonDescriptor }
   | { kind: "open-existing-identical"; projectId: ProjectId };
+
+type ProjectFromSpecimenState =
+  | { state: "not-yet-booted" }
+  | { state: "fetching" }
+  | {
+      state: "awaiting-user-choice";
+      projectName: string;
+      startAfreshOption: StartAfreshOption;
+      existingProjectOptions: Array<IProjectSummary>;
+    }
+  | { state: "creating-new" }
+  | { state: "redirecting" }
+  | { state: "failed"; message: string };
