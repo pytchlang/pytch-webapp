5156f736536d9e87aba7560b7413d6996a4d011d
Ben North <ben.north@tcd.ie>
2023-07-28 10:36:19 +0100

    redirectToProject(): Add thunk

--- src/model/project-from-specimen.ts
+++ src/model/project-from-specimen.ts
@@ -1,6 +1,7 @@
 import { Action, action, Thunk, thunk } from "easy-peasy";
 import { assertNever, propSetterAction } from "../utils";
 import { IProjectSummary } from "./projects";
+import { IPytchAppModel } from ".";
 import { ProjectId } from "./project-core";
 import {
   LessonDescriptor,
@@ -26,12 +27,32 @@ type ProjectFromSpecimenState =
 export type ProjectFromSpecimenFlow = {
   state: ProjectFromSpecimenState;
   setState: Action<ProjectFromSpecimenFlow, ProjectFromSpecimenState>;
+
+  redirectToProject: Thunk<
+    ProjectFromSpecimenFlow,
+    ProjectId,
+    void,
+    IPytchAppModel,
+    void
+  >;
+
   fail: Action<ProjectFromSpecimenFlow, string>;
 };
 
 export let projectFromSpecimenFlow: ProjectFromSpecimenFlow = {
   state: { state: "not-yet-booted" },
   setState: propSetterAction("state"),
+
+  redirectToProject: thunk((actions, projectId, helpers) => {
+    const allActions = helpers.getStoreActions();
+    actions.setState({ state: "redirecting" });
+
+    allActions.navigationRequestQueue.enqueue({
+      path: `/ide/${projectId}`,
+      opts: { replace: true },
+    });
+  }),
+
   fail: action((state, message) => {
     state.state = { state: "failed", message };
   }),
