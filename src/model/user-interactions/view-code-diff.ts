a4e12ab69d1279ded05cfd48d98ed05c89b2a993
Ben North <ben.north@tcd.ie>
2023-07-29 17:01:49 +0100

    Add skeleton model slice and value

---
+++ src/model/user-interactions/view-code-diff.ts
@@ -0,0 +1,8 @@
+import { Action, Thunk, thunk } from "easy-peasy";
+import { propSetterAction } from "../../utils";
+
+export type ViewCodeDiff = {
+};
+
+export let viewCodeDiff: ViewCodeDiff = {
+};
