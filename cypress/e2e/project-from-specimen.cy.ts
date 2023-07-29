a631153414c911627b44fc128918b38a3862100e
Ben North <ben.north@tcd.ie>
2023-07-29 11:33:28 +0100

    Set up fetch() interceptions

--- cypress/e2e/project-from-specimen.cy.ts
+++ cypress/e2e/project-from-specimen.cy.ts
@@ -2,5 +2,16 @@
 
 context("Create project from specimen", () => {
   it("behaves correctly", () => {
+    cy.intercept("GET", "**/hello-world-lesson.zip", {
+      fixture: "lesson-specimens/hello-world-lesson.zip",
+    });
+    cy.intercept(
+      "GET",
+      "**/_by_content_hash_/" +
+        "b7996f4c5671125f19c085a2102e8d673e8459109ff27daef4db652fe09e1663.zip",
+      {
+        fixture: "lesson-specimens/hello-world-lesson.zip",
+      }
+    );
   });
 });
