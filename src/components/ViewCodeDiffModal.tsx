299fd12068445b74f2c2da888139e80c825c75a4
Ben North <ben.north@tcd.ie>
2023-07-29 17:14:45 +0100

    PreTableDatum: Define local component

--- src/components/ViewCodeDiffModal.tsx
+++ src/components/ViewCodeDiffModal.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { EmptyProps } from "../utils";
 
+type PreTableDatumProps = { content: string | undefined };
+const PreTableDatum: React.FC<PreTableDatumProps> = ({ content }) => (
+  <td>
+    <pre>{content ?? ""}</pre>
+  </td>
+);
+
 export const ViewCodeDiffModal: React.FC<EmptyProps> = () => {
 };
