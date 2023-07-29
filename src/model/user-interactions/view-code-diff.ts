214b889b470ed927e53f26daf938e1bade41b361
Ben North <ben.north@tcd.ie>
2023-07-29 17:02:56 +0100

    CodeDiffHunk: Define type

--- src/model/user-interactions/view-code-diff.ts
+++ src/model/user-interactions/view-code-diff.ts
@@ -1,6 +1,12 @@
 import { Action, Thunk, thunk } from "easy-peasy";
 import { propSetterAction } from "../../utils";
 
+export type CodeDiffHunk =
+  | { kind: "context"; commonLines: Array<string> }
+  | { kind: "change"; aLines: Array<string>; bLines: Array<string> }
+  | { kind: "add"; bLines: Array<string> }
+  | { kind: "del"; aLines: Array<string> };
+
 export type ViewCodeDiff = {
 };
 
