3ad8bcab970043e86152bf88317cab85048400c7
Ben North <ben.north@tcd.ie>
2023-09-01 15:33:15 +0100

    Define component

---
+++ src/components/Junior/AddSomethingButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export type AddSomethingButtonProps = {
+  onClick: React.MouseEventHandler;
+};
+
+export const AddSomethingButton: React.FC<AddSomethingButtonProps> = ({
+  onClick,
+}) => {
+  return (
+    <div className="AddSomethingButton">
+      <div onClick={onClick}>
+        <span>
+          <FontAwesomeIcon className="fa-lg" icon="plus" />
+        </span>
+      </div>
+    </div>
+  );
+};
