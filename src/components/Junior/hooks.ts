d8d19932d0fdfc0e6bfce09bdfb456edcbeb3a3d
Ben North <ben.north@tcd.ie>
2023-09-01 15:25:15 +0100

    useJrEditState(), useJrEditActions(): Define hooks

--- src/components/Junior/hooks.ts
+++ src/components/Junior/hooks.ts
@@ -1,5 +1,8 @@
+import { State, Actions } from "easy-peasy";
 import { PytchProgramOps } from "../../model/pytch-program";
-import { useStoreState } from "../../store";
+import { useStoreActions, useStoreState } from "../../store";
+
+import { EditState } from "../../model/junior/edit-state";
 
 export const useStructuredProgram = () =>
   useStoreState(
@@ -10,3 +13,20 @@ export const useStructuredProgram = () =>
         "per-method"
       ).program
   );
+
+type JrEditStateMapper<R> = (state: State<EditState>) => R;
+type JrEditActionsMapper<R> = (actions: Actions<EditState>) => R;
+
+/** Like `useStoreState()`, but just within the top-level `jrEditState`
+ * model slice.  The given `mapState` function is passed
+ * `state.jrEditState` rather than the top-level `state`. */
+export function useJrEditState<R>(mapState: JrEditStateMapper<R>): R {
+  return useStoreState((state) => mapState(state.jrEditState));
+}
+
+/** Like `useStoreActions()`, but just within the top-level
+ * `jrEditState` model slice.  The given `mapActions` function is passed
+ * `actions.jrEditState` rather than the top-level `actions`. */
+export function useJrEditActions<R>(mapActions: JrEditActionsMapper<R>): R {
+  return useStoreActions((actions) => mapActions(actions.jrEditState));
+}
