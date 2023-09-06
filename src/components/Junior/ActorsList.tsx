267d4cec04269075bf417955ad489aff9a938cba
Ben North <ben.north@tcd.ie>
2023-09-06 15:46:36 +0100

    ActorThumbnail: Define local component

--- src/components/Junior/ActorsList.tsx
+++ src/components/Junior/ActorsList.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 import { Lorem } from "./Lorem";
+import {
+  AssetMetaDataOps,
+  Uuid,
+} from "../../model/junior/structured-program";
+import { useStoreState } from "../../store";
+import { AssetImageThumbnail } from "../AssetImageThumbnail";
+
+type ActorThumbnailProps = { id: Uuid };
+const ActorThumbnail: React.FC<ActorThumbnailProps> = ({ id }) => {
+  const maybeFirstImage = useStoreState((state) =>
+    AssetMetaDataOps.firstMatching(
+      state.activeProject.project.assets,
+      id,
+      "image"
+    )
+  );
+
+  const wrap = (content: JSX.Element) => (
+    <div className="thumbnail">{content}</div>
+  );
+
+  if (maybeFirstImage == null) {
+    return wrap(<div className="asset-preview">[No costumes]</div>);
+  }
+
+  if (maybeFirstImage.presentation.kind !== "image") {
+    throw new Error(
+      "expecting an image but presentation is of kind " +
+        `"${maybeFirstImage.presentation.kind}"`
+    );
+  }
+
+  return wrap(
+    <AssetImageThumbnail
+      image={maybeFirstImage.presentation.image}
+      maxSize={60}
+    />
+  );
+};
 
 export const ActorsList = () => {
   return (
