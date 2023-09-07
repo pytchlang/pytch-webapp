1cf5978c3cda3d54dc1f5c4e5445e06396cb09fe
Ben North <ben.north@tcd.ie>
2023-09-07 17:42:13 +0100

    AppearancesList: Populate with <AppearancesContent>

--- src/components/Junior/AppearancesList.tsx
+++ src/components/Junior/AppearancesList.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Lorem } from "./Lorem";
 import {
   ActorKind,
   ActorKindOps,
+  StructuredProgramOps,
 } from "../../model/junior/structured-program";
 import { AssetPresentation } from "../../model/asset";
 import { AppearanceCard } from "./AppearanceCard";
 import { NoContentHelp } from "./NoContentHelp";
+import { useJrEditState } from "./hooks";
+import { useStoreState } from "../../store";
+import { PytchProgramOps } from "../../model/pytch-program";
 
 type AppearancesContentProps = {
   actorKind: ActorKind;
@@ -36,12 +39,39 @@ const AppearancesContent: React.FC<AppearancesContentProps> = ({
 };
 
 export const AppearancesList = () => {
+  const assets = useStoreState((state) => state.activeProject.project.assets);
+  const focusedActorId = useJrEditState((s) => s.focusedActor);
+
+  // The following can throw; what happens?
+  const focusedActor = useStoreState((state) =>
+    StructuredProgramOps.uniqueActorById(
+      PytchProgramOps.ensureKind(
+        "<AppearancesList>",
+        state.activeProject.project.program,
+        "per-method"
+      ).program,
+      focusedActorId
+    )
+  );
+
+  const content = (() => {
+    // These startswith() calls feel a bit dodgy.
+    const actorAssets = assets.filter(
+      (a) =>
+        a.name.startsWith(focusedActorId) &&
+        a.assetInProject.mimeType.startsWith("image/")
+    );
+    return (
+      <AppearancesContent
+        actorKind={focusedActor.kind}
+        appearances={actorAssets}
+      />
+    );
+  })();
+
   return (
     <div className="abs-0000-oflow">
-      <div className="Junior-AppearancesList">
-        <h2>AppearancesList</h2>
-        <Lorem />
-      </div>
+      <div className="Junior-AppearancesList">{content}</div>
     </div>
   );
 };
