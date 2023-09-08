d86f22379a19a097d71a0f3247db6f5f3b90d281
Ben North <ben.north@tcd.ie>
2023-09-08 16:22:07 +0100

    operation: Add slot and individual setters

--- src/model/junior/upsert-hat-block.ts
+++ src/model/junior/upsert-hat-block.ts
@@ -11,8 +11,26 @@ import {
 import { IPytchAppModel } from "..";
 import { assertNever, propSetterAction } from "../../utils";
 
+import {
+  HandlerUpsertionAction,
+  HandlerUpsertionOperation,
+} from "./structured-program/program";
+import { Uuid } from "./structured-program/core-types";
+
 type IUpsertHatBlockSpecific = {
+  operation: HandlerUpsertionOperation;
+
+  setActorId: Action<IUpsertHatBlockSpecific, Uuid>;
+  setAction: Action<IUpsertHatBlockSpecific, HandlerUpsertionAction>;
 };
 
 const upsertHatBlockSpecific: IUpsertHatBlockSpecific = {
+  operation: { actorId: "", action: { kind: "insert" } },
+
+  setActorId: action((state, actorId) => {
+    state.operation.actorId = actorId;
+  }),
+  setAction: action((state, action) => {
+    state.operation.action = action;
+  }),
 };
