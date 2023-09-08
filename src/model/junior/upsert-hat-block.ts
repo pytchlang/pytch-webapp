6b032b0ce54463fff83a6cdb1be909b2d51b0cb6
Ben North <ben.north@tcd.ie>
2023-09-08 16:18:57 +0100

    Define model-slice skeleton

---
+++ src/model/junior/upsert-hat-block.ts
@@ -0,0 +1,18 @@
+import {
+  action,
+  Action,
+  Actions,
+  computed,
+  Computed,
+  thunk,
+  Thunk,
+} from "easy-peasy";
+
+import { IPytchAppModel } from "..";
+import { assertNever, propSetterAction } from "../../utils";
+
+type IUpsertHatBlockSpecific = {
+};
+
+const upsertHatBlockSpecific: IUpsertHatBlockSpecific = {
+};
