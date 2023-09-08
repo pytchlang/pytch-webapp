317ce93591e2be9f81ed84e9261a39fbb3d27c92
Ben North <ben.north@tcd.ie>
2023-09-08 19:25:56 +0100

    ScriptsEditor: Include <AddHandlerButton>

--- src/components/Junior/CodeEditor.tsx
+++ src/components/Junior/CodeEditor.tsx
@@ -53,7 +53,10 @@ const ScriptsEditor = () => {
   const nHandlers = handlerIds.length;
 
   const wrap = (content: JSX.Element) => (
-    <div className="Junior-ScriptsEditor">{content}</div>
+    <div className="Junior-ScriptsEditor">
+      {content}
+      <AddHandlerButton />
+    </div>
   );
 
   if (nHandlers === 0) {
