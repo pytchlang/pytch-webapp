db120f8e1e1cf0d8f91bd9df803f4b7f9969f23f
Ben North <ben.north@tcd.ie>
2023-09-08 16:55:36 +0100

    Modals: Include UpsertHandlerModal

--- src/components/Junior/Modals.tsx
+++ src/components/Junior/Modals.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { AddSpriteModal } from "./AddSpriteModal";
 import { AddJrAssetsModal } from "./AddAssetsModal";
+import { UpsertHandlerModal } from "./UpsertHandlerModal";
 
 export const Modals = () => {
   return (
     <>
       <AddSpriteModal />
       <AddJrAssetsModal />
+      <UpsertHandlerModal />
     </>
   );
 };
