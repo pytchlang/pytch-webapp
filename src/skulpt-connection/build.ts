03ca9a3b7ad10dedbc32af89540df6c16e06b130
Ben North <ben.north@tcd.ie>
2023-09-08 19:59:12 +0100

    build(): Provide assets arg to flatCodeText()

--- src/skulpt-connection/build.ts
+++ src/skulpt-connection/build.ts
@@ -52,7 +52,10 @@ export const build = async (
     Sk.pytch.async_load_image = (name: string) => {
       return assetServer.loadImage(name);
     };
-    const flattenedProgram = PytchProgramOps.flatCodeText(project.program);
+    const flattenedProgram = PytchProgramOps.flatCodeText(
+      project.program,
+      project.assets
+    );
     const codeText = flattenedProgram.code;
     await Sk.pytchsupport.import_with_auto_configure(codeText);
     Sk.pytch.current_live_project.on_green_flag_clicked();
