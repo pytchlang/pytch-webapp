56e2aa810b00e2cff10631baa8f13fcd1fa20e50
Ben North <ben.north@tcd.ie>
2023-09-08 10:30:55 +0100

    (Add comments)

--- src/components/Junior/AppearancesTabTitle.tsx
+++ src/components/Junior/AppearancesTabTitle.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import classNames from "classnames";
 
+// Ensure that the "Backdrops" or "Costumes" tab is always the same
+// width.  Otherwise we get an annoying jitter as you switch between the
+// stage and a sprite.
+
 // TODO: Is there a way to compute this?
 const allDisplayTitleValues = ["Costumes", "Backdrops"];
 // And then use a tighter type for "value" here?
