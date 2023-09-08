a921a90e08996433db7a6bb17a0c51d49b5d7829
Ben North <ben.north@tcd.ie>
2023-09-08 10:36:14 +0100

    SoundsContent: Define local component

--- src/components/Junior/SoundsList.tsx
+++ src/components/Junior/SoundsList.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 import { Lorem } from "./Lorem";
+import { AssetPresentation } from "../../model/asset";
+import {
+  ActorKind,
+} from "../../model/junior/structured-program";
+import { SoundCard } from "./SoundCard";
+import { NoContentHelp } from "./NoContentHelp";
+
+type SoundsContentProps = {
+  actorKind: ActorKind;
+  sounds: Array<AssetPresentation>;
+};
+const SoundsContent = ({ actorKind, sounds }: SoundsContentProps) => {
+  if (sounds.length === 0)
+    return <NoContentHelp actorKind={actorKind} contentKind="sounds" />;
+
+  return (
+    <>
+      {sounds.map((a) => (
+        <SoundCard
+          key={a.id}
+          actorKind={actorKind}
+          assetPresentation={a}
+          fullPathname={a.name}
+        />
+      ))}
+    </>
+  );
+};
 
 export const SoundsList = () => {
   return (
