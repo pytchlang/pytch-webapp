cfb1e653c67f5b25bb748891174498f6e66d5cd5
Ben North <ben.north@tcd.ie>
2023-09-10 10:36:47 +0100

    Set up each test with simple PytchJr project

--- cypress/e2e/junior/error-reporting.cy.ts
+++ cypress/e2e/junior/error-reporting.cy.ts
@@ -1,2 +1,14 @@
 context("Interact with errors", () => {
+  beforeEach(() => {
+    // Initial ** is to match the fetched URL both when running
+    // development server and when serving a deployment zipfile.
+    cy.intercept("GET", "**/cypress/simple-pytchjr-project.zip", {
+      fixture: "project-zipfiles/simple-pytchjr-project.zip",
+    });
+    cy.pytchResetDatabase({
+      initialUrl: "/suggested-demo/cypress/simple-pytchjr-project",
+    });
+    cy.get("button").contains("Demo").click();
+    cy.pytchGreenFlag();
+  });
 });
