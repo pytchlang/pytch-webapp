60f3764589027589144fba3f572e981394845751
Ben North <ben.north@tcd.ie>
2023-09-10 21:38:36 +0100

    AppearancesContent: Define local component

--- src/components/Junior/AppearancesList.tsx
+++ src/components/Junior/AppearancesList.tsx
@@ -1,5 +1,39 @@
 import React from "react";
 import { Lorem } from "./Lorem";
+import {
+  ActorKind,
+  ActorKindOps,
+} from "../../model/junior/structured-program";
+import { AssetPresentation } from "../../model/asset";
+import { AppearanceCard } from "./AppearanceCard";
+import { NoContentHelp } from "./NoContentHelp";
+
+type AppearancesContentProps = {
+  actorKind: ActorKind;
+  appearances: Array<AssetPresentation>;
+};
+const AppearancesContent: React.FC<AppearancesContentProps> = ({
+  actorKind,
+  appearances,
+}) => {
+  if (appearances.length === 0) {
+    const appearanceName = ActorKindOps.names(actorKind).appearancesDisplay;
+    return <NoContentHelp actorKind={actorKind} contentKind={appearanceName} />;
+  }
+
+  return (
+    <>
+      {appearances.map((a) => (
+        <AppearanceCard
+          key={a.id}
+          actorKind={actorKind}
+          assetPresentation={a}
+          fullPathname={a.name}
+        />
+      ))}
+    </>
+  );
+};
 
 export const AppearancesList = () => {
   return (
