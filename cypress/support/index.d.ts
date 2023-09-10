0b8490b920c394148972386fb9cb1329a4e935d8
Ben North <ben.north@tcd.ie>
2023-09-10 09:52:42 +0100

    (Reformat)
    This is a huge diff but almost all of it is just a re-indentation.
    There's one line which is actually reformatted.

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -7,152 +7,154 @@ import {
 
 declare global {
   namespace Cypress {
-  interface Chainable {
-    /** Reset the "pytch" storage database to be empty apart from one
-     * project, which by default has one sample image asset and one
-     * sample sound asset.  Additional assets can be passed in if
-     * required via `options`. */
-    pytchResetDatabase(options?: ResetDatabaseOptions): Chainable<Element>;
+    interface Chainable {
+      /** Reset the "pytch" storage database to be empty apart from one
+       * project, which by default has one sample image asset and one
+       * sample sound asset.  Additional assets can be passed in if
+       * required via `options`. */
+      pytchResetDatabase(options?: ResetDatabaseOptions): Chainable<Element>;
 
-    /** Reset the "pytch" storage database, then navigate to the sole
-     * project created as part of the seeding.  Extra assets to be
-     * loaded into the seed project can be passed in if required. */
-    pytchExactlyOneProject(
-      resetDatabaseOptions?: ResetDatabaseOptions
-    ): Chainable<Element>;
+      /** Reset the "pytch" storage database, then navigate to the sole
+       * project created as part of the seeding.  Extra assets to be
+       * loaded into the seed project can be passed in if required. */
+      pytchExactlyOneProject(
+        resetDatabaseOptions?: ResetDatabaseOptions
+      ): Chainable<Element>;
 
-    /** Open the project matching the given name, assuming we are
-     * currently on the "My Projects" page. */
-    pytchOpenProject(name: string): Chainable<Element>;
+      /** Open the project matching the given name, assuming we are
+       * currently on the "My Projects" page. */
+      pytchOpenProject(name: string): Chainable<Element>;
 
-    /** Attempt to upload the zipfiles with the given basenames. */
-    pytchTryUploadZipfiles(zipBasenames: Array<string>): void;
+      /** Attempt to upload the zipfiles with the given basenames. */
+      pytchTryUploadZipfiles(zipBasenames: Array<string>): void;
 
-    /** Assert that the list of project names is as expected. */
-    pytchProjectNamesShouldDeepEqual(expectedNames: Array<string>): void;
+      /** Assert that the list of project names is as expected. */
+      pytchProjectNamesShouldDeepEqual(expectedNames: Array<string>): void;
 
-    /** Assuming we are currently working in the IDE, go to the app's
-     * home page. */
-    pytchHomeFromIDE(): void;
+      /** Assuming we are currently working in the IDE, go to the app's
+       * home page. */
+      pytchHomeFromIDE(): void;
 
-    /** Assuming we are currently working in the IDE, switch to the
-     * project with the given name. */
-    pytchSwitchProject(name: string): Chainable<Element>;
+      /** Assuming we are currently working in the IDE, switch to the
+       * project with the given name. */
+      pytchSwitchProject(name: string): Chainable<Element>;
 
-    /** Reset the "pytch" storage database, then create and navigate to
-     * a project following a sample tutorial. */
-    pytchProjectFollowingTutorial(
-      tutorialMatch?: string,
-      tutorialSlug?: string
-    ): Chainable<Element>;
+      /** Reset the "pytch" storage database, then create and navigate to
+       * a project following a sample tutorial. */
+      pytchProjectFollowingTutorial(
+        tutorialMatch?: string,
+        tutorialSlug?: string
+      ): Chainable<Element>;
 
-    /** Reset the "pytch" storage database, then create and navigate to
-     * a project created as a demo of a sample tutorial. */
-    pytchProjectDemonstratingTutorial(
-      tutorialMatch?: string,
-      tutorialSlug?: string
-    ): Chainable<Element>;
+      /** Reset the "pytch" storage database, then create and navigate to
+       * a project created as a demo of a sample tutorial. */
+      pytchProjectDemonstratingTutorial(
+        tutorialMatch?: string,
+        tutorialSlug?: string
+      ): Chainable<Element>;
 
-    /** Assert that the IDE currently displays the "Images and sounds"
-     * tab in the info pane, and that the listed assets have the given
-     * names, in the given order. */
-    pytchShouldShowAssets(expectedNames: Array<string>): Chainable<Element>;
+      /** Assert that the IDE currently displays the "Images and sounds"
+       * tab in the info pane, and that the listed assets have the given
+       * names, in the given order. */
+      pytchShouldShowAssets(expectedNames: Array<string>): Chainable<Element>;
 
-    /** Set focus to the Ace editor. */
-    pytchFocusEditor(): void;
+      /** Set focus to the Ace editor. */
+      pytchFocusEditor(): void;
 
-    /** Set the code directly to the given `codeText`. */
-    pytchSetCodeRaw(codeText: string): void;
+      /** Set the code directly to the given `codeText`. */
+      pytchSetCodeRaw(codeText: string): void;
 
-    /** Set the code to the result of de-indenting the given text. */
-    pytchSetCodeWithDeIndent(indentedCodeText: string): void;
+      /** Set the code to the result of de-indenting the given text. */
+      pytchSetCodeWithDeIndent(indentedCodeText: string): void;
 
-    /** Build the current state of the project via the BUILD button. */
-    pytchBuild(): Chainable<Element>;
+      /** Build the current state of the project via the BUILD button. */
+      pytchBuild(): Chainable<Element>;
 
-    /** Set the code-editor's content to the given code (after
-     * de-indenting it), and then build the project.  This may result in
-     * build errors, or in a successfully-built project. */
-    pytchBuildCode(rawCode: string): Chainable<Element>;
+      /** Set the code-editor's content to the given code (after
+       * de-indenting it), and then build the project.  This may result in
+       * build errors, or in a successfully-built project. */
+      pytchBuildCode(rawCode: string): Chainable<Element>;
 
-    /** Assert that the code editor of the IDE contains exactly the given code. */
-    pytchCodeTextShouldEqual(expectedCode: string): void;
+      /** Assert that the code editor of the IDE contains exactly the given code. */
+      pytchCodeTextShouldEqual(expectedCode: string): void;
 
-    /** Assert that the code editor of the IDE contains code containing
-     * the given match. */
-    pytchCodeTextShouldContain(match: string): Chainable<Element>;
+      /** Assert that the code editor of the IDE contains code containing
+       * the given match. */
+      pytchCodeTextShouldContain(match: string): Chainable<Element>;
 
-    /** Assert that the "Output" pane contains the given match. */
-    pytchStdoutShouldContain(match: string): Chainable<Element>;
+      /** Assert that the "Output" pane contains the given match. */
+      pytchStdoutShouldContain(match: string): Chainable<Element>;
 
-    /** Assert that the "Output" pane equals the given match. */
-    pytchStdoutShouldEqual(match: string): Chainable<Element>;
+      /** Assert that the "Output" pane equals the given match. */
+      pytchStdoutShouldEqual(match: string): Chainable<Element>;
 
-    /** Assert that the canvas is showing the given colour over the
-     * entirety of the stage area. */
-    pytchCanvasShouldBeSolidColour(expectedColour: ArrayRGBA): void;
+      /** Assert that the canvas is showing the given colour over the
+       * entirety of the stage area. */
+      pytchCanvasShouldBeSolidColour(expectedColour: ArrayRGBA): void;
 
-    /** Assert that the "Errors" pane is not active, and that it is
-     * devoid of error reports. */
-    pytchShouldHaveBuiltWithoutErrors(): Chainable<Element>;
+      /** Assert that the "Errors" pane is not active, and that it is
+       * devoid of error reports. */
+      pytchShouldHaveBuiltWithoutErrors(): Chainable<Element>;
 
-    /** Assert that the "Errors" pane is active and contains
-     * introductory (i.e., context-setting) text satisfying the
-     * given match. */
-    pytchShouldShowErrorContext(match: ContentMatch): Chainable<Element>;
+      /** Assert that the "Errors" pane is active and contains
+       * introductory (i.e., context-setting) text satisfying the
+       * given match. */
+      pytchShouldShowErrorContext(match: ContentMatch): Chainable<Element>;
 
-    /** Assert that the "Errors" pane is active and contains an error
-     * card satisfying the given match, of the given kind (user-space or
-     * Pytch-internal). */
-    pytchShouldShowErrorCard(
-      match: ContentMatch,
-      kind: PytchErrorKind
-    ): Chainable<Element>;
+      /** Assert that the "Errors" pane is active and contains an error
+       * card satisfying the given match, of the given kind (user-space or
+       * Pytch-internal). */
+      pytchShouldShowErrorCard(
+        match: ContentMatch,
+        kind: PytchErrorKind
+      ): Chainable<Element>;
 
-    /** Assert that the "Errors" pane is active and contains a stack
-     * trace having the given number of frame summaries. */
-    pytchShouldHaveErrorStackTraceOfLength(nFrames: number): Chainable<Element>;
+      /** Assert that the "Errors" pane is active and contains a stack
+       * trace having the given number of frame summaries. */
+      pytchShouldHaveErrorStackTraceOfLength(
+        nFrames: number
+      ): Chainable<Element>;
 
-    /** Click the green flag. */
-    pytchGreenFlag(): Chainable<Element>;
+      /** Click the green flag. */
+      pytchGreenFlag(): Chainable<Element>;
 
-    /** Click the red stop. */
-    pytchRedStop(): Chainable<Element>;
+      /** Click the red stop. */
+      pytchRedStop(): Chainable<Element>;
 
-    /** Within the IDE, choose the given entry from the drop-down menu. */
-    pytchChooseDropdownEntry(entryName: string): Chainable<void>;
+      /** Within the IDE, choose the given entry from the drop-down menu. */
+      pytchChooseDropdownEntry(entryName: string): Chainable<void>;
 
-    /** Send keypresses to the running project. */
-    pytchSendKeysToProject(keys: string): Chainable<Element>;
+      /** Send keypresses to the running project. */
+      pytchSendKeysToProject(keys: string): Chainable<Element>;
 
-    /** Click on stage at given STAGE coordinates (e.g., (0, 0) is centre). */
-    pytchClickStage(stageX: number, stageY: number): Chainable<Element>;
+      /** Click on stage at given STAGE coordinates (e.g., (0, 0) is centre). */
+      pytchClickStage(stageX: number, stageY: number): Chainable<Element>;
 
-    /** Send keypresses to the currently-focussed element of the web-app. */
-    pytchSendKeysToApp(keys: string): Chainable<Element>;
+      /** Send keypresses to the currently-focussed element of the web-app. */
+      pytchSendKeysToApp(keys: string): Chainable<Element>;
 
-    /** Resize the stage to achieve the given increase in height. */
-    pytchDragStageDivider(sizeIncrease: number): void;
+      /** Resize the stage to achieve the given increase in height. */
+      pytchDragStageDivider(sizeIncrease: number): void;
 
-    /** Assert that the Build / Green-flag tooltips behave while the
-     * user clicks through the button tour. */
-    pytchRunThroughButtonTour(): Chainable<Element>;
+      /** Assert that the Build / Green-flag tooltips behave while the
+       * user clicks through the button tour. */
+      pytchRunThroughButtonTour(): Chainable<Element>;
 
-    /** Activate the drop-down menu for the asset with the given
-     * `assetName`.  Optionally, run the given `maybeChooseItem`
-     * function once the dropdown appears. */
-    pytchActivateAssetDropdown(
-      assetName: string,
-      maybeChooseItem?: () => void
-    ): Chainable<void>;
+      /** Activate the drop-down menu for the asset with the given
+       * `assetName`.  Optionally, run the given `maybeChooseItem`
+       * function once the dropdown appears. */
+      pytchActivateAssetDropdown(
+        assetName: string,
+        maybeChooseItem?: () => void
+      ): Chainable<void>;
 
-    /** Activate the drop-down menu for the asset with the given
-     * `assetName` and then click on the item with the given `itemName`
-     * once the drop-down appears. */
-    pytchClickAssetDropdownItem(
-      assetName: string,
-      itemName: string
-    ): Chainable<void>;
-  }
+      /** Activate the drop-down menu for the asset with the given
+       * `assetName` and then click on the item with the given `itemName`
+       * once the drop-down appears. */
+      pytchClickAssetDropdownItem(
+        assetName: string,
+        itemName: string
+      ): Chainable<void>;
+    }
   }
 }
