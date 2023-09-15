93e413ea511ec8cfc0b287e81360b6811f74ca86
Ben North <ben.north@tcd.ie>
2023-09-15 17:48:18 +0100

    AddSomethingButton: Add optl "label" prop

--- src/components/Junior/AddSomethingButton.tsx
+++ src/components/Junior/AddSomethingButton.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export type AddSomethingButtonProps = {
+  label?: string;
   onClick: React.MouseEventHandler;
 };
 
 export const AddSomethingButton: React.FC<AddSomethingButtonProps> = ({
+  label,
   onClick,
 }) => {
   return (
     <div className="AddSomethingButton">
       <div onClick={onClick}>
+        {label && <span className="label">{label}</span>}
         <span className="icon">
           <FontAwesomeIcon className="fa-lg" icon="plus" />
         </span>
