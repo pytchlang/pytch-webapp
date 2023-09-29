36a96beeca0a7efadc178c3e6c067933aa9b71f7
Ben North <ben.north@tcd.ie>
2023-09-29 15:09:04 +0100

    addFromMediaLib(): Define helper

--- cypress/e2e/junior/actor-assets.cy.ts
+++ cypress/e2e/junior/actor-assets.cy.ts
@@ -1,3 +1,7 @@
+import {
+  settleModalDialog,
+} from "./utils";
+
 context("Working with assets of an actor", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
@@ -5,4 +9,20 @@ context("Working with assets of an actor", () => {
 
   const clickAddSomething = (match: string) =>
     cy.get("div.tab-pane.active .AddSomethingButton").contains(match).click();
+
+  const addFromMediaLib = (matches: Array<string>) => {
+    clickAddSomething("from media library");
+
+    for (const match of matches) {
+      // Sometimes the media library is scrolled such that the chosen
+      // image is out of view.  Force Cypress to click it:
+      cy.get(".clipart-card .clipart-name")
+        .contains(match)
+        .should("have.length", 1)
+        .click({ force: true });
+    }
+
+    const expButtonMatch = `Add ${matches.length}`;
+    settleModalDialog(expButtonMatch);
+  };
 });
