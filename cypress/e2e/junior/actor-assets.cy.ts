407a22e5214c23468690054fbe431d1d544814f9
Ben North <ben.north@tcd.ie>
2023-09-29 15:08:51 +0100

    clickAddSomething(): Define helper

--- cypress/e2e/junior/actor-assets.cy.ts
+++ cypress/e2e/junior/actor-assets.cy.ts
@@ -2,4 +2,7 @@ context("Working with assets of an actor", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
   });
+
+  const clickAddSomething = (match: string) =>
+    cy.get("div.tab-pane.active .AddSomethingButton").contains(match).click();
 });
