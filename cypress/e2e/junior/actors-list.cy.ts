6d055ad014a818c9257c672b23b042408269b36c
Ben North <ben.north@tcd.ie>
2023-09-29 15:03:31 +0100

    launchAddSprite(): Define helper

--- cypress/e2e/junior/actors-list.cy.ts
+++ cypress/e2e/junior/actors-list.cy.ts
@@ -2,4 +2,7 @@ context("Work with list of actors", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
   });
+
+  const launchAddSprite = () =>
+    cy.get(".Junior-ActorsList-container .AddSomethingButton").click();
 });
