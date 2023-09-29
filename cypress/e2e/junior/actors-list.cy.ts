7e323e556b798c345631a62fd316bd91dc892a75
Ben North <ben.north@tcd.ie>
2023-09-29 15:03:58 +0100

    Add "can add sprites" test

--- cypress/e2e/junior/actors-list.cy.ts
+++ cypress/e2e/junior/actors-list.cy.ts
@@ -1,3 +1,8 @@
+import {
+  assertActorNames,
+  settleModalDialog,
+} from "./utils";
+
 context("Work with list of actors", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
@@ -5,4 +10,17 @@ context("Work with list of actors", () => {
 
   const launchAddSprite = () =>
     cy.get(".Junior-ActorsList-container .AddSomethingButton").click();
+
+  it("can add sprites", () => {
+    assertActorNames(["Stage", "Snake"]);
+
+    launchAddSprite();
+    settleModalDialog("OK");
+    assertActorNames(["Stage", "Snake", "Sprite1"]);
+
+    launchAddSprite();
+    cy.get(".modal-dialog input").type("{selectAll}{del}Banana");
+    settleModalDialog("OK");
+    assertActorNames(["Stage", "Snake", "Sprite1", "Banana"]);
+  });
 });
