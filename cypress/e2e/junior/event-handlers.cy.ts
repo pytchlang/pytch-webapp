efb7cac4cb85eb0b020577cef81b39163f188add
Ben North <ben.north@tcd.ie>
2023-09-29 15:16:56 +0100

    Add "can choose which event handler to add" test

--- cypress/e2e/junior/event-handlers.cy.ts
+++ cypress/e2e/junior/event-handlers.cy.ts
@@ -62,4 +62,41 @@ context("Create/modify/delete event handlers", () => {
     settleModalDialog("Cancel");
     assertHandlersUnchanged();
   });
+
+  it("can choose which event handler to add", () => {
+    launchAddHandler();
+
+    // We have not yet typed a message for "when I receive", so choosing
+    // that hat block should leave "OK" disabled.  All others are
+    // immediately OK.
+    type ActionSpec = { match: string; expOkEnabled?: boolean };
+    const specs: Array<ActionSpec> = [
+      { match: "when green flag clicked" },
+      { match: "when I start as a clone" },
+      { match: "when this sprite/stage clicked" }, // TODO: Fix text
+      { match: "when I receive", expOkEnabled: false },
+      { match: "when key" },
+    ];
+
+    cy.get(".modal-footer button").contains("OK").as("ok-btn");
+
+    for (const spec of specs) {
+      cy.get("li.EventKindOption").contains(spec.match).click();
+      cy.get("li.EventKindOption.chosen")
+        .should("have.length", 1)
+        .contains(spec.match);
+
+      const expOkEnabled = spec.expOkEnabled ?? true;
+      const predicate = expOkEnabled ? "be.enabled" : "be.disabled";
+      cy.get("@ok-btn").should(predicate);
+    }
+
+    // If we provide a message, that should become active, and OK should
+    // be enabled.
+    cy.get("li.EventKindOption input").type("go-for-it");
+    cy.get("li.EventKindOption.chosen")
+      .should("have.length", 1)
+      .contains("when I receive");
+    cy.get("@ok-btn").should("be.enabled");
+  });
 });
