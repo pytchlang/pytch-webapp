4dca44acae3add8ff334044a4b15b41080dd22e7
Ben North <ben.north@tcd.ie>
2023-09-29 15:14:44 +0100

    launchAddHandler(): Define helper

--- cypress/e2e/junior/event-handlers.cy.ts
+++ cypress/e2e/junior/event-handlers.cy.ts
@@ -1,5 +1,16 @@
+import {
+  selectActorAspect,
+  selectSprite,
+} from "./utils";
+
 context("Create/modify/delete event handlers", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
   });
+
+  const launchAddHandler = () => {
+    selectSprite("Snake");
+    selectActorAspect("Code");
+    cy.get(".Junior-ScriptsEditor .AddSomethingButton").click();
+  };
 });
