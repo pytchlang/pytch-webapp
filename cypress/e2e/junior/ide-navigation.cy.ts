c9e7f1c77fe265e9f775f0f3cbe93cc24f97329f
Ben North <ben.north@tcd.ie>
2023-09-29 15:02:12 +0100

    Add "shows correct actor-aspect tab labels" test

--- cypress/e2e/junior/ide-navigation.cy.ts
+++ cypress/e2e/junior/ide-navigation.cy.ts
@@ -1,3 +1,9 @@
+import {
+  assertAspectTabLabels,
+  selectSprite,
+  selectStage,
+} from "./utils";
+
 context("Basic use of per-method IDE", () => {
   before(() => {
     cy.pytchBasicJrProject();
@@ -6,4 +12,11 @@ context("Basic use of per-method IDE", () => {
   // These tests are all independent, so we can use the same IDE
   // instance throughout.
 
+  it("shows correct actor-aspect tab labels", () => {
+    selectStage();
+    assertAspectTabLabels(["Code", "Backdrops", "Sounds"]);
+
+    selectSprite("Snake");
+    assertAspectTabLabels(["Code", "Costumes", "Sounds"]);
+  });
 });
