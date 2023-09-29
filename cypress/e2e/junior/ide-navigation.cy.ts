50c355f0571a2b4d53e4f709930d5f5dba4e574e
Ben North <ben.north@tcd.ie>
2023-09-29 15:00:26 +0100

    Add skeleton

---
+++ cypress/e2e/junior/ide-navigation.cy.ts
@@ -0,0 +1,9 @@
+context("Basic use of per-method IDE", () => {
+  before(() => {
+    cy.pytchBasicJrProject();
+  });
+
+  // These tests are all independent, so we can use the same IDE
+  // instance throughout.
+
+});
