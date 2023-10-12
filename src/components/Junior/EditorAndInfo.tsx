881fa59d6fff9eb27bc9ccc244db09dc4058b551
Ben North <ben.north@tcd.ie>
2023-10-12 18:18:54 +0100

    EditorAndInfo: Add class if info panel collapsed

--- src/components/Junior/EditorAndInfo.tsx
+++ src/components/Junior/EditorAndInfo.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { InfoPanel } from "./InfoPanel";
 import { ActorProperties } from "./ActorProperties";
 import { EmptyProps } from "../../utils";
+import { useJrEditState } from "./hooks";
+import classNames from "classnames";
 
 export const EditorAndInfo: React.FC<EmptyProps> = () => {
+  const infoPanelIsCollapsed = useJrEditState(
+    (s) => s.infoPanelState === "collapsed"
+  );
+
+  const classes = classNames("Junior-EditorAndInfo", { infoPanelIsCollapsed });
   return (
-    <div className="Junior-EditorAndInfo">
+    <div className={classes}>
       <ActorProperties />
       <InfoPanel />
     </div>
