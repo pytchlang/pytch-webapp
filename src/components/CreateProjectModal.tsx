e907fe1ad9a0b06a0dd0ae7a46bcd671686a5dcf
Ben North <ben.north@tcd.ie>
2023-11-02 17:47:57 +0000

    Add button-group for whether-example options

--- src/components/CreateProjectModal.tsx
+++ src/components/CreateProjectModal.tsx
@@ -85,6 +85,23 @@ export const CreateProjectModal = () => {
               ref={inputRef}
             />
           </Form.Group>
+          <hr />
+          <Form.Group className="whether-include-example">
+            <div className="option-buttons">
+              <WhetherExampleOption
+                thisOption="with-example"
+                activeOption={whetherExample}
+                label="With example code"
+                setActive={setWhetherExample}
+              />
+              <WhetherExampleOption
+                thisOption="without-example"
+                activeOption={whetherExample}
+                label="Without example code"
+                setActive={setWhetherExample}
+              />
+            </div>
+          </Form.Group>
         </Form>
         <MaybeErrorOrSuccessReport
           messageWhenSuccess="Project created!"
