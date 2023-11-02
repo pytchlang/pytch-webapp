83fb104d9afe1804b50a77a134d4ebe2d2ba6b3e
Ben North <ben.north@tcd.ie>
2023-11-02 17:05:12 +0000

    templateKindFromComponents(): Define

--- src/model/project-templates.ts
+++ src/model/project-templates.ts
@@ -1,3 +1,4 @@
+import { assertNever } from "../utils";
 import { PytchProgramKind } from "./pytch-program";
 
 export type ProjectTemplateKind =
@@ -10,3 +11,22 @@ export type WhetherExampleTag = "with-example" | "without-example";
 
 type ProjectTemplateKindAsComponents =
   `${WhetherExampleTag}/${PytchProgramKind}`;
+
+export function templateKindFromComponents(
+  whetherExample: WhetherExampleTag,
+  editorKind: PytchProgramKind
+): ProjectTemplateKind {
+  const kind: ProjectTemplateKindAsComponents = `${whetherExample}/${editorKind}`;
+  switch (kind) {
+    case "with-example/flat":
+      return "with-sample-code";
+    case "with-example/per-method":
+      return "simple-example-per-method";
+    case "without-example/flat":
+      return "bare-bones";
+    case "without-example/per-method":
+      return "bare-per-method";
+    default:
+      return assertNever(kind);
+  }
+}
