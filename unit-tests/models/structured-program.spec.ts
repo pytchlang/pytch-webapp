b8cfd46c4fb465d208e974627545f2e63a3c4914
Ben North <ben.north@tcd.ie>
2023-11-03 14:08:56 +0000

    newRandom(), eqArrays(): Test

--- unit-tests/models/structured-program.spec.ts
+++ unit-tests/models/structured-program.spec.ts
@@ -6,5 +6,18 @@ import {
 describe("Structured programs", () => {
   describe("uuids", () => {
     const Ops = UuidOps;
+
+    it("work with Uuids", () => {
+      const xs = [Ops.newRandom(), Ops.newRandom(), Ops.newRandom()];
+      const ys = xs.slice(0, 3);
+
+      assert.isFalse(ys === xs);
+      assert.isTrue(Ops.eqArrays(ys, xs));
+      assert.isFalse(Ops.eqArrays(xs.slice(0, 2), xs));
+
+      let zs = xs.slice(0, 3);
+      zs[2] = Ops.newRandom();
+      assert.isFalse(Ops.eqArrays(zs, xs));
+    });
   });
 });
