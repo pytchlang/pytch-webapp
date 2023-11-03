d5f5de9584896987e54e323132a712e1abfe5eb2
Ben North <ben.north@tcd.ie>
2023-11-03 14:08:41 +0000

    Add unit test skeleton

--- unit-tests/models/structured-program.spec.ts
+++ unit-tests/models/structured-program.spec.ts
@@ -1,4 +1,10 @@
 import { assert } from "chai";
+import {
+  UuidOps,
+} from "../../src/model/junior/structured-program";
 
 describe("Structured programs", () => {
+  describe("uuids", () => {
+    const Ops = UuidOps;
+  });
 });
