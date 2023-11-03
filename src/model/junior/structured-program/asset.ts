b28e372ce2abfccc8e762b0a34c8a84990863e4f
Ben North <ben.north@tcd.ie>
2023-11-03 14:18:02 +0000

    pathComponents(): Define

--- src/model/junior/structured-program/asset.ts
+++ src/model/junior/structured-program/asset.ts
@@ -18,6 +18,9 @@ export type AssetPathComponents = {
   basename: string;
 };
 
+// TODO: Tighten regexp?
+const dirnameBasenameRegExp = new RegExp("^([0-9a-f-]{36})/(.*)");
+
 export class AssetMetaDataOps {
   /** Return the first asset within `allAssets` which belongs to the
    * given `targetActorId` and is of the given `targetMimeMajorType`
@@ -37,5 +40,17 @@ export class AssetMetaDataOps {
     );
 
     return actorAssetsOfType.length === 0 ? null : actorAssetsOfType[0];
+  }
+
+  /** Split the given `fullPathname` into the "directory" part, which
+   * holds the `actorId`, and the "filename" part, which holds the
+   * `basename` . */
+  static pathComponents(fullPathname: string): AssetPathComponents {
+    const maybeMatch = dirnameBasenameRegExp.exec(fullPathname);
+    if (maybeMatch == null) {
+      throw new Error(`could not parse "${fullPathname}"`);
+    }
+
+    return { actorId: maybeMatch[1], basename: maybeMatch[2] };
   }
 }
