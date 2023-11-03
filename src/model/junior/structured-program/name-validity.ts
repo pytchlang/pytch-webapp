fe38d8cfa77a04f86ef8e7039a84f83c9618ee11
Ben North <ben.north@tcd.ie>
2023-11-03 20:10:06 +0000

    nameValidity(): Define

--- src/model/junior/structured-program/name-validity.ts
+++ src/model/junior/structured-program/name-validity.ts
@@ -11,3 +11,38 @@ const invalidBecause = (reason: string): NameValidity => ({
   status: "invalid",
   reason,
 });
+
+/** Assess the validity (or otherwise) of the given `candidateName` for
+ * a sprite, given that there already exist sprites names being the
+ * elements of `existingNames`. */
+export const nameValidity = (
+  existingNames: Array<string>,
+  candidateName: string
+): NameValidity => {
+  // This is a fudge but should do the job:
+  if (candidateName === "Stage") {
+    return invalidBecause('you cannot have a Sprite called "Stage"');
+  }
+
+  if (candidateName === "") {
+    return invalidBecause("it is empty");
+  }
+
+  if (!Sk.token.isIdentifier(candidateName)) {
+    return invalidBecause("it does not follow the rules for names");
+  }
+
+  if (existingNames.includes(candidateName)) {
+    // TODO: Will this ever be used for things other than Sprites?
+    return invalidBecause(`there is already a Sprite called ${candidateName}`);
+  }
+
+  try {
+    const classDef = `class ${candidateName}:\n pass`;
+    Sk.parse("<stdin>", classDef);
+    return { status: "valid" };
+  } catch {
+    // TODO: Are there other reasons we might find ourselves here?
+    return invalidBecause("it is a reserved name");
+  }
+};
