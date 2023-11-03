2278de716763a264347f3c79b9d7bbdbf23fa63a
Ben North <ben.north@tcd.ie>
2023-11-03 08:56:04 +0000

    Update, expand "can create from template" test

--- cypress/e2e/junior/new-project.cy.ts
+++ cypress/e2e/junior/new-project.cy.ts
@@ -1,15 +1,29 @@
+import { assertActorNames } from "./utils";
+
 context("Create and edit per-method programs", () => {
   beforeEach(() => {
     cy.pytchResetDatabase();
   });
 
-  it("can create from template", () => {
-    cy.contains("My projects").click();
-    cy.get("button").contains("Create new").click();
-    cy.get("button").contains("Empty PytchJr").as("PJ-btn").click();
-    cy.get("@PJ-btn").should("have.class", "btn-success");
-    cy.get("button").contains("Create project").click();
-    cy.get("div.Junior-ScriptsEditor");
-    cy.get("div.modal.show").should("not.exist");
-  });
+  [
+    { buttonMatch: "Without example", expActors: ["Stage"] },
+    { buttonMatch: "With example", expActors: ["Stage", "Snake"] },
+  ].forEach((spec) =>
+    it(`can create from template (${spec.buttonMatch})`, () => {
+      cy.contains("My projects").click();
+      cy.get("button").contains("Create new").click();
+
+      cy.get("button").contains(spec.buttonMatch).as("example-btn").click();
+      cy.get("@example-btn").should("have.class", "btn-success");
+
+      cy.get("button").contains("as sprites and scripts").as("pjr-btn").click();
+      cy.get("@pjr-btn").should("have.class", "btn-success");
+
+      cy.get("button").contains("Create project").click();
+      cy.get("div.Junior-ScriptsEditor");
+      cy.get("div.modal.show").should("not.exist");
+
+      assertActorNames(spec.expActors);
+    })
+  );
 });
