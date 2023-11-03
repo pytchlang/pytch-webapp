f4dba784ae9824722e7a503ba54ae63fac827412
Ben North <ben.north@tcd.ie>
2023-11-03 14:36:37 +0000

    EventDescriptor: Define type

--- src/model/junior/structured-program/event.ts
+++ src/model/junior/structured-program/event.ts
@@ -1,2 +1,9 @@
 import { assertNever } from "../../../utils";
 import { Uuid, UuidOps } from "./core-types";
+
+export type EventDescriptor =
+  | { kind: "green-flag" }
+  | { kind: "key-pressed"; keyName: string }
+  | { kind: "message-received"; message: string }
+  | { kind: "start-as-clone" }
+  | { kind: "clicked" };
