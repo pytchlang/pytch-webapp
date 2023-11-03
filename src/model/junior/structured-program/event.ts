f3fb5eac12c2980feeba1108d90e1bc55d8228a4
Ben North <ben.north@tcd.ie>
2023-11-03 14:38:11 +0000

    arity(): Define

--- src/model/junior/structured-program/event.ts
+++ src/model/junior/structured-program/event.ts
@@ -11,4 +11,19 @@ export type EventDescriptor =
 export type EventDescriptorKind = EventDescriptor["kind"];
 
 export class EventDescriptorKindOps {
+  /** Return the number of "arguments" the given `kind` of
+   * event-descriptor needs.  This is always either `0` or `1`. */
+  static arity(kind: EventDescriptorKind): number {
+    switch (kind) {
+      case "green-flag":
+      case "clicked":
+      case "start-as-clone":
+        return 0;
+      case "key-pressed":
+      case "message-received":
+        return 1;
+      default:
+        return assertNever(kind);
+    }
+  }
 }
