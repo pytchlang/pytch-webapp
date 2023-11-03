6aeddfc817da92ff0cbe7de4d2d4adc02e0b45f6
Ben North <ben.north@tcd.ie>
2023-11-03 13:58:21 +0000

    eqArrays(): Define function

--- src/model/junior/structured-program/core-types.ts
+++ src/model/junior/structured-program/core-types.ts
@@ -1,4 +1,20 @@
 export type Uuid = string;
 
 export class UuidOps {
+  /** Return `true`/`false` according to whether the two given arrays
+   * are equal in the sense that they contain the same `Uuid` values in
+   * the same order. */
+  static eqArrays(x: Array<Uuid>, y: Array<Uuid>): boolean {
+    if (x.length !== y.length) {
+      return false;
+    }
+
+    for (let i = 0; i !== x.length; ++i) {
+      if (x[i] !== y[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
