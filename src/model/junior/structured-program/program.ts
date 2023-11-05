ab856eb3a34a92eb0d778cd7fcbfa8cd12e7681c
Ben North <ben.north@tcd.ie>
2023-11-05 11:37:58 +0000

    addSprite(): Define

--- src/model/junior/structured-program/program.ts
+++ src/model/junior/structured-program/program.ts
@@ -79,4 +79,17 @@ export class StructuredProgramOps {
   }
 
   // MUTATORS:
+
+  /** Mutate in-place the given `program` by adding a new empty Sprite
+   * with the given `name`. */
+  static addSprite(program: StructuredProgram, name: string) {
+    if (StructuredProgramOps.hasSpriteByName(program, name))
+      throw new Error(`already have sprite called "${name}"`);
+
+    // TODO: Reject name if not a valid Python identifier.  Could restrict
+    // to beginner-friendly subset and/or impose additional constraints,
+    // e.g., must start with capital letter.
+
+    program.actors.push(ActorOps.newEmptySprite(name));
+  }
 }
