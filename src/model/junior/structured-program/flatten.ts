d764b7aef3435993bcda7dbc9d81e09237185520
Ben North <ben.north@tcd.ie>
2023-11-05 13:54:47 +0000

    Add file skeleton

---
+++ src/model/junior/structured-program/flatten.ts
@@ -0,0 +1,7 @@
+// Conversion of structured program into flat Python code.
+
+import { Actor, ActorKindOps } from "./actor";
+import { AssetMetaData, AssetMetaDataOps } from "./asset";
+import { EventDescriptorOps } from "./event";
+import { StructuredProgram } from "./program";
+import { SourceMapEntry } from "./source-map";
