c2b91e641b3dce57e678de54e48381ab7de152de
Ben North <ben.north@tcd.ie>
2023-11-05 13:17:53 +0000

    SourceMapEntry: Define type

--- src/model/junior/structured-program/source-map.ts
+++ src/model/junior/structured-program/source-map.ts
@@ -1,1 +1,17 @@
 import { Uuid } from "./core-types";
+
+/** Correspondence between
+ *
+ * * a contiguous chunk of lines in the flat representation of a
+ *   program;
+ * * the body of a particular individual handler within a particular
+ *   individual actor, in the by-actor, by-script representation of that
+ *   same program.
+ *
+ * Only the start line of the chunk is recorded.  (TODO: Consider adding
+ * an (exclusive) `endLine` slot.) */
+export type SourceMapEntry = {
+  startLine: number;
+  actorId: Uuid;
+  handlerId: Uuid;
+};
