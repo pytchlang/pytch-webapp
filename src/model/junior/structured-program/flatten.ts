40065a1f13a922d26d1545a17c9c1978b4f3225a
Ben North <ben.north@tcd.ie>
2023-11-05 13:58:48 +0000

    FlattenResults: Define type

--- src/model/junior/structured-program/flatten.ts
+++ src/model/junior/structured-program/flatten.ts
@@ -5,3 +5,16 @@ import { AssetMetaData, AssetMetaDataOps } from "./asset";
 import { EventDescriptorOps } from "./event";
 import { StructuredProgram } from "./program";
 import { SourceMapEntry } from "./source-map";
+
+/** The result of converting a structured program into one flat Python
+ * program:
+ *
+ * * `codeText` — the flat Python program;
+ * * `mapEntries` — an array of correspondences between the individual
+ *   handlers of the individual actors in the structured program and
+ *   contiguous chunks of lines in the resulting flat program.
+ */
+export type FlattenResults = {
+  codeText: string;
+  mapEntries: Array<SourceMapEntry>;
+};
