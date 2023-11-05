c64b1fd485b9093a75cfc3e7ac6348af891fd5eb
Ben North <ben.north@tcd.ie>
2023-11-05 11:04:49 +0000

    Add file skeleton

---
+++ src/model/junior/structured-program/program.ts
@@ -0,0 +1,4 @@
+import { Actor, ActorOps, ActorSummary } from "./actor";
+import { Uuid } from "./core-types";
+import { EventDescriptor, EventHandler, EventHandlerOps } from "./event";
+import { assertNever } from "../../../utils";
