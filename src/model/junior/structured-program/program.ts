df5ee65569bc1dc1acadd8c28aa6e97ecfdff903
Ben North <ben.north@tcd.ie>
2023-11-05 11:05:00 +0000

    StructuredProgram: Define type

--- src/model/junior/structured-program/program.ts
+++ src/model/junior/structured-program/program.ts
@@ -2,3 +2,7 @@ import { Actor, ActorOps, ActorSummary } from "./actor";
 import { Uuid } from "./core-types";
 import { EventDescriptor, EventHandler, EventHandlerOps } from "./event";
 import { assertNever } from "../../../utils";
+
+export type StructuredProgram = {
+  actors: Array<Actor>;
+};
