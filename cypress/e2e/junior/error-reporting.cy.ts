a51ddc42f6afdbcc63544654f2d824dcb8ef9d3e
Ben North <ben.north@tcd.ie>
2023-11-11 13:05:19 +0000

    (Reformat)

--- cypress/e2e/junior/error-reporting.cy.ts
+++ cypress/e2e/junior/error-reporting.cy.ts
@@ -47,29 +47,29 @@ context("Interact with errors", () => {
     withPytchJrProgramIt(
       `switches to error tab on error (${spec.activeActor} active)`,
       (program, actions) => {
-    const snake = program.actors[1];
-    actions.setHandlerPythonCode({
-      actorId: snake.id,
-      handlerId: snake.handlers[0].id,
-      code: 'print(3 + "a")\n',
-    });
+        const snake = program.actors[1];
+        actions.setHandlerPythonCode({
+          actorId: snake.id,
+          handlerId: snake.handlers[0].id,
+          code: 'print(3 + "a")\n',
+        });
 
-    // I /think/ this has now updated the store, which is what the build
-    // process uses, so we don't have to wait for the DOM to update with
-    // the new code.
-    spec.activateActorFun();
-    selectActorAspect("Sounds");
-    selectInfoPane("Output");
-    cy.pytchGreenFlag();
+        // I /think/ this has now updated the store, which is what the build
+        // process uses, so we don't have to wait for the DOM to update with
+        // the new code.
+        spec.activateActorFun();
+        selectActorAspect("Sounds");
+        selectInfoPane("Output");
+        cy.pytchGreenFlag();
 
-    cy.pytchShouldShowJuniorErrorCard(
-      "unsupported operand type(s)",
-      "user-space"
-    );
+        cy.pytchShouldShowJuniorErrorCard(
+          "unsupported operand type(s)",
+          "user-space"
+        );
 
-    cy.get(".go-to-line").should("have.length", 1).click();
-    cy.get(".ActorCard").eq(1).should("have.class", "isFocused");
-    cy.contains('3 + "a"').should("be.visible");
+        cy.get(".go-to-line").should("have.length", 1).click();
+        cy.get(".ActorCard").eq(1).should("have.class", "isFocused");
+        cy.contains('3 + "a"').should("be.visible");
       }
     )
   );
