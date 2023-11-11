5b88f385e17830045740eabb01c5e9b94a981b5a
Ben North <ben.north@tcd.ie>
2023-11-11 14:53:59 +0000

    HelpElementProps: Extract type

--- src/components/HelpSidebar.tsx
+++ src/components/HelpSidebar.tsx
@@ -200,9 +200,13 @@ const PurePythonElement: React.FC<PurePythonElementDescriptor & IToggleHelp> = (
 // It's a bit clumsy to accept a toggleHelp function for all elements,
 // since not all elements use it.  E.g., a heading element has no
 // toggle-help button.  But it does no real harm.
-const HelpElement: React.FC<
-  HelpElementDescriptor & { key: string; toggleHelp: () => void }
-> = (props) => {
+type HelpElementProps = {
+  key: string;
+  toggleHelp: () => void;
+};
+const HelpElement: React.FC<HelpElementDescriptor & HelpElementProps> = (
+  props
+) => {
   switch (props.kind) {
     case "heading":
       return <HeadingElement {...props} />;
