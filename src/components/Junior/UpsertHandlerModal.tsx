8f33861bfffd09266e2a4aba76664ba960420530
Ben North <ben.north@tcd.ie>
2023-11-12 08:27:04 +0000

    EventKindOption: Attempt-if-ready on double-click

--- src/components/Junior/UpsertHandlerModal.tsx
+++ src/components/Junior/UpsertHandlerModal.tsx
@@ -35,12 +35,19 @@ const EventKindOption: React.FC<EventKindOptionProps> = ({
   const setChosenKind = useJrEditActions(
     (a) => a.upsertHatBlockInteraction.setChosenKind
   );
+  const attemptIfReady = useJrEditActions(
+    (a) => a.upsertHatBlockInteraction.attemptIfReady
+  );
 
   const chosen = chosenKind === kind;
   const classes = classNames("EventKindOption", { chosen });
 
   return (
-    <li className={classes} onClick={() => setChosenKind(kind)}>
+    <li
+      className={classes}
+      onClick={() => setChosenKind(kind)}
+      onDoubleClick={() => attemptIfReady()}
+    >
       <div className="bump" />
       {children}
     </li>
