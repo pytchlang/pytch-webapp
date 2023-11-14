364f835b3fe09b0d33d6ce956646b126a875baae
Ben North <ben.north@tcd.ie>
2023-11-14 16:54:03 +0000

    styleStringFromClass: Compute constant

--- src/model/highlight-as-ace.ts
+++ src/model/highlight-as-ace.ts
@@ -3,3 +3,37 @@ import { IAceEditorProps } from "react-ace";
 type AceEditorT = Parameters<Required<IAceEditorProps>["onLoad"]>[0];
 type AceRange = ReturnType<AceEditorT["getSelectionRange"]>;
 type AceToken = ReturnType<AceEditorT["session"]["getTokens"]>[number];
+
+const styleStringFromClass = (() => {
+  // The following was manually extracted from the github theme CSS
+  // within the Ace source.  Try to remember to check now and again that
+  // it still matches.
+  const defFromClass = new Map<string, string>([
+    ["keyword", "/bold"],
+    ["string", "#d14/"],
+    ["variable.class", "teal/"],
+    ["constant.numeric", "#099/"],
+    ["function.support", "#0086b3/"],
+    ["comment", "#998/italic"],
+    ["variable.language", "#0086b3/"],
+    ["keyword.operator", "/bold"],
+    ["paren", "/bold"],
+    ["boolean", "/bold"],
+    ["variable.instance", "teal/"],
+    ["constant.language", "/bold"],
+  ]);
+
+  let styleFromClass = new Map<string, string>();
+  for (const [cls, def] of defFromClass.entries()) {
+    const [colourStyle, fontStyle] = def.split("/");
+
+    let styleString = "";
+    if (colourStyle !== "") styleString += `color:${colourStyle};`;
+    if (fontStyle === "bold") styleString += `font-weight:bold;`;
+    if (fontStyle === "italic") styleString += `font-style:italic;`;
+
+    styleFromClass.set(cls, styleString);
+  }
+
+  return styleFromClass;
+})();
