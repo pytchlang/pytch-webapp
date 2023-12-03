d945459bb3060ec60fb37d0f366f1463c129cc75
Ben North <ben.north@tcd.ie>
2023-12-03 11:42:32 +0000

    ActorIdentiferOps: Define class

--- src/model/junior/jr-tutorial.ts
+++ src/model/junior/jr-tutorial.ts
@@ -1,3 +1,18 @@
+import { assertNever } from "../../utils";
+
 // Use full word "Identifier" so as not to make people think it's a
 // short numeric id, or a Uuid, or anything like that.
 type ActorIdentifier = { kind: "stage" } | { kind: "sprite"; name: string };
+
+export class ActorIdentifierOps {
+  static nounPhrase(actorIdentifier: ActorIdentifier): string {
+    switch (actorIdentifier.kind) {
+      case "stage":
+        return "the stage";
+      case "sprite":
+        return `the “${actorIdentifier.name}” sprite`;
+      default:
+        return assertNever(actorIdentifier);
+    }
+  }
+}
