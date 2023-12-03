665a0993c41d1580e815fab854ae32e114610a63
Ben North <ben.north@tcd.ie>
2023-12-03 13:18:33 +0000

    ScriptDiffViewLine: Define local component
    And related type ScriptDiffLine.

--- src/components/Junior/lesson/commit/ScriptDiff.tsx
+++ src/components/Junior/lesson/commit/ScriptDiff.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import classNames from "classnames";
+import { PrettyPrintedLine } from "../../../../model/code-diff";
+import RawElement from "../../../RawElement";
+import { assertNever } from "../../../../utils";
 
 type DiffViewKind = "bare-old" | "old-diff" | "new-diff";
+type ScriptDiffLine = PrettyPrintedLine<HTMLElement>;
 
 type DiffViewKindSelectorProps = {
   viewKind: DiffViewKind;
@@ -36,4 +40,39 @@ const DiffViewKindSelector: React.FC<DiffViewKindSelectorProps> = ({
       )}
     </div>
   );
+};
+
+type ScriptDiffViewLineProps = { line: ScriptDiffLine };
+const ScriptDiffViewLine: React.FC<ScriptDiffViewLineProps> = ({ line }) => {
+  switch (line.kind) {
+    case "context":
+    case "change":
+    case "add":
+    case "del":
+      // HTMLElement instances cannot appear multiple times in the DOM,
+      // so give a clone of the rich line's element to <RawElement>.
+      return (
+        <div className={line.kind}>
+          <pre className="lineno">
+            <code>{line.lineNumber}</code>
+          </pre>
+          <RawElement element={line.richLine.cloneNode(true) as HTMLElement} />
+        </div>
+      );
+    case "add-padding":
+    case "change-padding":
+    case "del-padding":
+      return (
+        <div className={line.kind}>
+          <pre className="lineno" />
+          <div>
+            <pre>
+              <code>{line.helpText}</code>
+            </pre>
+          </div>
+        </div>
+      );
+    default:
+      return assertNever(line);
+  }
 };
