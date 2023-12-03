372a7461cc5969dfacb455e256fa58825ff1e168
Ben North <ben.north@tcd.ie>
2023-12-03 14:40:08 +0000

    HelpStage: Define local component

--- src/components/Junior/lesson/LearnerTask.tsx
+++ src/components/Junior/lesson/LearnerTask.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  LearnerTaskHelpStage,
   LearnerTaskHelpStageFragment,
 } from "../../../model/junior/jr-tutorial";
 import { assertNever } from "../../../utils";
@@ -25,5 +26,34 @@ const HelpStageFragment: React.FC<HelpStageFragmentProps> = ({ fragment }) => {
 
   return content == null ? null : (
     <div className="HelpStageFragment">{content}</div>
+  );
+};
+
+type HelpStageProps = {
+  stageIndex: number;
+  nStagesShown: number;
+  keyPath: string;
+  stage: LearnerTaskHelpStage;
+};
+const HelpStage: React.FC<HelpStageProps> = ({
+  stageIndex,
+  nStagesShown,
+  keyPath,
+  stage,
+}) => {
+  if (stageIndex >= nStagesShown) {
+    return null;
+  }
+
+  const content = stage.fragments.map((fragment, idx) => (
+    <HelpStageFragment key={idx} fragment={fragment} />
+  ));
+  return (
+    <>
+      <div className="help-stage-divider" />
+      <div key={keyPath} className="LearnerTask-HelpStage">
+        {content}
+      </div>
+    </>
   );
 };
