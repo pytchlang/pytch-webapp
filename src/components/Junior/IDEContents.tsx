cedad5f97e12dc233e2d862b180e7d53eb19d7ce
Ben North <ben.north@tcd.ie>
2023-12-03 14:54:12 +0000

    IDEContents: Render lesson content if reqd

--- src/components/Junior/IDEContents.tsx
+++ src/components/Junior/IDEContents.tsx
@@ -5,19 +5,28 @@ import { StageAndActorsList } from "./StageAndActorsList";
 import { EmptyProps } from "../../utils";
 import { useStoreState } from "../../store";
 import { Modals } from "./Modals";
+import { MaybeContent } from "./lesson/MaybeContent";
+import classNames from "classnames";
+import { useHasLinkedLesson } from "./lesson/hooks";
 
 export const IDEContents: React.FC<EmptyProps> = () => {
   const projectName = useStoreState(
     (state) => state.activeProject.project.name
   );
+  const hasLinkedLesson = useHasLinkedLesson();
+
+  const classes = classNames("Junior-IDEContents", "abs-0000", {
+    hasLinkedLesson,
+  });
 
   return (
     <>
       <Modals />
       <DivSettingWindowTitle
-        className="Junior-IDEContents abs-0000"
+        className={classes}
         windowTitle={`Pytch: ${projectName}`}
       >
+        <MaybeContent />
         <EditorAndInfo />
         <StageAndActorsList />
       </DivSettingWindowTitle>
