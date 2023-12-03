365e2e5b06a5b5768078207b10e39289aaec01d4
Ben North <ben.north@tcd.ie>
2023-12-03 14:54:43 +0000

    IDEContents: Connect to live-reload server

--- src/components/Junior/IDEContents.tsx
+++ src/components/Junior/IDEContents.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DivSettingWindowTitle } from "../DivSettingWindowTitle";
 import { EditorAndInfo } from "./EditorAndInfo";
 import { StageAndActorsList } from "./StageAndActorsList";
 import { EmptyProps } from "../../utils";
-import { useStoreState } from "../../store";
+import { useStoreActions, useStoreState } from "../../store";
 import { Modals } from "./Modals";
 import { MaybeContent } from "./lesson/MaybeContent";
 import classNames from "classnames";
@@ -14,6 +14,12 @@ export const IDEContents: React.FC<EmptyProps> = () => {
     (state) => state.activeProject.project.name
   );
   const hasLinkedLesson = useHasLinkedLesson();
+
+  const maybeConnectToLiveReloadServer = useStoreActions(
+    (actions) => actions.reloadServer.maybeConnect
+  );
+
+  useEffect(() => maybeConnectToLiveReloadServer());
 
   const classes = classNames("Junior-IDEContents", "abs-0000", {
     hasLinkedLesson,
