3eaed9a8ef45063cc1512a6941f96536ed4842d7
Ben North <ben.north@tcd.ie>
2023-12-06 19:39:31 +0000

    useHasLinkedLesson(): Define hook

--- src/components/Junior/lesson/hooks.ts
+++ src/components/Junior/lesson/hooks.ts
@@ -1,6 +1,18 @@
 import { useStoreState } from "../../../store";
 import { LinkedJrTutorial } from "../../../model/junior/jr-tutorial";
 
+export const useHasLinkedLesson = (): boolean =>
+  useStoreState((state) => {
+    const loadState = state.activeProject.linkedContentLoadingState;
+
+    return (
+      (loadState.kind === "succeeded" &&
+        loadState.linkedContent.kind === "jr-tutorial") ||
+      (loadState.kind === "pending" &&
+        loadState.linkedContentRef.kind === "jr-tutorial")
+    );
+  });
+
 export const useLinkedJrTutorial = (): LinkedJrTutorial =>
   useStoreState((state) => {
     const contentState = state.activeProject.linkedContentLoadingState;
