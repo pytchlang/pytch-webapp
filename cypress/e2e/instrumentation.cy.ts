2f28093bf8a6f010601dcc8e6d8c23c4b19f86ea
Ben North <ben.north@tcd.ie>
2023-12-21 10:11:00 +0000

    awaitInstrumentationEvent(): Define test helper

--- cypress/e2e/instrumentation.cy.ts
+++ cypress/e2e/instrumentation.cy.ts
@@ -4,5 +4,8 @@ const initIntercept = () => {
   cy.intercept({ pathname: "ok.txt" }, { body: "OK\n" }).as("ok-endpoint");
 };
 
+const awaitInstrumentationEvent = () =>
+  cy.wait("@ok-endpoint").its("request.query").should("have.property", "evt");
+
 context("Send anonymous instrumentation events", () => {
 });
