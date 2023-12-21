492627f6d349ff534067a17bc5cb7ce7a9dd3a31
Ben North <ben.north@tcd.ie>
2023-12-21 10:10:51 +0000

    initIntercept(): Define test helper

--- cypress/e2e/instrumentation.cy.ts
+++ cypress/e2e/instrumentation.cy.ts
@@ -1,4 +1,8 @@
 /// <reference types="cypress" />
 
+const initIntercept = () => {
+  cy.intercept({ pathname: "ok.txt" }, { body: "OK\n" }).as("ok-endpoint");
+};
+
 context("Send anonymous instrumentation events", () => {
 });
