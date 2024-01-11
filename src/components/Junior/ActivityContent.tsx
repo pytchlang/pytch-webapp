a2f7694bc41fc639e88f2c6161a13da9d6e25f55
Ben North <ben.north@tcd.ie>
2024-01-11 16:56:55 +0000

    HelpSidebar: Define local component

--- src/components/Junior/ActivityContent.tsx
+++ src/components/Junior/ActivityContent.tsx
@@ -1,1 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useStoreActions } from "../../store";
+import { HelpSidebarInnerContent } from "../HelpSidebar";
+
+const HelpSidebar = () => {
+  const ensureHaveContent = useStoreActions(
+    (actions) => actions.ideLayout.helpSidebar.ensureHaveContent
+  );
+
+  useEffect(() => {
+    ensureHaveContent();
+  });
+
+  return (
+    <div className="HelpSidebar">
+      <div className="content">
+        <div className="inner-content">
+          <HelpSidebarInnerContent activeProgramKind="per-method" />
+        </div>
+      </div>
+    </div>
+  );
+};
