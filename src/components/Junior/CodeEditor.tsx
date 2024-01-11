da1f4d756c40d5d32ef06b61241170b28ce934d7
Ben North <ben.north@tcd.ie>
2024-01-11 16:23:05 +0000

    HelpSidebarMachinery: Remove (jr)

--- src/components/Junior/CodeEditor.tsx
+++ src/components/Junior/CodeEditor.tsx
@@ -2,9 +2,6 @@ import React, { useEffect } from "react";
 import { useStoreState, useStoreActions } from "../../store";
 
 import { ActorSummaryOps } from "../../model/junior/structured-program/actor";
-import classNames from "classnames";
-import { HelpSidebar } from "./HelpSidebar";
-import { HelpSidebarOpenControl } from "../HelpSidebar";
 import { useJrEditActions, useJrEditState, useMappedProgram } from "./hooks";
 import { StructuredProgramOps } from "../../model/junior/structured-program";
 import { NoContentHelp } from "./NoContentHelp";
@@ -24,22 +21,6 @@ const AddHandlerButton: React.FC<EmptyProps> = () => {
   };
 
   return <AddSomethingSingleButton onClick={launchAdd} />;
-};
-
-const HelpSidebarMachinery = () => {
-  // TODO: The below makes this state be shared between editors for
-  // different program-kinds; maybe that's correct?
-  const isVisible = useStoreState(
-    (state) => state.ideLayout.helpSidebar.isVisible
-  );
-  const stateClass = isVisible ? "expanded" : "collapsed";
-
-  const classes = classNames("Junior-HelpSidebarMachinery", stateClass);
-  return (
-    <div className={classes}>
-      {isVisible ? <HelpSidebar /> : <HelpSidebarOpenControl />}
-    </div>
-  );
 };
 
 const ScriptsEditor = () => {
@@ -118,7 +99,6 @@ export const CodeEditor = () => {
 
   return (
     <div className="Junior-CodeEditor abs-0000">
-      <HelpSidebarMachinery />
       <ScriptsEditor />
     </div>
   );
