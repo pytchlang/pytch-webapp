1df763f63fc069bf518508de7abf46a29c5c3edd
Ben North <ben.north@tcd.ie>
2024-01-11 16:55:29 +0000

    ActivityBarTab: Define local component

--- src/components/Junior/ActivityBar.tsx
+++ src/components/Junior/ActivityBar.tsx
@@ -3,6 +3,9 @@ import {
   ActivityContentState,
   ActivityBarTabKey,
 } from "../../model/junior/edit-state";
+import { useJrEditActions } from "./hooks";
+import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconName } from "@fortawesome/fontawesome-common-types";
 
 type TabKeyUiDetails = { icon: IconName; tooltip: string };
@@ -22,3 +25,22 @@ function uiDetailsFromTabKey(tab: ActivityBarTabKey): TabKeyUiDetails {
 
 const tabIsActive = (tab: ActivityBarTabKey, barState: ActivityContentState) =>
   barState.kind === "expanded" && barState.tab === tab;
+
+type ActivityBarTabProps = { tab: ActivityBarTabKey; isActive: boolean };
+const ActivityBarTab: React.FC<ActivityBarTabProps> = ({ tab, isActive }) => {
+  const collapseAction = useJrEditActions((a) => a.collapseActivityContent);
+  const expandAction = useJrEditActions((a) => a.expandActivityContent);
+
+  const onClick = isActive ? () => collapseAction() : () => expandAction(tab);
+  const uiDetails = uiDetailsFromTabKey(tab);
+  const classes = classNames("ActivityBarTab", { isActive });
+
+  return (
+    <div className={classes} onClick={onClick}>
+      <div className="tabkey-icon">
+        <FontAwesomeIcon icon={uiDetails.icon} />
+      </div>
+      <div className="tabkey-tooltip">{uiDetails.tooltip}</div>
+    </div>
+  );
+};
