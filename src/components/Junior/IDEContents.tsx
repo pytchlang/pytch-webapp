bc6debf758a1515a33d3c0c6fd6d8aa600d5485c
Ben North <ben.north@tcd.ie>
2024-01-11 16:58:38 +0000

    IDEContents: Render activity bar and content

--- src/components/Junior/IDEContents.tsx
+++ src/components/Junior/IDEContents.tsx
@@ -5,15 +5,18 @@ import { StageAndActorsList } from "./StageAndActorsList";
 import { EmptyProps } from "../../utils";
 import { useStoreActions, useStoreState } from "../../store";
 import { Modals } from "./Modals";
-import { MaybeContent } from "./lesson/MaybeContent";
 import classNames from "classnames";
-import { useHasLinkedLesson } from "./lesson/hooks";
+import { ActivityBar } from "./ActivityBar";
+import { useJrEditState } from "./hooks";
+import { ActivityContent } from "./ActivityContent";
 
 export const IDEContents: React.FC<EmptyProps> = () => {
   const projectName = useStoreState(
     (state) => state.activeProject.project.name
   );
-  const hasLinkedLesson = useHasLinkedLesson();
+  const activityContentFullStateLabel = useJrEditState(
+    (s) => s.activityContentFullStateLabel
+  );
 
   const maybeConnectToLiveReloadServer = useStoreActions(
     (actions) => actions.reloadServer.maybeConnect
@@ -21,9 +24,11 @@ export const IDEContents: React.FC<EmptyProps> = () => {
 
   useEffect(() => maybeConnectToLiveReloadServer());
 
-  const classes = classNames("Junior-IDEContents", "abs-0000", {
-    hasLinkedLesson,
-  });
+  const classes = classNames(
+    "Junior-IDEContents",
+    "abs-0000",
+    `activity-content-${activityContentFullStateLabel}`
+  );
 
   return (
     <>
@@ -32,7 +37,8 @@ export const IDEContents: React.FC<EmptyProps> = () => {
         className={classes}
         windowTitle={`Pytch: ${projectName}`}
       >
-        <MaybeContent />
+        <ActivityBar />
+        <ActivityContent />
         <EditorAndInfo />
         <StageAndActorsList />
       </DivSettingWindowTitle>
