ebf984a686f110522a95f58aefd287891895823b
Ben North <ben.north@tcd.ie>
2024-01-11 16:52:31 +0000

    uiDetailsFromTabKey(): Define function
    And helper const map.

--- src/components/Junior/ActivityBar.tsx
+++ src/components/Junior/ActivityBar.tsx
@@ -1,4 +1,20 @@
 import React from "react";
+import {
+  ActivityBarTabKey,
+} from "../../model/junior/edit-state";
 import { IconName } from "@fortawesome/fontawesome-common-types";
 
 type TabKeyUiDetails = { icon: IconName; tooltip: string };
+
+const uiDetailsFromTabKeyLut = new Map<ActivityBarTabKey, TabKeyUiDetails>([
+  ["helpsidebar", { icon: "question-circle", tooltip: "Scratch/Python help" }],
+  ["lesson", { icon: "book", tooltip: "Lesson content" }],
+]);
+
+function uiDetailsFromTabKey(tab: ActivityBarTabKey): TabKeyUiDetails {
+  const mDetails = uiDetailsFromTabKeyLut.get(tab);
+  if (mDetails == null) {
+    throw new Error(`unrecognised tab-key name "${tab}"`);
+  }
+  return mDetails;
+}
