0fcacd151dfde589244c2a1cc1d88aed7b89ff8b
Ben North <ben.north@tcd.ie>
2024-01-18 20:23:55 +0000

    Add "launches with lesson activity open" test

--- cypress/e2e/junior/lesson.cy.ts
+++ cypress/e2e/junior/lesson.cy.ts
@@ -2,4 +2,10 @@ context("Navigation of per-method lesson", () => {
   beforeEach(() => {
     cy.pytchJrLesson();
   });
+
+  it("launches with lesson activity open", () => {
+    cy.get(".ActivityContent .Junior-LessonContent-container").should(
+      "be.visible"
+    );
+  });
 });
