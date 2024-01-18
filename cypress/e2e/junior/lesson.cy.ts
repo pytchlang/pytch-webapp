6e7722ace3d2c44a4b9812c1eba49cbcf3479dd1
Ben North <ben.north@tcd.ie>
2024-01-18 20:24:14 +0000

    Define next-/prev-chapter test helpers
    Define e2e test helpers
    
        clickToNextChapter()
        clickToPrevChapter()

--- cypress/e2e/junior/lesson.cy.ts
+++ cypress/e2e/junior/lesson.cy.ts
@@ -1,3 +1,5 @@
+import { clickUniqueSelected } from "./utils";
+
 context("Navigation of per-method lesson", () => {
   beforeEach(() => {
     cy.pytchJrLesson();
@@ -8,4 +10,12 @@ context("Navigation of per-method lesson", () => {
       "be.visible"
     );
   });
+
+  function clickToNextChapter() {
+    clickUniqueSelected(".Junior-ChapterNavigation button.next");
+  }
+
+  function clickToPrevChapter() {
+    clickUniqueSelected(".Junior-ChapterNavigation button.prev");
+  }
 });
