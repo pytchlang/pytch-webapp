bb0494138259f05c7f8a7626351d4bd94a9350ab
Ben North <ben.north@tcd.ie>
2024-01-25 13:47:56 +0000

    EditorKindThumbnail: Take optional "size" prop

--- src/components/EditorKindThumbnail.tsx
+++ src/components/EditorKindThumbnail.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import FlatEditorThumbnail from "../images/flat-80h.png";
-import PerMethodEditorThumbnail from "../images/per-method-80h.png";
+import FlatEditorThumbnail48 from "../images/flat-48h.png";
+import PerMethodEditorThumbnail48 from "../images/per-method-48h.png";
+import FlatEditorThumbnail80 from "../images/flat-80h.png";
+import PerMethodEditorThumbnail80 from "../images/per-method-80h.png";
 import { PytchProgramKind } from "../model/pytch-program";
 
-type EditorKindThumbnailProps = { programKind: PytchProgramKind };
+type EditorKindThumbnailProps = {
+  programKind: PytchProgramKind;
+  size?: "lg" | "sm";
+};
 export const EditorKindThumbnail: React.FC<EditorKindThumbnailProps> = ({
   programKind,
+  size,
 }) => {
+  const effectiveSize = size ?? "lg";
+
   const editorKindThumbnail =
-    programKind === "flat" ? FlatEditorThumbnail : PerMethodEditorThumbnail;
+    effectiveSize === "lg"
+      ? programKind === "flat"
+        ? FlatEditorThumbnail80
+        : PerMethodEditorThumbnail80
+      : programKind === "flat"
+      ? FlatEditorThumbnail48
+      : PerMethodEditorThumbnail48;
 
   return (
     <div className="EditorKindThumbnail">
