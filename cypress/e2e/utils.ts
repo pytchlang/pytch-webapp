9dedb3c125039320473f496b2367d3c917495531
Ben North <ben.north@tcd.ie>
2024-01-26 10:35:28 +0000

    saveButton: Define test helper object

---
+++ cypress/e2e/utils.ts
@@ -0,0 +1,39 @@
+/** Object with function properties to help with testing behaviour of
+ * the Save button.  In most cases, tests should be able to use:
+ *
+ * * `shouldReactToInteraction(interaction)` — assert that the Save
+ *   button is unlit; perform the given `interaction()`; assert that the
+ *   Save button is lit; click it; assert that it's not lit.
+ *
+ * The following finer-grained functions also exist if more control is
+ * needed:
+ *
+ * * `click()` — click the Save button
+ * * `shouldShowNoUnsavedChanges()` — assert that the Save button is in
+ *   its normal, unhighlighted state, indicating that there are no
+ *   unsaved changes
+ * * `shouldShowUnsavedChanges()` — assert that the Save button is in
+ *   its highlighted state, indicating that there **are** unsaved
+ *   changes
+ *   */
+export const saveButton = (() => {
+  const button = () => cy.get("button.save-button");
+  const assertClass = (cls: string) => () => button().should("have.class", cls);
+
+  const click = () => button().click();
+  const shouldShowNoUnsavedChanges = assertClass("no-changes-since-last-save");
+  const shouldShowUnsavedChanges = assertClass("unsaved-changes-exist");
+
+  return {
+    click,
+    shouldShowNoUnsavedChanges,
+    shouldShowUnsavedChanges,
+    shouldReactToInteraction(interaction: () => void) {
+      shouldShowNoUnsavedChanges();
+      interaction();
+      shouldShowUnsavedChanges();
+      click();
+      shouldShowNoUnsavedChanges();
+    },
+  };
+})();
