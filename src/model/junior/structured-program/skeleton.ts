5913e5962d3768e9778eb6378202318d8c89ee81
Ben North <ben.north@tcd.ie>
2024-02-01 09:58:49 +0000

    IEmbodyContext: Define, export interface

--- src/model/junior/structured-program/skeleton.ts
+++ src/model/junior/structured-program/skeleton.ts
@@ -1,3 +1,4 @@
+import { Uuid } from "./core-types";
 import { ActorKind } from "./actor";
 import { EventDescriptor } from "./event";
 
@@ -20,3 +21,13 @@ export type NoIdActor = {
 export type NoIdsStructuredProject = {
   actors: Array<NoIdActor>;
 };
+
+/** When constructing a `StructuredProgram` (and in particular its
+ * actors) from a `NoIdsStructuredProject` (i.e., "embodying" it), we
+ * need to also keep track of the assets within the
+ * `NoIdsStructuredProject`.  This is done by an object implementing
+ * `IEmbodyContext`, which has a method the "embodiment" process can use
+ * to register each actor's assets. */
+export interface IEmbodyContext {
+  registerActorAsset(actorId: Uuid, assetBasename: string): void;
+}
