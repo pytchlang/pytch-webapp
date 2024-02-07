feed600c2160fcf790a4adb0d048ddb0782fcaae
Ben North <ben.north@tcd.ie>
2024-02-07 14:16:42 +0000

    AddSomethingButton: Take, heed "what" prop

--- src/components/Junior/AddSomethingButton.tsx
+++ src/components/Junior/AddSomethingButton.tsx
@@ -6,16 +6,19 @@ import { ActorKind } from "../../model/junior/structured-program";
 export type AddSomethingButtonWhat = "sprite" | "script" | `${ActorKind}-asset`;
 
 export type AddSomethingButtonProps = {
+  what: AddSomethingButtonWhat;
   label?: string;
   onClick: React.MouseEventHandler;
 };
 
 export const AddSomethingButton: React.FC<AddSomethingButtonProps> = ({
+  what,
   label,
   onClick,
 }) => {
+  const classes = classNames("AddSomethingButton", `add-${what}`);
   return (
-    <div className="AddSomethingButton" onClick={onClick}>
+    <div className={classes} onClick={onClick}>
       {label && <span className="label">{label}</span>}
       <span className="icon">
         <FontAwesomeIcon className="fa-lg" icon="plus" />
