f618ff31d075420e1a1aa35676060bb558b707a0
Ben North <ben.north@tcd.ie>
2024-02-15 11:56:35 +0000

    ScratchAndButtons: Remove copy button

--- src/components/HelpSidebar.tsx
+++ src/components/HelpSidebar.tsx
@@ -98,10 +98,7 @@ const ScratchAndButtons: React.FC<IScratchAndPython & IToggleHelp> = (
   return (
     <div className={`scratch-with-buttons${maybeLongClass}`}>
       <div className="scratch-block-wrapper" ref={scratchRef} />
-      <div className="buttons">
-        <HelpToggleButton {...props} />
-        <MaybeCopyButton pythonToCopy={props.pythonToCopy} />
-      </div>
+      <HelpToggleButton {...props} />
     </div>
   );
 };
