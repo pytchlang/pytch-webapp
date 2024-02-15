0d32392d866413c76de26aafe626b997ccd3e746
Ben North <ben.north@tcd.ie>
2024-02-15 12:19:58 +0000

    setScriptCode(): Define test helper

--- cypress/e2e/junior/module-imports.cy.ts
+++ cypress/e2e/junior/module-imports.cy.ts
@@ -1,5 +1,15 @@
+import { selectActorAspect, selectSprite } from "./utils";
+
 context("Use Python stdlib modules", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
   });
+
+  const setScriptCode = (codeText: string) => {
+    selectSprite("Snake");
+    selectActorAspect("Code");
+    cy.get(".PytchScriptEditor .ace_editor").type(
+      `{selectAll}{del}${codeText}`
+    );
+  };
 });
