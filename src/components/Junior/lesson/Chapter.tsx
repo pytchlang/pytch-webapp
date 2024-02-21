11d55c379dca3eafd2602bbaa74280046a5642b4
Ben North <ben.north@tcd.ie>
2024-02-21 11:56:14 +0000

    ChapterChunk: Remove
    In preparation for redesigning Chapter component.

--- src/components/Junior/lesson/Chapter.tsx
+++ src/components/Junior/lesson/Chapter.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import {
   JrTutorialChapter,
-  JrTutorialChapterChunk,
 } from "../../../model/junior/jr-tutorial";
 import { assertNever } from "../../../utils";
 import { LearnerTask } from "./LearnerTask";
 import { RawOrScratchBlock } from "./RawOrScratchBlock";
-
-type ChapterChunkProps = { keyPath: string; chunk: JrTutorialChapterChunk };
-const ChapterChunk: React.FC<ChapterChunkProps> = ({ keyPath, chunk }) => {
-  switch (chunk.kind) {
-    case "element":
-      return <RawOrScratchBlock element={chunk.element} />;
-    case "learner-task":
-      return <LearnerTask key={keyPath} keyPath={keyPath} task={chunk.task} />;
-    default:
-      return assertNever(chunk);
-  }
-};
 
 type ChapterProps = { chapter: JrTutorialChapter };
 export const Chapter: React.FC<ChapterProps> = ({ chapter }) => {
