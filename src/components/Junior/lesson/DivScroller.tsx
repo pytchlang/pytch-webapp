1453546d8708d8b497f6c88b12f1700e76eb6b1f
Ben North <ben.north@tcd.ie>
2024-02-21 14:47:58 +0000

    DivScroller: Use just chapterIndex as state

--- src/components/Junior/lesson/DivScroller.tsx
+++ src/components/Junior/lesson/DivScroller.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useLinkedJrTutorial } from "./hooks";
+import { useMappedLinkedJrTutorial } from "./hooks";
 
 export type DivScrollerProps = {
   containerDivRef: React.RefObject<HTMLDivElement>;
@@ -8,8 +8,9 @@ export type DivScrollerProps = {
 // TODO: Compare this and the equivalent for "flat" tutorials and settle
 // on one approach.
 export const DivScroller: React.FC<DivScrollerProps> = (props) => {
-  const linkedTutorial = useLinkedJrTutorial();
-  const chapterIndex = linkedTutorial.interactionState.chapterIndex;
+  const chapterIndex = useMappedLinkedJrTutorial(
+    (tutorial) => tutorial.interactionState.chapterIndex
+  );
   const [lastScrolledChapIdx, setLastScrolledChapIdx] = useState(0);
 
   useEffect(() => {
