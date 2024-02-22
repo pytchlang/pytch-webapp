fb273bb1351800708ab92f9f6b12aa13d5567ecb
Ben North <ben.north@tcd.ie>
2024-02-22 11:30:15 +0000

    WidthMonitor: Take prop for non-stage width

--- src/components/Junior/WidthMonitor.tsx
+++ src/components/Junior/WidthMonitor.tsx
@@ -1,24 +1,23 @@
 import React, { useEffect } from "react";
-import { EmptyProps } from "../../utils";
 import { useStoreActions } from "../../store";
 import { stageWidth } from "../../constants";
 
 const minStageWidth = (2 * stageWidth) / 3;
-export const WidthMonitor: React.FC<EmptyProps> = () => {
+
+type WidthMonitorProps = {
+  nonStageWd: number;
+};
+export const WidthMonitor: React.FC<WidthMonitorProps> = ({ nonStageWd }) => {
   const setStageDisplayWidth = useStoreActions(
     (actions) => actions.ideLayout.setStageDisplayWidth
   );
 
   const handleResize = () => {
-    // TODO: This "1100" is a bit magic; it came from the min width of
-    // the first two columns (512 each) then adding a bit.  Do something
-    // more sensible for this.
-    //
     // TODO: Various places that use the displaySize state should
     // provide a custom equality function to avoid needless
     // re-rendering.
     //
-    const stageWdToFill = window.innerWidth - 1100;
+    const stageWdToFill = window.innerWidth - nonStageWd;
     const targetWidth = Math.min(
       stageWidth,
       Math.max(minStageWidth, stageWdToFill)
