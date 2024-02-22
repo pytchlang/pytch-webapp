35f58af2fac63fdd857b3c04b65135594c242e29
Ben North <ben.north@tcd.ie>
2024-02-22 11:28:25 +0000

    WidthMonitor: Move to own file

--- src/components/Junior/lesson/Content.tsx
+++ src/components/Junior/lesson/Content.tsx
@@ -1,45 +1,11 @@
-import React, { UIEventHandler, createRef, useEffect } from "react";
+import React, { UIEventHandler, createRef } from "react";
 import { EmptyProps } from "../../../utils";
 import { ChapterNavigation } from "./ChapterNavigation";
 import { Chapter } from "./Chapter";
 import { ProgressTrail } from "./ProgressTrail";
 import { DivScroller } from "./DivScroller";
-import { useStoreActions } from "../../../store";
-import { stageWidth } from "../../../constants";
 import { useJrEditActions } from "../hooks";
-
-const minStageWidth = (2 * stageWidth) / 3;
-
-const WidthMonitor: React.FC<EmptyProps> = () => {
-  const setStageDisplayWidth = useStoreActions(
-    (actions) => actions.ideLayout.setStageDisplayWidth
-  );
-
-  const handleResize = () => {
-    // TODO: This "1100" is a bit magic; it came from the min width of
-    // the first two columns (512 each) then adding a bit.  Do something
-    // more sensible for this.
-    //
-    // TODO: Various places that use the displaySize state should
-    // provide a custom equality function to avoid needless
-    // re-rendering.
-    //
-    const stageWdToFill = window.innerWidth - 1100;
-    const targetWidth = Math.min(
-      stageWidth,
-      Math.max(minStageWidth, stageWdToFill)
-    );
-    setStageDisplayWidth(targetWidth);
-  };
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  });
-
-  return null;
-};
+import { WidthMonitor } from "../WidthMonitor";
 
 export const Content: React.FC<EmptyProps> = () => {
   const contentRef = createRef<HTMLDivElement>();
