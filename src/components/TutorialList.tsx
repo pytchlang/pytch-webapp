7c7cacf3634e43facc05ab7abb605a8e97bbbcd6
Ben North <ben.north@tcd.ie>
2024-03-15 14:43:28 +0000

    ToggleUiStyleLink: Define local component

--- src/components/TutorialList.tsx
+++ src/components/TutorialList.tsx
@@ -3,8 +3,9 @@ import NavBanner from "./NavBanner";
 import { useStoreActions, useStoreState } from "../store";
 import { SyncState } from "../model/project";
 import { TutorialSummaryDisplay } from "./TutorialSummaryDisplay";
-import { EmptyProps } from "../utils";
+import { EmptyProps, assertNever } from "../utils";
 import { PytchProgramKind } from "../model/pytch-program";
+import { useSetActiveUiVersionFun } from "./hooks/active-ui-version";
 
 const LoadingTutorialsPlaceholder = () => {
   const syncState = useStoreState(
@@ -18,6 +19,41 @@ const LoadingTutorialsPlaceholder = () => {
       <p>Loading...</p>
     </div>
   );
+};
+
+const ToggleUiStyleLink: React.FC<EmptyProps> = () => {
+  const activeUiVersion = useStoreState(
+    (state) => state.versionOptIn.activeUiVersion
+  );
+  const setUiVersion1 = useSetActiveUiVersionFun("v1");
+  const setUiVersion2 = useSetActiveUiVersionFun("v2");
+
+  const content = (() => {
+    switch (activeUiVersion) {
+      case "v1":
+        return (
+          <p className="tutorials-change-ui-style">
+            <span onClick={setUiVersion2}>
+              Show our new script-by-script tutorial!
+            </span>
+          </p>
+        );
+      case "v2":
+        return (
+          <p className="tutorials-change-ui-style">
+            This list includes our new script-by-script tutorial. You can{" "}
+            <span onClick={setUiVersion1}>
+              see just the classic Pytch tutorials
+            </span>{" "}
+            if you prefer.
+          </p>
+        );
+      default:
+        return assertNever(activeUiVersion);
+    }
+  })();
+
+  return <div className="Tutorials-ToggleUiStyleLink">{content}</div>;
 };
 
 const TutorialList: React.FC<EmptyProps> = () => {
