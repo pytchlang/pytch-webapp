f2ab36613172ad1a1fe65f8dee0b58b587db2b9e
Ben North <ben.north@tcd.ie>
2024-03-15 15:33:56 +0000

    Add (forward-looking) docstrings
    The reference to "the panel" is to a feature which will be committed
    soon.

--- src/model/version-opt-in.ts
+++ src/model/version-opt-in.ts
@@ -1,7 +1,14 @@
 import { Action as GenericAction } from "easy-peasy";
 import { propSetterAction } from "../utils";
 
+/** Whether we are showing the "Pytch v2" user-interface elements, such
+ * as the ability to create script-by-script projects and the
+ * script-by-script tutorial. */
 export type VersionTag = "v1" | "v2";
+
+/** The user can launch operations from the panel explaining what the
+ * new v2 features are.  The type `V2_OperationState` describes whether
+ * such an operation is currently in progress.  */
 export type V2_OperationState = "idle" | "in-progress";
 
 // "Slice action" — Action<> specialised for this slice-type.
