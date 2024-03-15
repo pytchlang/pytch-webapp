3d56a200a0aadecc22dc2fa7d686b1c29238e9de
Ben North <ben.north@tcd.ie>
2024-03-15 13:59:46 +0000

    SAction: Define generic type

--- src/model/version-opt-in.ts
+++ src/model/version-opt-in.ts
@@ -1,4 +1,9 @@
+import { Action as GenericAction } from "easy-peasy";
+
 export type VersionTag = "v1" | "v2";
+
+// "Slice action" — Action<> specialised for this slice-type.
+type SAction<PayloadT> = GenericAction<VersionOptIn, PayloadT>;
 
 export type VersionOptIn = {
 };
