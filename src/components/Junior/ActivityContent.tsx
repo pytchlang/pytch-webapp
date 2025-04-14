ac6019abfcbb0bdc823198ab1d3545fff588ec4b
Ben North <ben.north@tcd.ie>
2025-04-14 14:40:42 +0100

    ActivityContent: Remove previous key handling
    To perhaps be revisited at some point, but for now it has been observed
    to cause frustration with C-c not working.

--- src/components/Junior/ActivityContent.tsx
+++ src/components/Junior/ActivityContent.tsx
@@ -1,31 +1,17 @@
-import React, { KeyboardEventHandler } from "react";
+import React from "react";
 import { EmptyProps, assertNever } from "../../utils";
 import { useJrEditState } from "./hooks";
 import { MaybeContent as MaybeLessonContent } from "./lesson/MaybeContent";
-import { aceControllerMap } from "../../skulpt-connection/code-editor";
 import { WidthMonitor } from "./WidthMonitor";
 import { HelpSidebar } from "../HelpSidebar";
 import Tutorial from "../Tutorial";
 
 export const ActivityContent: React.FC<EmptyProps> = () => {
   const s = useJrEditState((s) => s.activityContentState);
-  const handlerId = useJrEditState((s) => s.mostRecentFocusedEditor);
 
   if (s.kind === "collapsed") {
     return <WidthMonitor nonStageWd={576} />;
   }
-
-  const onKey: KeyboardEventHandler = (event) => {
-    const mController = aceControllerMap.get(handlerId);
-    if (mController != null) {
-      // This seems to be enough: looks like the editor reacts to the
-      // "keyup" event, which it duly receives.  Shift and control seem
-      // OK too.
-      mController.editor.focus();
-    } else {
-      (event.target as HTMLDivElement).blur();
-    }
-  };
 
   const content = (() => {
     switch (s.tab) {
@@ -53,7 +39,6 @@ export const ActivityContent: React.FC<EmptyProps> = () => {
     <div
       className="ActivityContent-container"
       tabIndex={-1}
-      onKeyDown={onKey}
       id={`pytch:activity-bar-tab:tabpanel:${s.tab}`}
       role="tabpanel"
       aria-labelledby={`pytch:activity-bar-tab:tab:${s.tab}`}
