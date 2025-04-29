96106d6e09b1417f10c1719ecec1ee0a996d5416
Ben North <ben.north@tcd.ie>
2025-04-29 17:20:04 +0100

    Container: Store idNub in top-level DIV data attr

--- src/components/ListOfThings.tsx
+++ src/components/ListOfThings.tsx
@@ -84,7 +84,7 @@ const Container: React.FC<PropsWithChildren<ListOfThingsProps>> = ({
 
   return (
     <ContainerContext.Provider value={contextValue}>
-    <div tabIndex={-1} onKeyDown={containerKeyDown}>
+    <div data-list-id-nub={idNub} tabIndex={-1} onKeyDown={containerKeyDown}>
       {children}
     </div>
     </ContainerContext.Provider>
