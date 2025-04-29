c8c22c4e3e85dcc10ef69d2c7f6e026e06c4e1b6
Ben North <ben.north@tcd.ie>
2025-04-29 16:50:07 +0100

    AddSthButton: Add containing-list attr from ctx

--- src/components/Junior/AddSomethingButton.tsx
+++ src/components/Junior/AddSomethingButton.tsx
@@ -1,7 +1,8 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
 import { ActorKind } from "../../model/junior/structured-program";
+import { ListOfThings } from "../ListOfThings";
 
 export type AddSomethingButtonWhat =
   | "sprite"
@@ -20,9 +21,16 @@ export const AddSomethingButton: React.FC<AddSomethingButtonProps> = ({
   label,
   onClick,
 }) => {
+  const maybeListContainerCtx = useContext(ListOfThings.ContainerContext);
+
+  const dataProps =
+    maybeListContainerCtx == null
+      ? {}
+      : { "data-containing-list-id-nub": maybeListContainerCtx.idNub };
+
   const classes = classNames("AddSomethingButton", `add-${what}`);
   return (
-    <button className={classes} onClick={onClick}>
+    <button className={classes} onClick={onClick} {...dataProps}>
       {label && <span className="label">{label}</span>}
       <span className="icon">
         <FontAwesomeIcon className="fa-lg" icon="plus" />
