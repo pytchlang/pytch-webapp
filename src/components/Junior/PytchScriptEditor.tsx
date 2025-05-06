c864f2f66319d07c41d4aff4ca7c1f2e911f8795
Ben North <ben.north@tcd.ie>
2025-05-06 15:04:46 +0100

    (Reformat)

--- src/components/Junior/PytchScriptEditor.tsx
+++ src/components/Junior/PytchScriptEditor.tsx
@@ -222,7 +222,9 @@ const PytchScriptEditor: React.FC<PytchScriptEditorProps> = ({
   };
 
   return (
-    <CaptiveContextMenu.Container onKeyDown={onKeyDown}>
+    <CaptiveContextMenu.Container
+      onKeyDown={onKeyDown}
+    >
       <li className={classes}>
         <DragPreviewImage connect={preview} src={PytchScriptPreview} />
         <div ref={dropRef}>
