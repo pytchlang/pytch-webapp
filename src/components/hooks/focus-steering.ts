adc4b851368b898ee817b6f1811c5a4919b1ed4d
Ben North <ben.north@tcd.ie>
2025-06-04 12:21:43 +0100

    groupContainerRefCallback: Add slot

--- src/components/hooks/focus-steering.ts
+++ src/components/hooks/focus-steering.ts
@@ -1,7 +1,9 @@
 import { createContext } from "react";
+import { GroupedFocusManager } from "../../model/junior/grouped-focus";
 import { PytchProgramKind } from "../../model/pytch-program";
 
 type FocusContextT = {
+  groupContainerRefCallback: GroupedFocusManager["containerRefCallback"];
 };
 
 export const FocusContext = createContext<FocusContextT | null>(null);
@@ -9,6 +11,12 @@ export const FocusContext = createContext<FocusContextT | null>(null);
 export const createFocusContext = (
   _programKind: PytchProgramKind // Will use in due course
 ): FocusContextT => {
+  const groupedFocusManager = new GroupedFocusManager();
+
+  const groupContainerRefCallback =
+    groupedFocusManager.containerRefCallback.bind(groupedFocusManager);
+
   return {
+    groupContainerRefCallback,
   };
 };
