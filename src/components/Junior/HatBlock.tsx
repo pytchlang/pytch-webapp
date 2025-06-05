79032b63749508db1f65cafbbda899e6196095e6
Ben North <ben.north@tcd.ie>
2025-06-05 17:02:26 +0100

    HatBlock: Supply actorKind to update-handler flow

--- src/components/Junior/HatBlock.tsx
+++ src/components/Junior/HatBlock.tsx
@@ -13,7 +13,7 @@ import {
 } from "../../model/junior/structured-program";
 import { assertNever } from "../../utils";
 import { descriptorFromBrowserKeyName } from "../../model/junior/keyboard-layout";
-import { useJrEditActions } from "./hooks";
+import { useFocusedActorKind, useJrEditActions } from "./hooks";
 import { CaptiveContextMenu } from "../CaptiveContextMenu";
 import { useFocusContext } from "../hooks/focus-steering";
 
@@ -116,6 +116,7 @@ export const HatBlock: React.FC<HatBlockProps> = ({
   event,
 }) => {
   const focusContext = useFocusContext("per-method");
+  const focusedActorKind = useFocusedActorKind();
   const launchUpsertAction = useJrEditActions((a) => a.upsertHatBlockFlow.run);
   const reorderHandlers = useStoreActions(
     (actions) => actions.activeProject.reorderHandlers
@@ -127,6 +128,7 @@ export const HatBlock: React.FC<HatBlockProps> = ({
         actorId,
         action: { kind: "update", handlerId, previousEvent: event },
       },
+      actorKind: focusedActorKind,
       onDispose: focusContext.onDisposeChangeHatBlock,
     });
   };
