c8517b9c1519cdba4e2f3017a97741acb696651e
Ben North <ben.north@tcd.ie>
2025-06-09 10:18:42 +0100

    eqPerMethodScriptUpserted(): Update re new slots
    No need to compare `kind`; it's fixed once we have
    PerMethodScriptUpserted instances.  Also note query.

--- src/model/junior/change-events.ts
+++ src/model/junior/change-events.ts
@@ -10,13 +10,15 @@ export type PerMethodScriptUpserted = {
   actorName: string;
 };
 
+// TODO: A given handler will only ever be part of one actor, so think
+// it's OK to not compare the actorKind or actorName properties?
 export function eqPerMethodScriptUpserted(
   x: PerMethodScriptUpserted,
   y: PerMethodScriptUpserted
 ) {
   return (
-    x.kind === y.kind &&
     x.upsertKind === y.upsertKind &&
-    x.handlerId === y.handlerId
+    x.handlerId === y.handlerId &&
+    x.handlerEventKind === y.handlerEventKind
   );
 }
