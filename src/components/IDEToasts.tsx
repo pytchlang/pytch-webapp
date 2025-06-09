e5fac4f6b0ef816f29459834832430b0224a3e48
Ben North <ben.north@tcd.ie>
2025-06-09 10:49:33 +0100

    IDEToast: Define local component

--- src/components/IDEToasts.tsx
+++ src/components/IDEToasts.tsx
@@ -1,4 +1,25 @@
 import React from "react";
+import { Toast } from "react-bootstrap";
+import {
+  KeyedNotableChange,
+  notableChangeDescription,
+} from "../model/notable-changes";
+
+type IDEToastProps = {
+  keyedChange: KeyedNotableChange;
+};
+const IDEToast: React.FC<IDEToastProps> = ({ keyedChange }) => {
+  const change = keyedChange.change;
+  const description = notableChangeDescription(change);
+  return (
+    <Toast>
+      <Toast.Header>
+        <strong className="me-auto">{description.header}</strong>
+      </Toast.Header>
+      <Toast.Body>{description.body}</Toast.Body>
+    </Toast>
+  );
+};
 
 export const IDEToasts: React.FC<object> = () => {
 };
