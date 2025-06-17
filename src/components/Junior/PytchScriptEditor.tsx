bf00c47d8434564820a86ae8736d2407fb808f9f
Ben North <ben.north@tcd.ie>
2025-06-17 13:37:29 +0100

    PytchScriptEditor: Use PytchScriptAceEditor
    Avoids rerendering the hatblock on every keystroke in the editor.

--- src/components/Junior/PytchScriptEditor.tsx
+++ src/components/Junior/PytchScriptEditor.tsx
@@ -1,16 +1,10 @@
 import React, { useEffect } from "react";
-import AceEditor from "react-ace";
-import { PytchAceAutoCompleter } from "../../skulpt-connection/code-completion";
-
 import {
   ActorKind,
   StructuredProgramOps,
   Uuid,
 } from "../../model/junior/structured-program";
-import { useStoreActions } from "../../store";
-
 import {
-  AceEditorT,
   aceControllerMap,
   pendingCursorWarp,
 } from "../../skulpt-connection/code-editor";
@@ -19,7 +13,6 @@ import { HatBlock } from "./HatBlock";
 import classNames from "classnames";
 
 import {
-  useJrEditActions,
   useMappedProgram,
   usePytchScriptDrag,
   usePytchScriptDrop,
@@ -30,16 +23,10 @@ import { DragPreviewImage } from "react-dnd";
 import { useNotableChanges } from "../hooks/notable-changes";
 import { ConjoinedResizeObserver } from "../../model/junior/conjoined-resize-observer";
 import { scrollCursorRowIntoView } from "./PytchScriptEditor-scroller";
-import { DevWorkContext } from "../../model/dev-work-context";
 import { CaptiveContextMenu } from "../CaptiveContextMenu";
 import { kFocusGroupItemClassName } from "../../model/junior/grouped-focus";
 import { useFocusContext } from "../hooks/focus-steering";
-
-// Adapted from https://stackoverflow.com/a/71952718
-const insertElectricFullStop = (editor: AceEditorT) => {
-  editor.insert(".");
-  editor.execCommand("startAutocomplete");
-};
+import { PytchScriptAceEditor } from "./PytchScriptAceEditor";
 
 function queryTextarea(aceId: string) {
   return document.querySelector<HTMLTextAreaElement>(`#${aceId} textarea`);
@@ -66,25 +53,17 @@ export const PytchScriptEditor: React.FC<PytchScriptEditorProps> = ({
   const [dropProps, dropRef] = usePytchScriptDrop(actorId, handlerId);
   const aceParentRef: React.RefObject<HTMLDivElement> = React.createRef();
 
-  const handler = useMappedProgram("<PytchScriptEditor>", (program) =>
-    StructuredProgramOps.uniqueHandlerByIdGlobally(program, handlerId)
+  const handlerEvent = useMappedProgram(
+    "<PytchScriptEditor>",
+    (program) =>
+      StructuredProgramOps.uniqueHandlerByIdGlobally(program, handlerId).event
   );
 
-  const setHandlerPythonCode = useStoreActions(
-    (actions) => actions.activeProject.setHandlerPythonCode
-  );
-  const setMostRecentFocusedEditor = useJrEditActions(
-    (a) => a.setMostRecentFocusedEditor
-  );
   const scriptUpsertedChanges = useNotableChanges(
     "script-upserted",
     (change) => change.handlerId === handlerId
   );
   const justUpserted = scriptUpsertedChanges.length > 0;
-
-  const updateCodeText = (code: string) => {
-    setHandlerPythonCode({ actorId, handlerId, code });
-  };
 
   useEffect(() => {
     const scroll = () => scrollCursorRowIntoView(handlerId);
@@ -144,71 +123,6 @@ export const PytchScriptEditor: React.FC<PytchScriptEditorProps> = ({
     return disconnectObserver;
   }, [aceParentRef, conjoinedResizeObserver]);
 
-  /** Once the editor has loaded, there are a few things we have to do:
-   *
-   * * Make an entry in the EventHandlerId->Editor map.
-   * * Check whether there is a pending cursor-warp request (from the
-   *   user clicking on an error-location button).
-   * * Turn off "overwrite" mode.
-   * * Mark the parent DIV such that e2e tests know everything is ready;
-   *   there was some test flakiness which this seemed to help, but the
-   *   flakiness was hard to reproduce so not certain.
-   *
-   * **TODO: Can some of this be unified with the set-up of the Ace
-   * editor in "flat" mode?**
-   */
-  const onAceEditorLoad = (editor: AceEditorT) => {
-    aceControllerMap.set(handlerId, editor);
-
-    editor.session.setOverwrite(false);
-    editor.commands.removeCommand("overwrite", true);
-
-    editor.commands.addCommand({
-      name: "insertElectricFullStop",
-      bindKey: { mac: ".", win: "." },
-      exec: insertElectricFullStop,
-    });
-
-    // Not sure how reliably this is true, but the onLoad seems to fire
-    // before aceParentRef is set.  In dev mode, this is OK because
-    // React renders everything twice, but when testing against a
-    // production build, we don't ever set the attribute.  Poll until we
-    // can.  (Messy but seems to be working.)
-    function setLoadFiredAttr() {
-      const mDiv = aceParentRef.current;
-      if (mDiv != null) {
-        mDiv.setAttribute("data-on-load-fired", "yes");
-        const mTextArea =
-          mDiv.querySelector<HTMLTextAreaElement>(":scope textarea");
-        if (mTextArea != null) {
-          mTextArea.setAttribute("tabIndex", "-1");
-          if (mTextArea.dataset.escapeHandlerSet == null) {
-            mTextArea.addEventListener("keydown", (evt) => {
-              if (evt.key === "Escape") {
-                focusContext.focusBookmarkedItem("gfs__actorprops");
-              }
-            });
-            mTextArea.dataset.escapeHandlerSet = "yes";
-          }
-        }
-      } else {
-        setTimeout(setLoadFiredAttr, 20);
-      }
-    }
-    setLoadFiredAttr();
-  };
-
-  const onAceEditorFocus = () => {
-    setMostRecentFocusedEditor(handlerId);
-  };
-
-  const nCodeLines = handler.pythonCode.split("\n").length;
-
-  const workContext: DevWorkContext = { programKind: "per-method", actorKind };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const completers = [new PytchAceAutoCompleter(workContext) as any];
-
   const classes = classNames(
     "PytchScriptEditor",
     dragProps,
@@ -246,27 +160,17 @@ export const PytchScriptEditor: React.FC<PytchScriptEditorProps> = ({
               handlerId={handlerId}
               prevHandlerId={prevHandlerId}
               nextHandlerId={nextHandlerId}
-              event={handler.event}
+              event={handlerEvent}
             />
           </div>
         </div>
         <div className="drag-masked-editor">
           <div ref={aceParentRef} id={aceParentDivId}>
             <div className="hat-code-spacer" />
-            <AceEditor
-              mode="python"
-              theme="pytch"
-              enableBasicAutocompletion={completers}
-              value={handler.pythonCode}
-              onChange={updateCodeText}
-              name={aceId}
-              onLoad={onAceEditorLoad}
-              onFocus={onAceEditorFocus}
-              fontSize={14}
-              width="100%"
-              height="100%"
-              minLines={nCodeLines}
-              maxLines={nCodeLines}
+            <PytchScriptAceEditor
+              actorKind={actorKind}
+              actorId={actorId}
+              handlerId={handlerId}
             />
           </div>
           <div className="drag-mask" />
