67b243b38df6cc5bcd9712fe54cceb6a2b2bef65
Ben North <ben.north@tcd.ie>
2025-06-26 11:48:33 +0100

    useDeactivateChangeAction(): Define hook

--- src/components/hooks/notable-changes.ts
+++ src/components/hooks/notable-changes.ts
@@ -7,7 +7,7 @@ import {
   eqKeyedNotableChangeArrays,
   eqNotableChangeArrays,
 } from "../../model/notable-changes";
-import { useStoreState } from "../../store";
+import { useStoreActions, useStoreState } from "../../store";
 
 export function useNotableChanges<KindT extends NotableChangeKind>(
   kind: KindT,
@@ -30,4 +30,8 @@ export function useActiveNotableChanges(): Array<KeyedNotableChange> {
       ),
     eqKeyedNotableChangeArrays
   );
+}
+
+export function useDeactivateChangeAction() {
+  return useStoreActions((actions) => actions.activeProject._deactivateChange);
 }
