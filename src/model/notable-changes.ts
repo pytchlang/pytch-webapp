84a0c7e5ff3473fa75f206b638be8e78e7d67e86
Ben North <ben.north@tcd.ie>
2025-06-26 12:10:41 +0100

    NotableChange: Add PerMethodSpriteChanged disjunct
    And handle in eqNotableChange().

--- src/model/notable-changes.ts
+++ src/model/notable-changes.ts
@@ -2,12 +2,17 @@ import { arraysEqFun, assertNever } from "../utils";
 import {
   PerMethodScriptDeleted,
   PerMethodScriptUpserted,
+  PerMethodSpriteChanged,
   eqPerMethodScriptDeleted,
   eqPerMethodScriptUpserted,
+  eqPerMethodSpriteChanged,
 } from "./junior/change-events";
 import { ActorOps, EventDescriptorKindOps } from "./junior/structured-program";
 
-export type NotableChange = PerMethodScriptUpserted | PerMethodScriptDeleted;
+export type NotableChange =
+  | PerMethodScriptUpserted
+  | PerMethodScriptDeleted
+  | PerMethodSpriteChanged;
 
 export type NotableChangeKind = NotableChange["kind"];
 
@@ -21,6 +26,8 @@ export function eqNotableChange(x: NotableChange, y: NotableChange): boolean {
       return eqPerMethodScriptUpserted(x, y as PerMethodScriptUpserted);
     case "script-deleted":
       return eqPerMethodScriptDeleted(x, y as PerMethodScriptDeleted);
+    case "sprite-changed":
+      return eqPerMethodSpriteChanged(x, y as PerMethodSpriteChanged);
     default:
       return assertNever(x);
   }
