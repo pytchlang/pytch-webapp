a12062a171cd44d7bd6c99afcc7e332daa2b57d9
Ben North <ben.north@tcd.ie>
2025-07-03 16:17:51 +0100

    ActorsList: Simplify via FocusGroupContainer

--- src/components/Junior/ActorsList.tsx
+++ src/components/Junior/ActorsList.tsx
@@ -20,11 +20,9 @@ import { Dropdown } from "react-bootstrap";
 import { ActorPropertiesTabKey } from "../../model/junior/edit-state";
 import { SingleTab } from "../SingleTab";
 import { CaptiveContextMenu } from "../CaptiveContextMenu";
-import {
-  kFocusGroupItemClassName,
-  focusGroupContainerClass,
-} from "../../model/junior/grouped-focus";
+import { kFocusGroupItemClassName } from "../../model/junior/grouped-focus";
 import { useFocusContext } from "../hooks/focus-steering";
+import { FocusGroupContainer } from "../FocusGroupContainer";
 
 type ActorThumbnailProps = { id: Uuid };
 const ActorThumbnail: React.FC<ActorThumbnailProps> = ({ id }) => {
@@ -220,10 +218,9 @@ export const ActorsList = () => {
     >
       <SingleTab title="Stage and sprites">
         <div className="abs-0000">
-          <div
-            ref={focusContext.groupContainerRefCallback()}
-            className={focusGroupContainerClass("gfs__actors__container")}
-            data-grouped-focus-key="ActorsList"
+          <FocusGroupContainer
+            className="gfs__actors__container"
+            groupedFocusKey="ActorsList"
           >
             <ol className="ActorsList">
               {actorNubs.map((a) => (
@@ -242,7 +239,7 @@ export const ActorsList = () => {
               label="Add sprite"
               onClick={launchAddSpriteModal}
             />
-          </div>
+          </FocusGroupContainer>
         </div>
       </SingleTab>
     </section>
