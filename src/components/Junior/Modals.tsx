f6dd1130bfc9a29fa93de76dca129c6f3c2f72f4
Ben North <ben.north@tcd.ie>
2025-07-07 17:52:51 +0100

    Junior/Modals: Include AddClipArtModal

--- src/components/Junior/Modals.tsx
+++ src/components/Junior/Modals.tsx
@@ -3,10 +3,12 @@ import { UpsertSpriteModal } from "./async-flow-modals/UpsertSpriteModal";
 import { UpsertHandlerModal } from "./async-flow-modals/UpsertHandlerModal";
 import { DeleteSpriteModal } from "./async-flow-modals/DeleteSpriteModal";
 import { DeleteHandlerModal } from "./async-flow-modals/DeleteHandlerModal";
+import { AddClipArtModal } from "../async-flow-modals/AddClipArtModal";
 
 export const Modals = () => {
   return (
     <>
+      <AddClipArtModal />
       <UpsertSpriteModal />
       <UpsertHandlerModal />
       <DeleteSpriteModal />
