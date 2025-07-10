7302d4fdeded7a7bdb4025fd5495b30812ac2b8f
Ben North <ben.north@tcd.ie>
2025-07-10 22:53:08 +0100

    asyncFlowModal(): Update re new fsm design

--- src/components/async-flow-modals/utils.tsx
+++ src/components/async-flow-modals/utils.tsx
@@ -2,6 +2,7 @@ import {
   ActiveAsyncUserFlowFsmState,
   AsyncUserFlowFsmState,
 } from "../../model/user-interactions/async-user-flow";
+import { GenericErrorModal } from "./GenericErrorModal";
 import { GenericWorkingModal } from "./GenericWorkingModal";
 
 export function asyncFlowModal<RunStateT, AttemptOutcomeNubT>(
@@ -14,6 +15,13 @@ export function asyncFlowModal<RunStateT, AttemptOutcomeNubT>(
     return null;
   } else if (fsmState.kind === "preparing") {
     return <GenericWorkingModal />;
+  } else if (fsmState.kind === "awaiting-ack-of-error") {
+    return (
+      <GenericErrorModal
+        message={fsmState.errorMessage}
+        onAck={fsmState.userAck}
+      />
+    );
   } else {
     return contentFun(fsmState);
   }
