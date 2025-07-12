fe8f6e84b87d7d1ce755b2e2f812c0be7d610797
Ben North <ben.north@tcd.ie>
2025-07-12 22:07:55 +0100

    (Tidy up imports)

--- src/components/async-flow-modals/UploadZipfilesModal.tsx
+++ src/components/async-flow-modals/UploadZipfilesModal.tsx
@@ -3,7 +3,6 @@ import { ChooseFiles } from "../ChooseFiles";
 import { FileProcessingFailures } from "../FileProcessingFailures";
 import { asyncFlowModal } from "./utils";
 import { settleFunctions } from "../../model/user-interactions/async-user-flow";
-import { GenericWorkingModal } from "./GenericWorkingModal";
 import { useFlowActions, useFlowState } from "../../model";
 import { assertNever } from "../../utils";
 
