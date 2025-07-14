1edf8b65b40216c2b0fdb1cda9159b97b345b260
Ben North <ben.north@tcd.ie>
2025-07-14 08:06:29 +0100

    <s>CropScaleImageFlow: Pulse toast on success
    And do not do so in the updateAssetTransformAndSync() thunk.

--- src/model/project.ts
+++ src/model/project.ts
@@ -1089,13 +1089,6 @@ export const activeProject: IActiveProject = {
     await actions.syncAssetsFromStorage();
 
     helpers.getStoreActions().projectCollection.noteDatabaseChange();
-    const assetAffixes = PytchProgramOps.assetPathAffixes(descriptor.assetName);
-    actions.pulseNotableChange({
-      kind: "asset-changed",
-      assetChangedKind: "update-transform",
-      operationContext: descriptor.operationContext,
-      assetDisplayName: assetAffixes.suffix,
-    });
   }),
 
   requestSyncToStorage: thunk(async (actions, _payload, helpers) => {
