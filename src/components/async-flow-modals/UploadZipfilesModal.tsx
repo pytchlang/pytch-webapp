454c8daf62027244e993f8068401ac0637664ea4
Ben North <ben.north@tcd.ie>
2025-07-14 17:03:01 +0100

    UploadZipfileModal: Show failures

--- src/components/async-flow-modals/UploadZipfilesModal.tsx
+++ src/components/async-flow-modals/UploadZipfilesModal.tsx
@@ -5,6 +5,7 @@ import { asyncFlowModal } from "./utils";
 import { settleFunctions } from "../../model/user-interactions/async-user-flow";
 import { useFlowActions, useFlowState } from "../../model";
 import { assertNever } from "../../utils";
+import { FileProcessingFailure } from "../../model/user-interactions/process-files";
 
 export const UploadZipfilesModal = () => {
   const { fsmState, isSubmittable } = useFlowState((f) => f.uploadZipfilesFlow);
@@ -12,13 +13,16 @@ export const UploadZipfilesModal = () => {
 
   return asyncFlowModal(fsmState, (activeFsmState) => {
     if (activeFsmState.kind === "awaiting-ack-of-notification") {
-      // TODO: Compute proper value when available.
-      const error = { fileFailures: [] };
+      const fileFailures: Array<FileProcessingFailure> =
+        activeFsmState.outcomeNub.failures.map((failure) => ({
+          filename: failure.filename,
+          reason: failure.reason,
+        }));
       return (
         <FileProcessingFailures
           titleText="Problem uploading project zipfiles"
           introText="Sorry, there was a problem uploading the projects:"
-          failures={error.fileFailures}
+          failures={fileFailures}
           dismiss={activeFsmState.userAck}
         />
       );
