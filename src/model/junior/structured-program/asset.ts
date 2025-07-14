6c5972f019d5160933c3ff4dafad83cf42ff2443
Ben North <ben.north@tcd.ie>
2025-07-14 10:38:09 +0100

    AssetMetaDataOps.assetSourceDisplayName(): Define

--- src/model/junior/structured-program/asset.ts
+++ src/model/junior/structured-program/asset.ts
@@ -1,3 +1,4 @@
+import { assertNever } from "../../../utils";
 import { Uuid } from "./core-types";
 
 // Compatible with AssetPresentation
@@ -197,5 +198,16 @@ export class AssetMetaDataOps {
         belongsToTargetActor(asset) &&
         asset.assetInProject.mimeType.startsWith(mimeTypePrefix)
     );
+  }
+
+  static assetSourceDisplayName(sourceKind: AssetSourceKind): string {
+    switch (sourceKind) {
+      case "this-device":
+        return "this device";
+      case "media-library":
+        return "the media library";
+      default:
+        return assertNever(sourceKind);
+    }
   }
 }
