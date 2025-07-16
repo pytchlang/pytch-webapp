0911f49a204452a1936f747fb94dedb39f2ba306
Ben North <ben.north@tcd.ie>
2025-07-16 08:59:58 +0100

    <s>itShowsToastFor(): Define e2e test helper

---
+++ cypress/e2e/notable-change-toasts.cy.ts
@@ -0,0 +1,31 @@
+import { GatedDelay } from "./utils";
+
+////////////////////////////////////////////////////////////////////////
+
+type ItShowsToastForDescriptor = {
+  only?: boolean;
+  setup: () => void;
+  submit: () => void;
+  toastBodyMatch: string | RegExp;
+};
+
+function itShowsToastFor(label: string, descr: ItShowsToastForDescriptor) {
+  const createTest = descr.only ?? false ? it.only : it;
+
+  createTest(label, () => {
+    descr.setup();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    cy.window().then(async (window: any) => {
+      const gatedDelay = GatedDelay.installNew(window);
+      descr.submit();
+      cy.get(".toast-body")
+        .should("have.length", 1)
+        .contains(descr.toastBodyMatch)
+        .then(() => {
+          gatedDelay.release();
+          cy.get(".toast-body").should("not.exist");
+        });
+    });
+  });
+}
