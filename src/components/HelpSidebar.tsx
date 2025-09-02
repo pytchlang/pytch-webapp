f2bc98701cccd15cc84fd82ac3474cd1aa8b95ef
Ben North <ben.north@tcd.ie>
2025-09-02 17:30:09 +0100

    PurePythonElement: Render rich Python

--- src/components/HelpSidebar.tsx
+++ src/components/HelpSidebar.tsx
@@ -276,12 +276,12 @@ const PurePythonElement: React.FC<
   PurePythonElementDescriptor & { displayContext: HelpDisplayContext }
 > = (props) => {
   const helpElements = helpElementsFromProps(props);
-  const pythonCode = pythonCodeFromProps(props);
+  const richPython = richPythonFromProps(props);
 
   return (
     <details className="pytch-method">
       <HelpNodeSummary>
-        <PythonAndButtons python={pythonCode} />
+        <PythonAndButtons richPython={richPython} />
         <AccordionTextSignifier />
       </HelpNodeSummary>
       <HelpText help={helpElements} />
