2f9a00d90548514db578387e45c937bd693450b1
Ben North <ben.north@tcd.ie>
2025-09-03 10:24:41 +0100

    Define constants for shift-F10/-Tab

--- cypress/e2e/keyboard-navigation/utils.ts
+++ cypress/e2e/keyboard-navigation/utils.ts
@@ -1,5 +1,8 @@
 export type KeyOrShortcut = Parameters<typeof cy.realPress>[0];
 
+export const kShiftTab: KeyOrShortcut = ["Shift", "Tab"];
+export const kShiftF10: KeyOrShortcut = ["Shift", "F10"];
+
 export function realPress(keyOrShortcut: KeyOrShortcut, nTimes?: number) {
   nTimes ??= 1;
   for (let _i = 0; _i !== nTimes; ++_i) {
