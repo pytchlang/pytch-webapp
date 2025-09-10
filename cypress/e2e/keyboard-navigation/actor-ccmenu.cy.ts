8fd8c36e859a954574767321137764626a4e9ba4
Ben North <ben.north@tcd.ie>
2025-09-10 16:38:20 +0100

    Define helper constants

--- cypress/e2e/keyboard-navigation/actor-ccmenu.cy.ts
+++ cypress/e2e/keyboard-navigation/actor-ccmenu.cy.ts
@@ -1,5 +1,19 @@
+import {
+  selectSprite,
+  selectStage,
+} from "../junior/utils";
+
 context("Actor ccmenu operations", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
   });
+
+  const actorKindsAndSelectors = {
+    stage: { actorKind: "stage", select: selectStage },
+    sprite: { actorKind: "sprite", select: () => selectSprite("Snake") },
+  };
+
+  // Stage dropdown should have "rename" and "delete" disabled.
+  const expStageEnabled = [true, true, true, false, false];
+  const expSpriteEnabled = [true, true, true, true, true];
 });
