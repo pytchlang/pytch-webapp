50435f0c3bd28780273db0ae0aecd584255352f7
Ben North <ben.north@tcd.ie>
2025-09-10 21:30:49 +0100

    Add "copy name" test

--- cypress/e2e/keyboard-navigation/asset-ccmenu.cy.ts
+++ cypress/e2e/keyboard-navigation/asset-ccmenu.cy.ts
@@ -1,13 +1,25 @@
 import {
+  focusActorAssetViaMouse,
   loadFromZipfile,
   selectActorAspect,
   selectSprite,
 } from "../junior/utils";
+import {
+  assertCopiedText,
+} from "../utils";
+import { assertFocus, chooseCcMenuItem } from "./utils";
 
 context("Asset-card ccmenu", () => {
   beforeEach(() => {
     loadFromZipfile("eight-grey-costumes.zip");
     selectSprite("GreyThing");
     selectActorAspect("Costumes");
+  });
+
+  it("copy name", () => {
+    focusActorAssetViaMouse(3);
+    chooseCcMenuItem(0);
+    assertCopiedText("'solid-003.png'");
+    assertFocus("appearance-card", 3);
   });
 });
