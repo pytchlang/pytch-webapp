ff7b66e15f63be548a51ccea8f4556bf59e9c0c4
Ben North <ben.north@tcd.ie>
2025-09-10 19:11:03 +0100

    Add "remember which script is active" test

--- cypress/e2e/keyboard-navigation/actor-properties.cy.ts
+++ cypress/e2e/keyboard-navigation/actor-properties.cy.ts
@@ -1,8 +1,38 @@
 import { loadFromZipfile, selectSprite } from "../junior/utils";
+import { assertFocus, chooseCcMenuItem, kShiftTab, realPress } from "./utils";
 
 context("Navigating actor properties", () => {
   beforeEach(() => {
     loadFromZipfile("per-method-four-scripts.zip");
     selectSprite("Snake");
+  });
+
+  it("remember which script is active", () => {
+    chooseCcMenuItem(0); // "Go to code"
+    assertFocus("script", 0);
+    realPress("ArrowDown", 2);
+    assertFocus("script", 2);
+
+    selectSprite("Snake");
+    chooseCcMenuItem(1); // "Go to costumes"
+    assertFocus("appearance-card", 0);
+
+    selectSprite("Snake");
+    chooseCcMenuItem(0); // "Go to code"
+    assertFocus("script", 2);
+
+    selectSprite("Snake");
+    chooseCcMenuItem(2); // "Go to sounds"
+    assertFocus("add-sound-button");
+
+    selectSprite("Snake");
+    chooseCcMenuItem(0); // "Go to code"
+    assertFocus("script", 2);
+
+    realPress(kShiftTab, 2);
+    assertFocus("help-sidebar", [0]);
+
+    realPress("Tab", 2);
+    assertFocus("script", 2);
   });
 });
