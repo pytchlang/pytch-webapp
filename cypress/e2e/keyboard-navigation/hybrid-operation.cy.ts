88626540f41c16c7ca128bb2442cf84f52fd44db
Ben North <ben.north@tcd.ie>
2025-09-10 16:34:47 +0100

    Add "summon with kbd, operate with mouse" test

--- cypress/e2e/keyboard-navigation/hybrid-operation.cy.ts
+++ cypress/e2e/keyboard-navigation/hybrid-operation.cy.ts
@@ -1,5 +1,20 @@
+import { selectSprite, settleModalDialog } from "../junior/utils";
+import { assertFocus, summonCcMenuByKbd } from "./utils";
+
 context("Hybrid kbd/mouse operation", () => {
   beforeEach(() => {
     cy.pytchBasicJrProject();
+  });
+
+  it("summon with kbd, operate with mouse", () => {
+    selectSprite("Snake");
+    summonCcMenuByKbd();
+
+    cy.get(".ActorCard .show.dropdown a.dropdown-item")
+      .contains("DELETE")
+      .click();
+
+    settleModalDialog("Cancel");
+    assertFocus("actor-card", 1);
   });
 });
