0994a35e9d30ec40e144046c91fe83f2f0d57407
Ben North <ben.north@tcd.ie>
2025-09-10 16:35:43 +0100

    Add "summon kbd, other op with mouse" test

--- cypress/e2e/keyboard-navigation/hybrid-operation.cy.ts
+++ cypress/e2e/keyboard-navigation/hybrid-operation.cy.ts
@@ -1,5 +1,5 @@
-import { selectSprite, settleModalDialog } from "../junior/utils";
-import { assertFocus, summonCcMenuByKbd } from "./utils";
+import { launchAdd, selectSprite, settleModalDialog } from "../junior/utils";
+import { assertFocus, realPress, summonCcMenuByKbd } from "./utils";
 
 context("Hybrid kbd/mouse operation", () => {
   beforeEach(() => {
@@ -16,5 +16,19 @@ context("Hybrid kbd/mouse operation", () => {
 
     settleModalDialog("Cancel");
     assertFocus("actor-card", 1);
+  });
+
+  it("summon with kbd, launch different operation with mouse", () => {
+    selectSprite("Snake");
+    summonCcMenuByKbd();
+
+    cy.get(".ActorCard a.dropdown-item").should("have.length", 5);
+    realPress("ArrowDown", 2);
+    assertFocus("actor-card-menu-item", [1, 2]);
+
+    launchAdd.script();
+    cy.get(".show.dropdown").should("not.exist");
+    settleModalDialog("Cancel");
+    assertFocus("script", 0);
   });
 });
