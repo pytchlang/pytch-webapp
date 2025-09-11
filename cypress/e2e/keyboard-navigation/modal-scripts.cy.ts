f7c1b5d8bc65471e3efeab2ce449bf5f85d95ab6
Ben North <ben.north@tcd.ie>
2025-09-11 09:19:30 +0100

    Add "navigate with kbd and mouse" test

--- cypress/e2e/keyboard-navigation/modal-scripts.cy.ts
+++ cypress/e2e/keyboard-navigation/modal-scripts.cy.ts
@@ -1,9 +1,11 @@
 import {
+  eventHandlerCodeShouldEqual,
+  focusScriptViaMouse,
   loadFromZipfile,
   selectActorAspect,
   selectSprite,
 } from "../junior/utils";
-import { assertFocus } from "./utils";
+import { assertFocus, kShiftTab, realPress } from "./utils";
 
 context("Working with scripts", () => {
   beforeEach(() => {
@@ -11,5 +13,48 @@ context("Working with scripts", () => {
     selectSprite("Snake");
     selectActorAspect("Code", "tab");
     assertFocus("actor-property-tab", "code");
+  });
+
+  it("navigate with kbd and mouse", () => {
+    realPress("Tab");
+    assertFocus("script", 0);
+
+    realPress("ArrowDown");
+    assertFocus("script", 1);
+
+    realPress("Enter");
+    assertFocus("script-code", 1);
+
+    cy.realType("# hello");
+    eventHandlerCodeShouldEqual(1, "# hello");
+
+    realPress("Escape");
+    assertFocus("script", 1);
+
+    realPress(kShiftTab);
+    assertFocus("actor-property-tab", "code");
+
+    realPress("Tab");
+    assertFocus("script", 1);
+
+    realPress("Tab");
+    assertFocus("add-script-button");
+
+    realPress(kShiftTab);
+    assertFocus("script", 1);
+
+    realPress("End");
+    assertFocus("script", 3);
+
+    focusScriptViaMouse(1);
+    assertFocus("script-code", 1);
+    realPress("Escape");
+    assertFocus("script", 1);
+
+    realPress("Home");
+    assertFocus("script", 0);
+
+    focusScriptViaMouse(2);
+    assertFocus("script-code", 2);
   });
 });
