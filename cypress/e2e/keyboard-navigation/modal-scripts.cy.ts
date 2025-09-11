a8608962348cd9ea6c2896d75d5f5e1bcc328489
Ben North <ben.north@tcd.ie>
2025-09-11 09:24:45 +0100

    Add "can perform ccmenu actions" test

--- cypress/e2e/keyboard-navigation/modal-scripts.cy.ts
+++ cypress/e2e/keyboard-navigation/modal-scripts.cy.ts
@@ -7,7 +7,7 @@ import {
   selectActorAspect,
   selectSprite,
 } from "../junior/utils";
-import { assertFocus, kShiftTab, realPress } from "./utils";
+import { assertFocus, chooseCcMenuItem, kShiftTab, realPress } from "./utils";
 
 context("Working with scripts", () => {
   beforeEach(() => {
@@ -64,4 +64,51 @@ context("Working with scripts", () => {
     assertHatBlockLabels(
       idxs.map((i) => ScriptOps.allExtendedHandlerLabels[i])
     );
+
+  it("can perform ccmenu actions", () => {
+    realPress("Tab");
+    realPress("ArrowDown", 2);
+    assertFocus("script", 2);
+
+    chooseCcMenuItem(3); /* Duplicate script */
+    assertFocus("script-code", 3);
+    assertPluckedLabels([0, 1, 2, 2, 3]);
+
+    realPress("Escape");
+    assertFocus("script", 3);
+
+    chooseCcMenuItem("End"); /* DELETE */
+    realPress("Tab"); /* OK button */
+    realPress("Space");
+    assertFocus("script", 3);
+    assertPluckedLabels([0, 1, 2, 3]);
+
+    realPress("ArrowUp");
+    chooseCcMenuItem("End"); /* DELETE */
+    realPress("Tab"); /* OK button */
+    realPress("Space");
+    assertFocus("script", 2);
+    assertPluckedLabels([0, 1, 3]);
+
+    chooseCcMenuItem(1); /* Move script up */
+    assertFocus("script", 1);
+    assertPluckedLabels([0, 3, 1]);
+
+    chooseCcMenuItem(1); /* Move script up */
+    assertFocus("script", 0);
+    assertPluckedLabels([3, 0, 1]);
+
+    realPress("ArrowDown");
+    chooseCcMenuItem(2); /* Move script down */
+    assertFocus("script", 2);
+    assertPluckedLabels([3, 1, 0]);
+
+    realPress("Home");
+    assertFocus("script", 0);
+    chooseCcMenuItem(0); /* Change hat block */
+    realPress("ArrowDown", 1); /* clicked -> start as clone */
+    realPress("Enter");
+    assertFocus("script", 0);
+    assertPluckedLabels([2, 1, 0]); /* fudge: re-use starting label [2] */
+  });
 });
