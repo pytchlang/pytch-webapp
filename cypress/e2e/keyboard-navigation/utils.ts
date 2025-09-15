44dc8898b8104eb8c334758b4c49fad5f9f4a7a3
Ben North <ben.north@tcd.ie>
2025-09-15 16:18:34 +0100

    activateHatBlockOption(): Define helper

--- cypress/e2e/keyboard-navigation/utils.ts
+++ cypress/e2e/keyboard-navigation/utils.ts
@@ -32,6 +32,12 @@ export function activateActivityViaTab(tab: ActivityBarTabKey) {
 
 ////////////////////////////////////////////////////////////////////////
 
+export function activateHatBlockOption(optionIdx: number): void {
+  cy.get(".EventKindOption").eq(optionIdx).click();
+}
+
+////////////////////////////////////////////////////////////////////////
+
 function helpEntrySelector(sectionIdx: number, entryIdx: number) {
   const sectionIdx1b = sectionIdx + 1;
   const entryIdx1b = entryIdx + 2; // Skip first child (<summary> elt)
