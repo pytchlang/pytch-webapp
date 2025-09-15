3d5f89788afa23b899f8e3257b21828c4854d4a3
Ben North <ben.north@tcd.ie>
2025-09-15 10:29:28 +0100

    Add "no tab-trap in code editor" test

--- cypress/e2e/keyboard-navigation/flat-ide.cy.ts
+++ cypress/e2e/keyboard-navigation/flat-ide.cy.ts
@@ -1,6 +1,18 @@
+import { assertFocus, kShiftTab, realPress } from "./utils";
+
 context("Flat code editing", () => {
   beforeEach(() => {
     cy.pytchResetDatabase();
     cy.pytchTryUploadZipfiles(["print-things.zip"]);
+  });
+
+  it("no tab-trap in code editor", () => {
+    cy.get("#pytch-ace-editor").click();
+    cy.realType("\n# 236-6132");
+    cy.pytchCodeTextShouldContain("236-6132");
+    realPress("Escape");
+    assertFocus("flat-code-tab");
+    realPress(kShiftTab);
+    assertFocus("help-sidebar", [0]);
   });
 });
