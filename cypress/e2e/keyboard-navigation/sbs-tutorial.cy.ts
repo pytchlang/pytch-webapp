3a71a49ce4cf78be22dedf94bf1a6610c56c4a80
Ben North <ben.north@tcd.ie>
2025-09-16 12:46:42 +0100

    Add "reveal hint and navigate solution" test

--- cypress/e2e/keyboard-navigation/sbs-tutorial.cy.ts
+++ cypress/e2e/keyboard-navigation/sbs-tutorial.cy.ts
@@ -1,9 +1,11 @@
 import { jumpToTutorialChapter } from "../utils";
 import {
   assertFocus,
+  assertLearnerTaskDiffShown,
   assertLearnerTaskDoneState,
   assertLearnerTaskHelpNStages,
   assertLearnerTaskHelpState,
+  kShiftTab,
   realPress,
 } from "./utils";
 
@@ -27,5 +29,47 @@ context("Sbs tutorial", () => {
     assertLearnerTaskHelpState(0, "hide");
     realPress("Enter");
     assertLearnerTaskHelpNStages(0, 0);
+  });
+
+  it("reveal hint and navigate solution", () => {
+    // A bit tedious to advance through the tutorial to a point where
+    // there is a hint.
+    realPress("Tab");
+    realPress("Enter");
+    assertLearnerTaskDoneState(0, "click-to-rewind");
+    realPress("Tab", 2);
+    realPress("Enter");
+    realPress("Tab", 2);
+    realPress("Enter");
+    realPress("Tab", 2);
+    realPress("Enter");
+    realPress("Tab", 3);
+
+    assertFocus("learner-task-help-button", 4);
+    assertLearnerTaskHelpNStages(4, 0);
+    assertLearnerTaskHelpState(4, "hint");
+    realPress("Enter");
+    assertLearnerTaskHelpNStages(4, 1);
+    assertLearnerTaskHelpState(4, "show");
+    realPress("Enter");
+    assertLearnerTaskHelpNStages(4, 2);
+    assertLearnerTaskHelpState(4, "hide");
+
+    realPress(kShiftTab, 2);
+    assertFocus("learner-task-diff-tab", [4, 0]);
+    assertLearnerTaskDiffShown(4, "bare-old");
+
+    realPress("ArrowRight");
+    assertFocus("learner-task-diff-tab", [4, 1]);
+    assertLearnerTaskDiffShown(4, "old-diff");
+
+    realPress("ArrowRight");
+    assertFocus("learner-task-diff-tab", [4, 2]);
+    assertLearnerTaskDiffShown(4, "new-diff");
+
+    realPress("Tab", 2);
+    realPress("Enter");
+    assertLearnerTaskHelpNStages(4, 0);
+    assertLearnerTaskHelpState(4, "hint");
   });
 });
