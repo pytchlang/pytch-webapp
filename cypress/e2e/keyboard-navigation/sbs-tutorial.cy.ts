d82553dc862f13fc7bc3bb3353b9e18797926ce0
Ben North <ben.north@tcd.ie>
2025-09-16 12:46:23 +0100

    Add "show solution" test

--- cypress/e2e/keyboard-navigation/sbs-tutorial.cy.ts
+++ cypress/e2e/keyboard-navigation/sbs-tutorial.cy.ts
@@ -1,6 +1,10 @@
 import { jumpToTutorialChapter } from "../utils";
 import {
   assertFocus,
+  assertLearnerTaskDoneState,
+  assertLearnerTaskHelpNStages,
+  assertLearnerTaskHelpState,
+  realPress,
 } from "./utils";
 
 context("Sbs tutorial", () => {
@@ -8,5 +12,20 @@ context("Sbs tutorial", () => {
     cy.pytchJrLesson();
     jumpToTutorialChapter(1);
     assertFocus("tutorial-content");
+  });
+
+  it("show solution", () => {
+    realPress("Tab");
+    assertFocus("learner-task-done-button", 0);
+    assertLearnerTaskDoneState(0, "click-when-done");
+    realPress("Tab");
+    assertFocus("learner-task-help-button", 0);
+    assertLearnerTaskHelpState(0, "show");
+    realPress("Enter");
+    assertFocus("learner-task-help-button", 0);
+    assertLearnerTaskHelpNStages(0, 1);
+    assertLearnerTaskHelpState(0, "hide");
+    realPress("Enter");
+    assertLearnerTaskHelpNStages(0, 0);
   });
 });
