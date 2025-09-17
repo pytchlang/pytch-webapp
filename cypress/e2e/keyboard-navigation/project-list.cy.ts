f6c1e827713ffcd0fbee52d0cfd4105f74ecc4d2
Ben North <ben.north@tcd.ie>
2025-09-17 10:34:55 +0100

    Add "navigate list" test

--- cypress/e2e/keyboard-navigation/project-list.cy.ts
+++ cypress/e2e/keyboard-navigation/project-list.cy.ts
@@ -1,3 +1,8 @@
+import {
+  focusProjectCardViaMouse,
+} from "../utils";
+import { assertFocus, kShiftTab, realPress } from "./utils";
+
 context("My projects list", () => {
   beforeEach(() => {
     cy.pytchResetDatabase();
@@ -5,5 +10,28 @@ context("My projects list", () => {
       "per-method-four-scripts.zip",
       "eight-grey-costumes.zip",
     ]);
+  });
+
+  it("navigate list", () => {
+    focusProjectCardViaMouse(1);
+    assertFocus("project-card", 1);
+    realPress(kShiftTab);
+    realPress("Tab");
+    assertFocus("project-card", 1);
+
+    realPress("ArrowDown");
+    assertFocus("project-card", 2);
+
+    focusProjectCardViaMouse(0, "select-toggle");
+    assertFocus("project-card", 0);
+
+    realPress("End");
+    assertFocus("project-card", 2);
+    realPress(kShiftTab);
+    realPress("Tab");
+    assertFocus("project-card", 2);
+
+    realPress("Home");
+    assertFocus("project-card", 0);
   });
 });
