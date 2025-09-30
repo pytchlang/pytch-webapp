a390bf1aca680ae38ff0501a1277dccb0226e45d
Ben North <ben.north@tcd.ie>
2025-09-30 12:27:25 +0100

    (Reformat)

--- src/components/Junior/PytchScriptEditor.tsx
+++ src/components/Junior/PytchScriptEditor.tsx
@@ -146,7 +146,10 @@ export const PytchScriptEditor: React.FC<PytchScriptEditorProps> = ({
       onClick={focusContext.onGroupItemClick}
       onActivate={focusTextArea}
     >
-      <div className={classes} data-handler-id={handlerId}>
+      <div
+        className={classes}
+        data-handler-id={handlerId}
+      >
         <DragPreviewImage connect={preview} src={PytchScriptPreview} />
         <div ref={dropRef}>
           <div ref={dragRef}>
