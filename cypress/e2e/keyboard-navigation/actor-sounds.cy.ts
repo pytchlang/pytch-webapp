f868d87428fba81bbef366d3bebc892c3cf54451
Ben North <ben.north@tcd.ie>
2025-10-02 11:10:11 +0100

    assertPluckedSoundNames(): Define local helper

--- cypress/e2e/keyboard-navigation/actor-sounds.cy.ts
+++ cypress/e2e/keyboard-navigation/actor-sounds.cy.ts
@@ -1,2 +1,18 @@
+import {
+  assertSoundNames,
+} from "../junior/utils";
+
 context("Alt-arrow reordering of sounds", () => {
+  const allSoundNames = [
+    "note-1.mp3",
+    "note-2.mp3",
+    "note-3.mp3",
+    "note-4.mp3",
+  ];
+
+  const assertPluckedSoundNames = (idxs: Array<number>) =>
+    assertSoundNames(
+      "sprite",
+      idxs.map((i) => allSoundNames[i])
+    );
 });
