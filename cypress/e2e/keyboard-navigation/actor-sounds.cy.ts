95eb71fed55967f381da02d93f82e00ef5fc0031
Ben North <ben.north@tcd.ie>
2025-10-02 11:10:30 +0100

    Add "reorder sounds" test

--- cypress/e2e/keyboard-navigation/actor-sounds.cy.ts
+++ cypress/e2e/keyboard-navigation/actor-sounds.cy.ts
@@ -1,6 +1,17 @@
 import {
   assertSoundNames,
+  focusActorAssetViaMouse,
+  loadFromZipfile,
+  selectActorAspect,
+  selectSprite,
 } from "../junior/utils";
+import {
+  assertFocus,
+  kAltArrowDown,
+  kAltArrowLeft,
+  kAltArrowRight,
+  realPress,
+} from "./utils";
 
 context("Alt-arrow reordering of sounds", () => {
   const allSoundNames = [
@@ -15,4 +26,31 @@ context("Alt-arrow reordering of sounds", () => {
       "sprite",
       idxs.map((i) => allSoundNames[i])
     );
+
+  it("reorder sounds", () => {
+    loadFromZipfile("four-sounds.zip");
+    selectSprite("Button");
+    selectActorAspect("Sounds");
+
+    focusActorAssetViaMouse(0);
+    assertFocus("sound-card", 0);
+    assertPluckedSoundNames([0, 1, 2, 3]);
+
+    realPress(kAltArrowDown);
+    assertFocus("sound-card", 1);
+    assertPluckedSoundNames([1, 0, 2, 3]);
+
+    realPress("ArrowDown");
+    realPress(kAltArrowRight);
+    assertFocus("sound-card", 3);
+    assertPluckedSoundNames([1, 0, 3, 2]);
+
+    realPress(kAltArrowDown);
+    assertFocus("sound-card", 3);
+    assertPluckedSoundNames([1, 0, 3, 2]);
+
+    realPress(kAltArrowLeft);
+    assertFocus("sound-card", 2);
+    assertPluckedSoundNames([1, 0, 2, 3]);
+  });
 });
