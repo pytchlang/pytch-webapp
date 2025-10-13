42642d99df2aafe7051c4ae7750ce4af0638ca6b
Ben North <ben.north@tcd.ie>
2025-10-13 11:38:51 +0100

    <s>Always hide ccmenu on (shift-)tab

--- src/components/CaptiveContextMenu.tsx
+++ src/components/CaptiveContextMenu.tsx
@@ -167,17 +167,7 @@ const Container: React.FC<PropsWithChildren<ContainerProps>> = ({
       const containerDiv = divRef.current;
       if (containerDiv != null) {
         if (evt.key === "Tab") {
-          const activeElt = document.activeElement;
-          if (activeElt == null) return;
-          const allItems = Array.from(
-            containerDiv.querySelectorAll(itemSelector)
-          );
-          const lastItemIdx = allItems.length - 1;
-          const oldActiveIdx = allItems.indexOf(activeElt);
-          const movingOut = oldActiveIdx === (evt.shiftKey ? 0 : lastItemIdx);
-          if (movingOut) {
-            setShow(false);
-          }
+          setShow(false);
         }
 
         handleMovementKeys(containerDiv, itemSelector, evt);
