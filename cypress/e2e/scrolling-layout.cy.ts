78bf55939a5403c0317c7cdc31bad4c627f11267
Ben North <ben.north@tcd.ie>
2025-10-14 14:14:09 +0100

    Add "error pane" scrolling test

--- cypress/e2e/scrolling-layout.cy.ts
+++ cypress/e2e/scrolling-layout.cy.ts
@@ -1,3 +1,5 @@
+import { loadFromZipfile } from "./junior/utils";
+
 function eltShouldNotOverflow($elts: JQuery<HTMLElement>) {
   cy.wrap($elts.length).should("eq", 1);
   const elt = $elts[0];
@@ -18,4 +20,13 @@ context("Scrolling panes", () => {
   const assertRootSize = () => {
     cy.get("main").then(eltShouldNotOverflow);
   };
+
+  it("error pane", () => {
+    loadFromZipfile("multiple-errors-green-flag");
+    assertRootSize();
+    cy.pytchGreenFlag();
+    cy.get("ol.ErrorReportList > li").should("have.length", 4);
+    assertEltOverflows(".Junior-InfoPanel-container .tab-content");
+    assertRootSize();
+  });
 });
