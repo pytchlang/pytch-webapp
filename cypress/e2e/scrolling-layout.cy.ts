872afb4e1c777af180285ba69df6a1d011ab84f3
Ben North <ben.north@tcd.ie>
2025-10-14 14:13:08 +0100

    eltShouldNotOverflow(): Define local helper

--- cypress/e2e/scrolling-layout.cy.ts
+++ cypress/e2e/scrolling-layout.cy.ts
@@ -1,2 +1,8 @@
+function eltShouldNotOverflow($elts: JQuery<HTMLElement>) {
+  cy.wrap($elts.length).should("eq", 1);
+  const elt = $elts[0];
+  cy.wrap(elt.scrollHeight).should("be.lte", elt.clientHeight);
+}
+
 context("Scrolling panes", () => {
 });
