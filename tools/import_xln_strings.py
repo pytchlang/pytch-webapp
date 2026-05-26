eb5cbe6f1876b36725f556e46ba1fd6d2a470c39
Ben North <ben.north@tcd.ie>
2026-05-26 10:36:49 +0100

    Emit more human-friendly JSON

--- tools/import_xln_strings.py
+++ tools/import_xln_strings.py
@@ -16,7 +16,8 @@ class XlnsFromNamespace:
         for ns, xlns in self._xlns_from_ns.items():
             xlns_path = self.root / f"{ns}.json"
             with xlns_path.open("wt") as xlns_file:
-                json.dump(xlns, xlns_file, ensure_ascii=False)
+                json.dump(xlns, xlns_file, ensure_ascii=False, indent=2)
+                xlns_file.write("\n")
 
 
 json_root = Path(sys.argv[1])
