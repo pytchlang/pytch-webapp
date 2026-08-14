0cd1ac8c3c7ca7aa07cd06d715db4825e1686dc2
Ben North <ben.north@tcd.ie>
2026-08-14 11:56:45 +0100

    DemoChapter: Make tab-stop and global focus target

--- src/components/demo-sidebar/DemoChapter.tsx
+++ src/components/demo-sidebar/DemoChapter.tsx
@@ -235,7 +235,10 @@ export const DemoChapter = () => {
           ) : undefined}
         </Row>
         <Row className={"flex-grow-1 chapter-markdown-wrapper"}>
-          <Col className={"chapter-markdown px-4"}>
+          <Col
+            className={"chapter-markdown px-4 gfs__help-content"}
+            tabIndex={0}
+          >
             <DemoChapterBody key={bodyKey} markdown={chapters[activeChapter]} />
           </Col>
         </Row>
