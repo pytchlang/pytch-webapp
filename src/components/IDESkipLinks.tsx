946125a5bf5f99ac04fcf271415ef7ae0cb9d725
Ben North <ben.north@tcd.ie>
2026-08-14 12:01:43 +0100

    SkipLink: Define local component

--- src/components/IDESkipLinks.tsx
+++ src/components/IDESkipLinks.tsx
@@ -1,1 +1,27 @@
 import React from "react";
+import { SkipLinkFocusTarget } from "../model/junior/global-steer-focus";
+import { useTranslation } from "react-i18next";
+import { useFocusContext } from "./hooks/focus-steering";
+
+type SkipLinkProps = {
+  focusTarget: SkipLinkFocusTarget; // Re-used as component of i18n key
+};
+const SkipLink: React.FC<SkipLinkProps> = ({ focusTarget }) => {
+  const { t } = useTranslation("ide");
+  const focusContext = useFocusContext();
+
+  const key = `skip-link.${focusTarget}.label` as const;
+  const label = t(key);
+  return (
+    <button
+      type="button"
+      className="visually-hidden-focusable"
+      aria-label={label}
+      onClick={() => {
+        focusContext.focusGlobalFocusTarget(focusTarget);
+      }}
+    >
+      <span>{label}</span>
+    </button>
+  );
+};
