666fd597ea78f9b833fa0060ffb62819361b5527
Ben North <ben.north@tcd.ie>
2026-08-17 16:05:43 +0100

    MaybeContentProps: Define local type

--- src/components/RenderedExternalContent.tsx
+++ src/components/RenderedExternalContent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { State } from "easy-peasy";
 import { ContentFetchState } from "../model/external-json-data";
 import { IPytchAppModel } from "../model";
+import { FetchedResourceKind } from "../model/fetched-resource";
 
 type FetchStateMapper<ContentT> = (
   state: State<IPytchAppModel>
@@ -11,3 +12,9 @@ type ContentComponentProps<ContentT> = {
   content: ContentT;
 };
 type ContentComponent<ContentT> = React.FC<ContentComponentProps<ContentT>>;
+
+type MaybeContentProps<ContentT> = {
+  fetchStateMapper: FetchStateMapper<ContentT>;
+  contentComponent: ContentComponent<ContentT>;
+  resourceKeySuffix: FetchedResourceKind;
+};
