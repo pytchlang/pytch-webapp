cfa235ccefa25cf3781f1e00f20f2e5e29a63120
Ben North <ben.north@tcd.ie>
2026-08-17 16:04:04 +0100

    FetchStateMapper: Define local type

--- src/components/RenderedExternalContent.tsx
+++ src/components/RenderedExternalContent.tsx
@@ -1,1 +1,8 @@
 import React from "react";
+import { State } from "easy-peasy";
+import { ContentFetchState } from "../model/external-json-data";
+import { IPytchAppModel } from "../model";
+
+type FetchStateMapper<ContentT> = (
+  state: State<IPytchAppModel>
+) => ContentFetchState<ContentT>;
