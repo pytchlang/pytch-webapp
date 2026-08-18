6688ca4fba3f86dbc84d40a21e2b9bbc0742eab1
Ben North <ben.north@tcd.ie>
2026-08-18 17:30:21 +0100

    AriaWrappedSpinner: Allow wrapperDivClass prop

--- src/components/Junior/AriaWrappedSpinner.tsx
+++ src/components/Junior/AriaWrappedSpinner.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import classNames from "classnames";
 import Spinner from "react-bootstrap/Spinner";
 import { useTranslation } from "react-i18next";
 import { assertNever } from "../../utils";
 
 type AriaWrappedSpinnerProps = {
   kind: "loading" | "working";
+  wrapperDivClass?: string;
   wrapperDivStyle?: React.CSSProperties;
 };
 export const AriaWrappedSpinner: React.FC<AriaWrappedSpinnerProps> = ({
   kind,
+  wrapperDivClass,
   wrapperDivStyle,
 }) => {
   const { t } = useTranslation("common");
@@ -24,11 +27,16 @@ export const AriaWrappedSpinner: React.FC<AriaWrappedSpinnerProps> = ({
     }
   })();
 
+  const divClass = classNames(
+    "spinner-container mx-auto my-3 text-center",
+    wrapperDivClass
+  );
+
   return (
     <div
       aria-label={t(labelKey)}
       role="status"
-      className="spinner-container mx-auto my-3 text-center"
+      className={divClass}
       style={wrapperDivStyle}
     >
       <Spinner aria-hidden="true" animation="border" />
