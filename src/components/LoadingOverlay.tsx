348d55ecc6661e9239a8c39ae2acc98bdbef9890
Ben North <ben.north@tcd.ie>
2026-08-18 12:58:13 +0100

    Always render Spinner; take spinnerClass prop

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -1,19 +1,23 @@
 import React, { PropsWithChildren } from "react";
+import { Spinner } from "react-bootstrap";
 
-type LoadingOverlayProps = PropsWithChildren<{
+type LoadingOverlayProps = {
   show: boolean;
-}>;
+  spinnerClass?: string;
+};
 
 export const LoadingOverlay: React.FC<LoadingOverlayProps> = ({
   show,
-  children,
+  spinnerClass,
 }) => {
   if (!show) return null;
 
   return (
     <div className="loading-in-progress">
       <div className="background"></div>
-      <div className="content">{children}</div>
+      <div className="content">
+        <Spinner animation="border" className={spinnerClass} />
+      </div>
     </div>
   );
 };
