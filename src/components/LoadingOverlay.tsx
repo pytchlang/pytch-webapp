fd5d7390f7573458b6c4c6b8c557486bad3ae2bf
Ben North <ben.north@tcd.ie>
2026-08-18 13:02:03 +0100

    Add ARIA attrs

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import { Spinner } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 type LoadingOverlayProps = {
   show: boolean;
@@ -10,13 +11,23 @@ export const LoadingOverlay: React.FC<LoadingOverlayProps> = ({
   show,
   spinnerClass,
 }) => {
+  const { t } = useTranslation("common");
+
   if (!show) return null;
 
   return (
     <div className="loading-in-progress">
       <div className="background"></div>
-      <div className="content">
-        <Spinner animation="border" className={spinnerClass} />
+      <div
+        className="content"
+        aria-label={t("loading-content.label")}
+        role="status"
+      >
+        <Spinner
+          aria-hidden="true"
+          animation="border"
+          className={spinnerClass}
+        />
       </div>
     </div>
   );
