01fb236c4497aab7c387c06005c03276bc75bdd9
Ben North <ben.north@tcd.ie>
2026-08-18 16:42:49 +0100

    AriaWrappedSpinner: Define component

---
+++ src/components/Junior/AriaWrappedSpinner.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Spinner from "react-bootstrap/Spinner";
+import { useTranslation } from "react-i18next";
+import { assertNever } from "../../utils";
+
+type AriaWrappedSpinnerProps = {
+  kind: "loading" | "working";
+};
+export const AriaWrappedSpinner: React.FC<AriaWrappedSpinnerProps> = ({
+  kind,
+}) => {
+  const { t } = useTranslation("common");
+
+  const labelKey = (() => {
+    switch (kind) {
+      case "loading":
+        return "loading-content.label" as const;
+      case "working":
+        return "working.title" as const;
+      default:
+        return assertNever(kind);
+    }
+  })();
+
+  return (
+    <div
+      aria-label={t(labelKey)}
+      role="status"
+      className="spinner-container mx-auto my-3 text-center"
+    >
+      <Spinner aria-hidden="true" animation="border" />
+    </div>
+  );
+};
