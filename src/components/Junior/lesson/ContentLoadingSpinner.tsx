a97e8afaabb757b863b210e33ba9c97a3fb6351f
Ben North <ben.north@tcd.ie>
2026-08-18 16:16:00 +0100

    ContentLoadingSpinner: Generalise; add ARIA attrs

--- src/components/Junior/lesson/ContentLoadingSpinner.tsx
+++ src/components/Junior/lesson/ContentLoadingSpinner.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { EmptyProps } from "../../../utils";
 import { Spinner } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 export const ContentLoadingSpinner: React.FC<EmptyProps> = () => {
+  const { t } = useTranslation("common");
   return (
-    <div className="Junior-LessonContent-container">
-      <div className="spinner-container">
-        <Spinner animation="border" />
-      </div>
+    <div
+      aria-label={t("loading-content.label")}
+      role="status"
+      className="spinner-container my-3 text-center"
+    >
+      <Spinner aria-hidden="true" animation="border" />
     </div>
   );
 };
