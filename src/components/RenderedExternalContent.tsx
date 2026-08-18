3ec15201787a003922ebe07324ca27e75a614096
Ben North <ben.north@tcd.ie>
2026-08-18 12:15:44 +0100

    Take, heed either/or content-render prop

--- src/components/RenderedExternalContent.tsx
+++ src/components/RenderedExternalContent.tsx
@@ -32,9 +32,8 @@ type ContentRenderer<ContentT> =
 
 type MaybeContentProps<ContentT> = {
   fetchStateMapper: FetchStateMapper<ContentT>;
-  contentComponent: ContentComponent<ContentT>;
   resourceKeySuffix: FetchedResourceKind | false;
-};
+} & ContentRenderer<ContentT>;
 
 export function RenderedExternalContent<ContentT>(
   props: MaybeContentProps<ContentT>
@@ -55,9 +54,10 @@ export function RenderedExternalContent<ContentT>(
         </div>
       );
     case "available": {
-      return React.createElement(props.contentComponent, {
-        content: contentFetchState.content,
-      });
+      const content = contentFetchState.content;
+      return props.renderContent != null
+        ? props.renderContent(content)
+        : React.createElement(props.contentComponent, { content });
     }
     case "error": {
       const resourceKeySuffix = props.resourceKeySuffix;
