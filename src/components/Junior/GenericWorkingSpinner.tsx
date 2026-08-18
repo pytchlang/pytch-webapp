071813e5cbb8cbccd6cccd531dc7a7f915d583a9
Ben North <ben.north@tcd.ie>
2026-08-18 17:30:54 +0100

    GenericWorkingSpinner: Allow wrapperDivClass prop

--- src/components/Junior/GenericWorkingSpinner.tsx
+++ src/components/Junior/GenericWorkingSpinner.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { EmptyProps } from "../../utils";
 import { AriaWrappedSpinner } from "./AriaWrappedSpinner";
 
-export const GenericWorkingSpinner: React.FC<EmptyProps> = () => (
-  <AriaWrappedSpinner kind="working" />
-);
+type GenericWorkingSpinnerProps = {
+  wrapperDivClass?: string;
+};
+export const GenericWorkingSpinner: React.FC<GenericWorkingSpinnerProps> = ({
+  wrapperDivClass,
+}) => <AriaWrappedSpinner kind="working" wrapperDivClass={wrapperDivClass} />;
