a2f029e1d8b5b265765f99cb5a6db9a3eb56f3a6
Ben North <ben.north@tcd.ie>
2026-08-18 15:02:08 +0100

    <s>ActionOrBusyButton: Define component

---
+++ src/components/async-flow-modals/ActionOrBusyButton.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  InteractingOrAttemptingAsyncUserFlowFsmState,
+  settleFunctions,
+} from "../../model/user-interactions/async-user-flow";
+import { Button, Spinner } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { ButtonVariant } from "react-bootstrap/esm/types";
+import { assertNever } from "../../utils";
+
+type ActionOrBusyButtonProps<RunStateT> = {
+  flowState: InteractingOrAttemptingAsyncUserFlowFsmState<RunStateT>;
+  isSubmittable: boolean;
+  interactingLabel: string;
+  variant?: ButtonVariant;
+};
+export function ActionOrBusyButton<RunStateT>({
+  flowState,
+  isSubmittable,
+  interactingLabel,
+  variant,
+}: ActionOrBusyButtonProps<RunStateT>): React.ReactNode {
+  const { t } = useTranslation("common");
+
+  switch (flowState.kind) {
+    case "attempting":
+      return (
+        <Button
+          as="div"
+          aria-label={t("working.title")}
+          role="status"
+          className="ActionOrBusyButton"
+          disabled={true}
+          variant="primary"
+        >
+          <Spinner aria-hidden="true" size="sm" />
+        </Button>
+      );
+    case "interacting": {
+      const settle = settleFunctions(isSubmittable, flowState);
+      return (
+        <Button
+          title={interactingLabel}
+          className="ActionOrBusyButton"
+          disabled={!isSubmittable}
+          variant={variant ?? "primary"}
+          onClick={settle.submit}
+        >
+          {interactingLabel}
+        </Button>
+      );
+    }
+    default:
+      return assertNever(flowState);
+  }
+}
