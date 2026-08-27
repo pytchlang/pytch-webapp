6e963e1ff06a330e72787612bf7a2715ef7b53be
Ben North <ben.north@tcd.ie>
2026-08-27 13:10:57 +0100

    Define e2e specs for flat/per-method skip-links

--- cypress/e2e/keyboard-navigation/skip-links.cy.ts
+++ cypress/e2e/keyboard-navigation/skip-links.cy.ts
@@ -1,5 +1,6 @@
 import { SkipLinkFocusTarget } from "../../../src/model/junior/global-steer-focus";
 import { PytchProgramKind } from "../../../src/model/pytch-program-types";
+import { assertFocus } from "./utils";
 
 type SkipLinkSpec = {
   target: SkipLinkFocusTarget;
@@ -10,3 +11,50 @@ type SkipLinkTestSpec = {
   projectZipfileFixture: string;
   skipSpecs: Array<SkipLinkSpec>;
 };
+
+const specs: Array<SkipLinkTestSpec> = [
+  {
+    programKind: "per-method",
+    projectZipfileFixture: "newly-created-per-method.zip",
+    skipSpecs: [
+      {
+        target: "activity-tab-bar",
+        assertFun: () => assertFocus("activity-tab", "helpsidebar"),
+      },
+      {
+        target: "per-method-actor-props",
+        assertFun: () => assertFocus("add-script-button"),
+      },
+      {
+        target: "project-stage",
+        assertFun: () => assertFocus("stage"),
+      },
+      {
+        target: "per-method-actors",
+        assertFun: () => assertFocus("actor-card", 0),
+      },
+    ],
+  },
+  {
+    programKind: "flat",
+    projectZipfileFixture: "v4-print-things.zip",
+    skipSpecs: [
+      {
+        target: "activity-tab-bar",
+        assertFun: () => assertFocus("activity-tab", "helpsidebar"),
+      },
+      {
+        target: "flat-code",
+        assertFun: () => assertFocus("flat-code-editor"),
+      },
+      {
+        target: "project-stage",
+        assertFun: () => assertFocus("stage"),
+      },
+      {
+        target: "flat-assets",
+        assertFun: () => assertFocus("flat-asset", 0),
+      },
+    ],
+  },
+];
