2c206b65f413a4b7edab6c3867fb69bf7f7014ef
Ben North <ben.north@tcd.ie>
2026-08-27 13:09:43 +0100

    Define types for skip-link e2e tests

---
+++ cypress/e2e/keyboard-navigation/skip-links.cy.ts
@@ -0,0 +1,13 @@
+import { SkipLinkFocusTarget } from "../../../src/model/junior/global-steer-focus";
+import { PytchProgramKind } from "../../../src/model/pytch-program-types";
+
+type SkipLinkSpec = {
+  target: SkipLinkFocusTarget;
+  assertFun: () => void;
+};
+
+type SkipLinkTestSpec = {
+  programKind: PytchProgramKind;
+  projectZipfileFixture: string;
+  skipSpecs: Array<SkipLinkSpec>;
+};
