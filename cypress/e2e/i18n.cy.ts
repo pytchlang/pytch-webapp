85e2d542c8f5c2de2c8f026eda4950beebfb556f
Ben North <ben.north@tcd.ie>
2026-08-28 10:04:53 +0100

    Test language choice on front page

--- cypress/e2e/i18n.cy.ts
+++ cypress/e2e/i18n.cy.ts
@@ -1,2 +1,21 @@
 context("Internationalisation", () => {
+  it("language choice on front page", () => {
+    cy.pytchResetDatabase();
+
+    const langSpecs = [
+      { code: "en", textMatch: "A bridge from Scratch to Python" },
+      { code: "ga", textMatch: "Droichead ó Scratch go Python" },
+    ];
+
+    function chooseLanguage(code: string) {
+      cy.get(".NavbarLanguageChooser a.dropdown-toggle").click();
+      cy.get(`.NavbarLanguageChooser a[data-language-code="${code}"]`).click();
+    }
+
+    langSpecs.forEach(({ code, textMatch }) => {
+      chooseLanguage(code);
+      cy.get("header h3").should("have.length", 1).contains(textMatch);
+      chooseLanguage("en");
+    });
+  });
 });
