f9e7486bd9bbcd798b5c0c8c7f533e1a1e64dd36
Ben North <ben.north@tcd.ie>
2026-09-08 09:46:19 +0100

    assign_help(): Use fallback xlns

--- tools/i18n/demux_poeditor_file.py
+++ tools/i18n/demux_poeditor_file.py
@@ -74,7 +74,10 @@ hsb_fb_xlns = i18n_fb_data_from_ns[HSB_KEY_STEM]
 
 def assign_help(help_entry: dict[str, Any], section_slug: str, slug: str) -> None:
     key_stem = f"{section_slug}.item.{slug}.help"
-    xlns = {k: v for k, v in hsb_xlns.items() if k.startswith(key_stem)}
+
+    # Populate with fallback then overwrite with the main target xlns.
+    xlns = {k: v for k, v in hsb_fb_xlns.items() if k.startswith(key_stem)}
+    xlns.update({k: v for k, v in hsb_xlns.items() if k.startswith(key_stem)})
 
     def xln(suffix: str) -> str:
         return xlns[f"{key_stem}{suffix}"]
