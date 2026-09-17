2bace2202e1ce44ec0ae51b96e69d2b092637bbf
Ben North <ben.north@tcd.ie>
2026-09-17 13:01:43 +0100

    IDELayout: Remove bookmark-setting code
    Will shortly re-implement more cleanly inside ActivityBar.

--- src/components/IDELayout.tsx
+++ src/components/IDELayout.tsx
@@ -46,43 +46,6 @@ export const IDELayout: React.FC<EmptyProps> = () => {
 
   useActionAsEffect((actions) => actions.reloadServer.maybeConnect);
 
-  // Even though we refer to activityContentFullStateLabel, we only want
-  // to set the bookmark on initial render; hence omitting
-  // activityContentFullStateLabel from deps array.
-  useEffect(
-    () => {
-      // TODO: The facts about which activities are present and which one
-      // is active when booted are spread across the code.  Here, and in
-      // (junior) `EditState.bootForProgram()`.  Would be good to tidy
-      // this up somehow.
-      const defaultBookmark = (() => {
-        switch (activityContentFullStateLabel) {
-          case "collapsed":
-          case "expanded-helpsidebar":
-            return 0;
-          case "expanded-specimen":
-          case "expanded-lesson":
-          case "expanded-tutorial":
-          case "expanded-demo":
-            return 1;
-          case "expanded-keynavhelp":
-          case "expanded-i18n":
-            console.warn(
-              "should not have expanded-(keynavhelp|i18n) on first render"
-            );
-            // But return something non-erroneous anyway.
-            return 0;
-          default:
-            return assertNever(activityContentFullStateLabel);
-        }
-      })();
-
-      focusContext.bookmarkItemByKeyAndIndex("ActivityBar", defaultBookmark);
-    },
-    // eslint-disable-next-line @eslint-react/exhaustive-deps
-    [focusContext]
-  );
-
   if (isFullScreen) {
     return <FullScreenLayout />;
   }
