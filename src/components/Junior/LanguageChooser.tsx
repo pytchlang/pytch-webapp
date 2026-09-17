4cc7c7984246eaab46dd0c63385df8e1f5cbf40e
Ben North <ben.north@tcd.ie>
2026-09-17 12:51:14 +0100

    LanguageChooser: Focus active button on expansion

--- src/components/Junior/LanguageChooser.tsx
+++ src/components/Junior/LanguageChooser.tsx
@@ -3,15 +3,19 @@ import { EmptyProps } from "../../utils";
 import { useI18nResolvedLanguage, useSetLanguageFun } from "./hooks";
 import { supportedLanguages } from "../../model/i18n";
 import { useTranslation } from "react-i18next";
+import { useRef } from "react";
+import { MaybeSeizeFocus } from "../MaybeSeizeFocus";
 import "./LanguageChooser.scss";
 
 export const LanguageChooser: React.FC<EmptyProps> = () => {
   const { t } = useTranslation("ide");
   const resolvedLanguage = useI18nResolvedLanguage();
   const setLanguageFun = useSetLanguageFun();
+  const activeButtonRef = useRef<HTMLButtonElement>(null);
 
   return (
     <div className="LanguageChooser gfs__help-content">
+      <MaybeSeizeFocus targetRef={activeButtonRef} />
       <Container className="help-text">
         <h1>{t("language-chooser.header")}</h1>
         <ListGroup>
@@ -20,6 +24,7 @@ export const LanguageChooser: React.FC<EmptyProps> = () => {
             return (
               <ListGroup.Item
                 key={lngDescr.lngCode}
+                ref={isActive ? activeButtonRef : undefined}
                 data-language-code={lngDescr.lngCode}
                 as="button"
                 active={isActive}
