2dbfc6354eda78d717d20f204d1e5b5b3eab51ae
Ben North <ben.north@tcd.ie>
2026-09-24 19:25:22 +0100

    Add docstring-like comment

--- tools/typecheck-at-merge-commits.sh
+++ tools/typecheck-at-merge-commits.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -e
 
+# This is likely to be specific to how I (BN) develop git histories.
+# I work with one flat history of commits, but marked up to indicate
+# sections.  Commits whose subject starts with "<s>" are the first
+# commit of a section.  Commits whose subject starts with </s> end
+# that section with a merge commit.  In the "linear" history, those
+# "</s>" commits are empty, i.e., make no changes to the tree.  Often
+# the within-section commits break the build, e.g., they make some
+# change but do not tidy up now-unused imports.  But the idea is that
+# the merge commits should always typecheck and lint cleanly.  This
+# script runs the `typecheck-at-commit.sh` script on each merge-marked
+# commit.
+#
+# See
+#
+#   https://github.com/bennorth/git-dendrify
+#
+# for more detail on this idea of linear vs structured histories.
+
 if [ "$#" -ne 2 ]; then
     >&2 echo Usage: "$0" STARTING-COMMIT OUTPUT-DIRECTORY
     exit 1
